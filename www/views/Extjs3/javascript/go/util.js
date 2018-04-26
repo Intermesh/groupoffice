cb37c0e1d089fdc0ce4a2401b9d49bb4ca33883e
Wesley Smits <wsmits@intermesh.nl>
2018-04-26 13:34:44 +0200

    Util quota

--- www/views/Extjs3/javascript/go/util.js
+++ www/views/Extjs3/javascript/go/util.js
@@ -16,3 +16,26 @@ go.util.mergeObjects = function (a, b) {
 	}
 	return a;
 };
+
+/**
+ * Convert bytes to a user readable format
+ * 
+ * @param int bytes
+ * @param boolean conventionDecimal
+ * @return {String}
+ */
+go.util.humanFileSize = function(bytes, conventionDecimal) {
+    var thresh = conventionDecimal ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = conventionDecimal
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
+};
