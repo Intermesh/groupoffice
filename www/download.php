1a4dfd86e8547b5f318441ed8faea9d3959a12b2
Merijn Schering <mschering@intermesh.nl>
2018-05-03 15:54:06 +0200

    Performance tuning

--- www/download.php
+++ www/download.php
@@ -1,6 +1,10 @@
 <?php
 require(__DIR__ . "/vendor/autoload.php");
+
+use go\core\App;
 use go\core\fs\Blob;
+
+App::get();
 
 $blob = Blob::findById($_GET['blob']);
 if (empty($blob) || !file_exists($blob->path())) {
