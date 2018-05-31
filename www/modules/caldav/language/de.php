2d3d03f38ab271d00f59931817ad91728d84f90a
Wesley Smits <wsmits@intermesh.nl>
2018-05-31 14:08:41 +0200

    Added check for config.ini

--- www/modules/caldav/language/de.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'name' => 'CalDAV-Server',
-  'description' => 'Ermöglicht den Zugriff auf Ihren Kalender über den CalDAV-Server. Die DAV-Dienste wurden mit der SabreDAV-Bibliothek erstellt.',
-);
--- www/modules/caldav/language/es.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'name' => 'Servidor CalDAV',
-  'description' => 'Permite acceder y/o sincronizar el calendario desde otras aplicaciones. El servicio  DAV se gestiona a través de la librería SabreDAV.',
-);
