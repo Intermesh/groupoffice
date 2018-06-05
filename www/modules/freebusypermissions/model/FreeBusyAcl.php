5cb1251148682895562c9032f1dfb519bde3912d
mdhart <mdhart@192.168.1.11>
2018-06-05 16:13:25 +0200

    6.3 fixes for freebusy permissions

--- www/modules/freebusypermissions/model/FreeBusyAcl.php
+++ www/modules/freebusypermissions/model/FreeBusyAcl.php
@@ -12,60 +12,27 @@
  * @copyright Copyright Intermesh BV.
  * @author Wesley Smits wsmits@intermesh.nl
  */
- 
+
+namespace GO\Freebusypermissions\Model;
+
 /**
  * The FreeBusyAcl model
  *
  * @package GO.modules.Freebusypermissions
- * @version $Id: FreeBusyAcl.php 7607 2012-07-03 10:31:57Z wsmits $
  * @copyright Copyright Intermesh BV.
  * @author Wesley Smits wsmits@intermesh.nl
  *
  * @property int $user_id
  * @property int $acl_id
  */
-
-
-namespace GO\Freebusypermissions\Model;
-
-
-class FreeBusyAcl extends \GO\Base\Db\ActiveRecord{
-
-	/**
-	 * Returns a static model of itself
-	 * 
-	 * @param String $className
-	 * @return FreeBusyAcl
-	 */
-	public static function model($className=__CLASS__)
-	{	
-		return parent::model($className);
+class FreeBusyAcl extends \GO\Base\Db\ActiveRecord {
+	
+	public function tableName() {
+		 return 'fb_acl';
 	}
 	
 	public function primaryKey() {
-		return array('user_id','acl_id');
+		return ['user_id','acl_id'];
 	}
-
-
-	/**
-	 * Enable this function if you want this model to check the acl's automatically.
-	 */
-	// public function aclField(){
-	//	 return 'acl_id';	
-	// }
-
-	/**
-	 * Returns the table name
-	 */
-	 public function tableName() {
-		 return 'fb_acl';
-	 }
-
-	/**
-	 * Here you can define the relations of this model with other models.
-	 * See the parent class for a more detailed description of the relations.
-	 */
-	 public function relations() {
-		 return array();
-	 }
+	
 }
