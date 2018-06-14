2eace928064159b8f9221fd5909a488521e58a15
Merijn Schering <mschering@intermesh.nl>
2018-06-14 12:20:43 +0200

    Clear database before installing custom fields module

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -1059,7 +1059,7 @@ var $billing_clear_payment_method_on_duplicate = true;
 	 * @var     StringHelper
 	 * @access  public
 	 */
-	var $version = '6.2.97';
+	var $version = '6.2.98';
 
 	/**
 	 * Modification date
@@ -1068,7 +1068,7 @@ var $billing_clear_payment_method_on_duplicate = true;
 	 * @access  public
 	 */
 	
-	var $mtime = '20180612';
+	var $mtime = '20180614';
 
 	#group configuration
 	/**
