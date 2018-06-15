c976703905bed2b82cdb22e5b7e697d3d2454f08
Merijn Schering <mschering@intermesh.nl>
2018-06-15 14:06:09 +0200

    Implemented loginAs in new users grid

--- www/go/modules/core/users/views/extjs3/SystemSettingsUserGrid.js
+++ www/go/modules/core/users/views/extjs3/SystemSettingsUserGrid.js
@@ -157,7 +157,19 @@ go.modules.core.users.SystemSettingsUserGrid = Ext.extend(go.grid.GridPanel, {
 						iconCls: 'ic-swap-horiz',
 						text: t("Login as this user"),
 						handler: function() {
-							alert('TODO');
+							go.Jmap.request({
+								method: "User/loginAs",
+								params: {userId: this.moreMenu.record.id},
+								callback: function(options, success, result) {
+									if(!result.success) {
+										Ext.MessageBox.alert(t("Error"), t("Failed to login as this user"));
+										return;
+									}
+									
+									//reload client
+									document.location = BaseHref;
+								}
+							})
 						},
 						scope: this						
 					},
--- www/modules/admin2userlogin/language/en.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'name' => 'Switch user',
-  'description' => 'Admins can switch to any user account with this module.',
-);
