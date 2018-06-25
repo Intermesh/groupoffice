4ba22d747eca42a74afb34b4a42015c89f054d9c
Merijn Schering <mschering@intermesh.nl>
2018-06-25 16:10:07 +0200

    Better way of keeping memory low on maintenance scripts

--- www/go/base/db/ActiveRecord.php
+++ www/go/base/db/ActiveRecord.php
@@ -4873,16 +4873,23 @@ ORDER BY `book`.`name` ASC ,`order`.`btime` DESC
 
 	public function rebuildSearchCache(){
 		
-		$pdo = new \GO\Base\Db\PDO();
-			//to avoid memory errors
-		$pdo->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY,false);
-		$this->setDbConnection($pdo);
 		
+				
 		$rc = new \GO\Base\Util\ReflectionClass($this);
 		$overriddenMethods = $rc->getOverriddenMethods();
 		if(in_array("getCacheAttributes", $overriddenMethods)){
-			$stmt = $this->find(FindParams::newInstance()->ignoreAcl()->select('t.*'));
-			$stmt->callOnEach('cacheSearchRecord', true);
+			
+			$start = 0;
+		
+			//per thousands to keep memory low
+			$stmt = $this->find(FindParams::newInstance()->ignoreAcl()->select('t.*')->limit(1000)->start($start));
+
+			while($stmt->rowCount()) {	
+				$stmt->callOnEach('cacheSearchRecord', true);
+				
+				$stmt = $this->find(FindParams::newInstance()->ignoreAcl()->select('t.*')->limit(1000)->start($start+=1000));				
+			}
+			
 		}
 	}
 
