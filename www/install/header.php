1d5e8893f57950ea0c76500a41de7b0ccc97e9e7
Merijn Schering <mschering@intermesh.nl>
2018-06-25 10:57:36 +0200

    Always show configfile errors

--- www/install/header.php
+++ www/install/header.php
@@ -18,9 +18,9 @@
 		
 		
 		if(is_dir("/etc/groupoffice/" . $_SERVER['HTTP_HOST'])) {	
-			echo "<section>";		
+			echo "<section><fieldset>";		
 			echo("A config folder was found in /etc/groupoffice/" . $_SERVER['HTTP_HOST'] .". Please move all your domain configuration folders from /etc/groupoffice/* into /etc/groupoffice/multi_instance/*. Only move folders, leave /etc/groupoffice/config.php and other files where they are.");
-			echo "</section>";
+			echo "</fieldset></section>";
 		
 			require('footer.php');
 			exit();
