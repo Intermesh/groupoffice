8b4e4b5c4c233be6c3430e9f5cdedcd5e8995f4a
Merijn Schering <mschering@intermesh.nl>
2018-06-28 10:59:02 +0200

    Password generator field copies new password to clipboard and prevents autocomplete.

--- www/views/Extjs3/javascript/go/form/PasswordGeneratorField.js
+++ www/views/Extjs3/javascript/go/form/PasswordGeneratorField.js
@@ -9,14 +9,23 @@ go.form.PasswordGeneratorField = Ext.extend(Ext.form.TriggerField, {
 		'ext:qtip': t("Generate password")
 	},
 	initComponent: function() {
-		this.defaultAutoCreate.autocomplete = "new-password"; //prevent autocomplete
+		this.defaultAutoCreate.autocomplete = "new-password"; //prevent autocomplete in chrome
 		go.form.PasswordGeneratorField.superclass.initComponent.call(this);
+		
+		this.on("afterrender", function(field) {
+			//clear browser autofill from firefox
+			setTimeout(function(){
+				field.reset();
+			}, 500);							
+		});
 	},
 	onTriggerClick: function () {
 		var pass = this.generatePassword(8);
 		this.setValue(pass);
-
-		Ext.MessageBox.alert(t("Password", "users"), t("The generated password is") + ": " + Ext.util.Format.htmlEncode(pass));
+		
+		go.util.copyTextToClipboard(pass);
+		
+		Ext.MessageBox.alert(t("Password", "users"), t("The generated password has been copied to your clipboard.")); // + ": " + Ext.util.Format.htmlEncode(pass)
 
 		this.fireEvent('generated', this, pass);
 	},
