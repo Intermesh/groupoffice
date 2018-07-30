16a438a17cec8e82937bc2fb8be2089a3f6db1ae
Merijn Schering <mschering@intermesh.nl>
2018-07-30 14:22:08 +0200

    highestModSeq

--- www/go/core/jmap/ReadOnlyEntityController.php
+++ www/go/core/jmap/ReadOnlyEntityController.php
@@ -173,7 +173,7 @@ abstract class ReadOnlyEntityController extends Controller {
 		$cls = $this->entityClass();
 		
 		//entities that don't support syncing can be listed and fetched with the read only controller
-		return $cls::getState();
+		return method_exists($cls, "getState") ? $cls::getState() : null;
 	}
 
 	/**
