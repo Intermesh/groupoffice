3a19f586dc552eb5c71652bcb3a7c6e782e85bdc
Merijn Schering <mschering@intermesh.nl>
2018-08-07 14:35:50 +0200

    Styled email addresses and phone numbers

--- www/go/modules/community/addressbook/views/extjs3/ContactDIalog.js
+++ www/go/modules/community/addressbook/views/extjs3/ContactDIalog.js
@@ -6,6 +6,7 @@ go.modules.community.addressbook.ContactDialog = Ext.extend(go.form.Dialog, {
 	height: 600,
 
 	initFormItems: function () {
+
 		var items = [{
 				xtype: 'fieldset',
 				autoHeight: true,
@@ -19,9 +20,9 @@ go.modules.community.addressbook.ContactDialog = Ext.extend(go.form.Dialog, {
 						xtype: 'textfield',
 						name: 'name',
 						fieldLabel: t("Name"),
-						anchor: '100%',
+						anchor: '-' + dp(28), //for delete button of gridfields
 						allowBlank: false
-					},{
+					}, {
 						xtype: "gridfield",
 						name: "emailAddresses",
 						store: new Ext.data.JsonStore({
@@ -33,23 +34,29 @@ go.modules.community.addressbook.ContactDialog = Ext.extend(go.form.Dialog, {
 								'email'
 							]
 						}),
-						fieldLabel: "E-mail addresses",
+						fieldLabel: t("E-mail addresses"),
 
 						autoExpandColumn: "email",
 						columns: [
 							{
+								xtype: "combocolumn",
+								store: new Ext.data.ArrayStore({
+									id: 0,
+									fields: [
+										'value',
+										'display'
+									],
+									data: [['work', t("Work")], ['home', t("Home")]]
+								}),
 								id: 'type',
 								sortable: false,
 								dataIndex: 'type',
 								hideable: false,
 								draggable: false,
 								menuDisabled: true,
-								width: dp(80),
-								editor: new Ext.form.TextField({
-									allowBlank: false,
-									emptyText: "Type"
-								})
-							},{
+								width: dp(100)
+
+							}, {
 								id: 'email',
 								sortable: false,
 								dataIndex: 'email',
@@ -58,12 +65,62 @@ go.modules.community.addressbook.ContactDialog = Ext.extend(go.form.Dialog, {
 								menuDisabled: true,
 								editor: new Ext.form.TextField({
 									allowBlank: false,
-									vtype: 'emailAddress',
-									emptyText: "E-mail"
+									vtype: 'emailAddress'
 								})
 							}
-						],
-					}]
+						]
+					}
+					
+					,{
+						xtype: "gridfield",
+						name: "phoneNumbers",
+						store: new Ext.data.JsonStore({
+							autoDestroy: true,
+							root: "records",
+							fields: [
+								'id',
+								'type',
+								'number'
+							]
+						}),
+						fieldLabel: t("Phone numbers"),
+
+						autoExpandColumn: "number",
+						columns: [
+							{
+								xtype: "combocolumn",
+								store: new Ext.data.ArrayStore({
+									id: 0,
+									fields: [
+										'value',
+										'display'
+									],
+									data: [['mobile', t("Mobile")], ['work', t("Work")], ['home', t("Home")]]
+								}),
+								id: 'type',
+								sortable: false,
+								dataIndex: 'type',
+								hideable: false,
+								draggable: false,
+								menuDisabled: true,
+								width: dp(100)
+
+							}, {
+								id: 'number',
+								sortable: false,
+								dataIndex: 'number',
+								hideable: false,
+								draggable: false,
+								menuDisabled: true,
+								editor: new Ext.form.TextField({
+									allowBlank: false
+								})
+							}
+						]
+					}
+				
+				
+				]
 			}
 		];//.concat(go.CustomFields.getFormFieldSets("Contact"));
 
