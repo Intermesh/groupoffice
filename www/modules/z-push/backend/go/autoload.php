bc9305a4feab18cc85e98f7d92171909798873ab
Merijn Schering <mschering@intermesh.nl>
2018-08-09 12:09:42 +0200

    Removed old z-push install dir

---
+++ www/modules/z-push/backend/go/autoload.php
@@ -0,0 +1,15 @@
+<?php
+require("config.php");
+require("goSyncUtils.php");
+require("goImporter.php");
+require("goExporter.php");
+
+require('goBaseBackendDiff.php');
+require('GoImapStreamWrapper.php');
+require('goCalendar.php');
+require('goContact.php');
+require('goMail.php');
+require('goNote.php');
+require('goTask.php');
+
+require('go.php');
