d2dfe24210b38c97e78a405d15daeea0335f1442
Merijn Schering <mschering@intermesh.nl>
2018-09-03 16:55:29 +0200

    Support newer encryption library in notes

--- www/go/modules/community/notes/views/extjs3/NoteDetail.js
+++ www/go/modules/community/notes/views/extjs3/NoteDetail.js
@@ -37,25 +37,54 @@ go.modules.community.notes.NoteDetail = Ext.extend(go.panels.DetailView, {
 
 	decrypt: function () {
 
-		if (!this.data.content || this.data.content.substring(0, 9) != "{GOCRYPT}") {
+		if (!this.data.content || this.data.content.substring(0, 8) != "{GOCRYPT") {
 			return;
 		}
+		
 		var key = prompt("Enter password to decrypt");
-		var msg = window.atob(this.data.content.substring(9));
 
-		var iv = (msg.substring(0, 32));			 // extract iv
-		var body = (msg.substring(32, msg.length - 32));	 //extract ciphertext
-		var serialized = mcrypt.Decrypt(body, iv, key, "rijndael-256", "ctr");
-		console.log(serialized);
-		//result should be a serialized sting by PHP
-		var match = serialized.match(/.*"([\s\S]*)"/);
-		if (!match) {
-			alert("Incorrect password!");
-			//this.data.content = "Encrypted text";
-			return;
+		if(this.data.content.substring(0, 9) == "{GOCRYPT}") {
+
+			var msg = window.atob(this.data.content.substring(9));
+
+			var iv = (msg.substring(0, 32));			 // extract iv
+			var body = (msg.substring(32, msg.length - 32));	 //extract ciphertext
+			var serialized = mcrypt.Decrypt(body, iv, key, "rijndael-256", "ctr");
+			//result should be a serialized sting by PHP
+			var match = serialized.match(/.*"([\s\S]*)"/);
+			if (!match) {
+				alert("Incorrect password!");
+				//this.data.content = "Encrypted text";
+				return;
+			}
+
+			this.data.content = Ext.util.Format.nl2br(match[1]);
+		} else
+		{
+			//new encryption
+			//this.data.content = "Decrypting...";
+			
+			go.Jmap.request({
+				method: "Note/decrypt",
+				params: {
+					id: this.data.id,
+					password: key
+				},
+				callback: function(options, success, response) {
+					if(success) {
+						console.log(response);
+						this.data.content = response.content;
+						
+						this.items.item(0).onLoad(this);
+					} else
+					{
+						Ext.MessageBox.alert(t("Error"), t("Invalid password"));
+					}
+				},
+				scope: this
+			})
 		}
-
-		this.data.content = Ext.util.Format.nl2br(match[1]);
+		
 	},
 
 	onLoad: function () {
