fc2c68b0a2d9a55e044cc2a2301b0f951c9eca72
Merijn Schering <mschering@intermesh.nl>
2018-09-06 14:53:04 +0200

    Added todo

--- www/go/modules/community/addressbook/Module.php
+++ www/go/modules/community/addressbook/Module.php
@@ -7,6 +7,21 @@ use go\core\module\Base;
  * @copyright (c) 2018, Intermesh BV http://www.intermesh.nl
  * @author Merijn Schering <mschering@intermesh.nl>
  * @license http://www.gnu.org/licenses/agpl-3.0.html AGPLv3
+ * 
+ * @todo 
+ * filters
+ * Merge
+ * Batch edit
+ * Export
+ * Import
+ * Carddav
+ * Document templates
+ * ActiveSync
+ * Migration
+ * Send newsletter
+ * 
+ * 
+ * 
  */
 class Module extends Base {
 							
