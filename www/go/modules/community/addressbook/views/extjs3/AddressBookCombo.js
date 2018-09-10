a2006d3c99ea4b81452cbfa2460257e914635cc7
Merijn Schering <mschering@intermesh.nl>
2018-09-10 16:11:31 +0200

    Address book filter

---
+++ www/go/modules/community/addressbook/views/extjs3/AddressBookCombo.js
@@ -0,0 +1,30 @@
+go.modules.community.addressbook.AddresBookCombo = Ext.extend(go.form.ComboBox, {
+	fieldLabel: t("Addres Book"),
+	hiddenName: 'addressBookId',
+	anchor: '100%',
+	emptyText: t("Please select..."),
+	pageSize: 50,
+	valueField: 'id',
+	displayField: 'name',
+	triggerAction: 'all',
+	editable: true,
+	selectOnFocus: true,
+	forceSelection: true,
+	allowBlank: false,
+	initComponent: function () {
+		Ext.applyIf(this, {
+			store: new go.data.Store({
+				fields: ['id', 'name'],
+				entityStore: go.Stores.get("AddressBook"),
+				baseParams: {
+					filter: {
+							permissionLevel: GO.permissionLevels.write
+					}
+				}
+			})
+		});
+		
+		go.modules.community.addressbook.AddresBookCombo.superclass.initComponent.call(this);
+
+	}
+});
--- www/go/modules/community/addressbook/views/extjs3/CustomFieldSetDialog.js
+++ www/go/modules/community/addressbook/views/extjs3/CustomFieldSetDialog.js
@@ -2,7 +2,7 @@ go.modules.community.addressbook.CustomFieldSetDialog = Ext.extend(go.form.Dialo
 	stateId: 'addressbook-custom-field-set-dialog',
 	title: t('Field set'),
 	entityStore: go.Stores.get("FieldSet"),
-	autoHeight: true,
+	height: dp(400),
 	initFormItems: function () {
 		return [{
 				xtype: 'fieldset',
@@ -27,23 +27,50 @@ go.modules.community.addressbook.CustomFieldSetDialog = Ext.extend(go.form.Dialo
 							{boxLabel: t("All"), inputValue: "all"},
 							{boxLabel: t("Contacts"), inputValue: 'contacts'},
 							{boxLabel: t("Organizations"), inputValue: 'organizations'}
-						],
+						]
+						
+					},{
+						xtype:"checkbox",
+						name: "enableAddressBookFilter",
+						boxLabel: t("Only show this field set for selected address books"),
+						hideLabel: true,
+						submit: false,
 						listeners: {
-							scope: this,
-							change: this.onEnableForChange
+							check: function(f, checked) {
+								this.formPanel.getForm().findField("addressBooks").setDisabled(!checked);
+							},
+							scope: this
 						}
-					}]
+					},
+					{
+						anchor: '100%',
+						disabled: true,
+						xtype: "chips",
+						submit: false,
+						entityStore: go.Stores.get("AddressBook"),
+						displayField: "name",
+						name: "addressBooks",
+						fieldLabel: t("Address books")
+					}
+				
+				]
 			}
 		];
 	},
+
 	
-	onEnableForChange : function(field, radioBtn) {
+	onLoad : function() {
+		this.evalEnableFor();
 		
+		return go.modules.community.addressbook.CustomFieldSetDialog.superclass.onLoad.call(this);
+	},
+	
+	onBeforeSubmit : function() {
 		if(!this.formPanel.values.filter) {
 			this.formPanel.values.filter = {};
 		}
 		
-		switch(radioBtn.inputValue) {
+		switch(this.formPanel.getForm().findField('enableFor').getValue()) {
 			case 'contacts':
 				this.formPanel.values.filter.isOrganization = false;
 				break;
@@ -57,17 +84,30 @@ go.modules.community.addressbook.CustomFieldSetDialog = Ext.extend(go.form.Dialo
 				break;
 				
 		}
+		
+		
+		var enableAbFilter = this.formPanel.getForm().findField("enableAddressBookFilter").getValue();
+		
+		if(!enableAbFilter) {
+			delete this.formPanel.values.filter.addressBooks;
+		} else
+		{
+			this.formPanel.values.filter.addressBooks = this.formPanel.getForm().findField("addressBooks").getValue().column("id");
+		}
+		
+		return go.modules.community.addressbook.CustomFieldSetDialog.superclass.onBeforeSubmit.call(this);
 	},
 	
-	onLoad : function() {
-		this.evalEnableFor();
-		
-		go.modules.community.addressbook.CustomFieldSetDialog.superclass.onLoad.call(this);
-	},
 	
 	evalEnableFor : function() {
 		var enableFor = this.formPanel.getForm().findField('enableFor');
-		console.log(this.formPanel.entity);
+		
+		this.formPanel.getForm().findField("enableAddressBookFilter").setValue(!!this.formPanel.entity.filter.addressBooks);
+		
+		go.Stores.get("AddressBook").get(this.formPanel.entity.filter.addressBooks, function(addressBooks) {
+			this.formPanel.getForm().findField("addressBooks").setValue(addressBooks);
+		}, this);
+		
 		switch(this.formPanel.entity.filter.isOrganization) {
 			case true:
 				enableFor.setValue("organizations");
