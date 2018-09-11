85e7d5a78369f06a9a32ae4b361bcc18f7cf0565
Michael de Hart <mdhart@intermesh.nl>
2018-09-11 17:26:35 +0200

    added openFileDialog method for upload

--- www/views/Extjs3/javascript/go/util.js
+++ www/views/Extjs3/javascript/go/util.js
@@ -28,6 +28,57 @@
 			}, function (err) {
 				console.error('Async: Could not copy text: ', err);
 			});
+		},
+		/**
+		 * cfg.multiple: boolean allow selecting multi files
+		 * cfg.accept: string mime type or file extensions to allow for selection
+		 * cfg.directory: boolean allow directory upload
+		 * cfg.autoUpload: boolean jmap upload file on select
+		 * cfg.listeners
+		 *   select => (files: File[]): callback to trigger when files are selected
+		 *   upload => (response: Blob): response from server after every Upload completed (if autoUpload)
+		 *   uploadComplete => () when all uploads are complete (if autoUpload)
+		 *   scope: same as in ext
+		 * @param {object} cfg
+		 */
+		openFileDialog: function(cfg) {
+			if (!this.uploadDialog) {
+				this.uploadDialog = document.createElement("input");
+				this.uploadDialog.setAttribute("type", "file");
+			}
+			if(cfg.directory) {
+				this.uploadDialog.setAttribute('webkitdirectory', true);
+				this.uploadDialog.setAttribute('directory', true);
+			} else {
+				this.uploadDialog.removeAttribute('webkitdirectory');
+				this.uploadDialog.removeAttribute('directory');
+			}
+			this.uploadDialog.setAttribute('multiple', cfg.multiple||null);
+			
+			this.uploadDialog.setAttribute('accept', cfg.accept || '');
+			this.uploadDialog.onchange = function (e) {
+				if(cfg.listeners.select) { 
+					cfg.listeners.select.call(cfg.listeners.scope||this, this.files); 
+				}
+				if(!cfg.autoUpload) {
+					return
+				}
+				var uploadCount = this.files.length;
+				for (var i = 0; i < this.files.length; i++) {
+					go.Jmap.upload(this.files[i], {
+						success: function(response) {
+							if(cfg.listeners.upload) {
+								cfg.listeners.upload.call(cfg.listeners.scope||this, response);
+							}
+							uploadCount--;
+							if(uploadCount === 0 && cfg.listeners.uploadComplete) {
+								cfg.listeners.uploadComplete.call(cfg.listeners.scope||this);
+							}
+						}
+					});
+				}
+			};
+			this.uploadDialog.click();
 		}
 
 	};
