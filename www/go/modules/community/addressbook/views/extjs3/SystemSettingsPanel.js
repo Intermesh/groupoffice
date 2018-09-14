6c97ef98e30e9aacceed3ee1298e6ce0f2c2558b
Merijn Schering <mschering@intermesh.nl>
2018-09-14 15:54:39 +0200

    Cleanup

--- www/go/modules/community/addressbook/views/extjs3/SystemSettingsPanel.js
+++ www/go/modules/community/addressbook/views/extjs3/SystemSettingsPanel.js
@@ -5,7 +5,10 @@ go.modules.community.addressbook.SystemSettingsPanel = Ext.extend(Ext.Panel, {
 		this.title = t("Address book");		
 		
 		this.items = [new go.modules.core.customfields.SystemSettingsPanel({
+				// The entity it's for
 				entity: "Contact",
+
+				//Optionally override this function to customize the fieldset dialog.
 				createFieldSetDialog : function() {
 					return new go.modules.community.addressbook.CustomFieldSetDialog();
 				}
