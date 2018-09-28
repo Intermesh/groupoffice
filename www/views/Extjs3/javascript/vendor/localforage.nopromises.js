fb64fd2b61a8fb6013dc6aca5eec6c84f617a664
Merijn Schering <mschering@intermesh.nl>
2018-09-28 10:06:51 +0200

    Fixed exception on deleteDatabase

--- www/views/Extjs3/javascript/vendor/localforage.nopromises.js
+++ www/views/Extjs3/javascript/vendor/localforage.nopromises.js
@@ -3,6 +3,8 @@
     Version 1.7.1
     https://localforage.github.io/localForage
     (c) 2013-2017 Mozilla, Apache License 2.0
+
+Merijn: Manually applied PR https://github.com/localForage/localForage/pull/807/commits/55111683e3a2078f28956ba9172f5a69686fb36e
 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.localforage = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw (f.code="MODULE_NOT_FOUND", f)}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 'use strict';
