80189b3e479437abfd3fbf71272139c15f33a000
Merijn Schering <mschering@intermesh.nl>
2018-10-11 14:47:28 +0200

    Render issue

--- www/modules/email/AccountDialog.js
+++ www/modules/email/AccountDialog.js
@@ -470,6 +470,7 @@ GO.email.AccountDialog = function(config) {
 		items : [this.tabPanel = new Ext.TabPanel({
 			hideLabel : true,
 			deferredRender : false,
+			layoutOnTabChange: true,
 			activeTab : 0,
 			border : false,
 			anchor : '100% 100%',
@@ -525,8 +526,8 @@ GO.email.AccountDialog = function(config) {
 	GO.email.AccountDialog.superclass.constructor.call(this, {
 		layout : 'fit',
 		modal : false,
-		height: dp(550),
-		width : dp(958),
+		height: dp(616),
+		width : dp(1008),
 		closeAction : 'hide',
 		title : t("E-mail Account", "email"),
 
