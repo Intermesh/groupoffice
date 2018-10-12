ebc99e79c99bbfaf529a54b76d32e730810a2382
Merijn Schering <mschering@intermesh.nl>
2018-10-12 14:53:26 +0200

    Fixed Holidays choice Language #164

---
+++ www/go/modules/community/notes/language/zh_tw.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'Notes' => '記事本',
+  'A module to make to notes' => '輸入說明',
+  'General' => '普通',
+  'Notebook' => '分類',
+  'Notebooks' => '分類',
+  'Note' => '記事',
+  'category_id' => '分類 id',
+  'content' => '內容',
+  'manageCategories' => '管理分類',
+);
