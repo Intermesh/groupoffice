f1e61e6bd1f530aa159024a15e186b630469ef75
Merijn Schering <mschering@intermesh.nl>
2018-10-15 15:41:16 +0200

    cleaned up

--- www/go/core/acl/model/Acl.php
+++ www/go/core/acl/model/Acl.php
@@ -106,7 +106,7 @@ class Acl extends \go\core\jmap\Entity {
 	
 	private function hasAdmins() {
 		foreach($this->groups as $group) {
-			if($group->groupId == Group::ID_ADMINS) {
+			if($group->groupId == Group::ID_ADMINS) {				
 				return true;
 			}
 		}
