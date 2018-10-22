8fccea9719786cc0c7c866eab35461104c946ae9
Merijn Schering <mschering@intermesh.nl>
2018-10-22 13:48:03 +0200

    E-mail search function

--- www/go/modules/core/search/controller/Search.php
+++ www/go/modules/core/search/controller/Search.php
@@ -2,13 +2,57 @@
 
 namespace go\modules\core\search\controller;
 
-use go\core\jmap\Controller;
+use go\core\acl\model\Acl;
+use go\core\db\Query;
+use go\core\jmap\EntityController;
+use go\modules\community\addressbook\model\Contact;
+use go\modules\core\modules\model\Module;
 use go\modules\core\search\model;
 
-class Search extends \go\core\jmap\EntityController {
+class Search extends EntityController {
 
 	protected function entityClass() {
 		return model\Search::class;
+	}
+
+	public function email($params) {
+
+		$query = new Query();
+		$query->select('u.id, "User" as entity, u.email, "" as type, u.displayName AS name')
+						->from('core_user', 'u')
+						->join('core_group', 'g', 'u.id = g.isUserGroupFor');
+
+		if (isset($params['q'])) {
+			$query
+							->where('email', 'LIKE', $params['q'] . '%')
+							->orWhere('displayName', 'LIKE', $params['q'] . '%');
+		}
+
+		Acl::applyToQuery($query, 'g.aclId');
+
+		if (Module::isAvailableFor("community", "addressbook")) {
+
+			$contactsQuery = (new Query)
+							->select('c.id, "Contact" as entity, e.email, e.type, c.name')
+							->from("addressbook_contact", "c")
+							->join("addressbook_email_address", "e", "e.contactId=c.id");
+
+			Contact::applyAclToQuery($contactsQuery);
+
+			if (isset($params['q'])) {
+				$contactsQuery
+								->where('e.email', 'LIKE', $params['q'] . '%')
+								->orWhere('c.name', 'LIKE', $params['q'] . '%');
+			}
+
+			$query->union($contactsQuery)
+							->offset($params['position'] ?? 0)
+							->limit(20);
+		}
+		
+		\go\core\jmap\Response::get()->addResponse([
+				'list' => array_map(function($r) {$r['id'] = (int) $r['id']; return $r;}, $query->toArray())
+				]);
 	}
 
 }
