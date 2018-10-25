6e6d4c1a12f33c00f2fdf931ac5c85904de70d1b
Merijn Schering <mschering@intermesh.nl>
2018-10-25 15:30:41 +0200

    Pass entityStore as string Pass added and changed to onChanges as objects

--- www/views/Extjs3/javascript/go/panels/CreateModifyTpl.js
+++ www/views/Extjs3/javascript/go/panels/CreateModifyTpl.js
@@ -1,17 +1,18 @@
 go.panels.CreateModifyTpl = Ext.extend(Ext.Panel, {
+	entityStore: "User",
 	cUserId: null,
 	mUserId: null,
 	tpl: new Ext.XTemplate('<p class="s6 pad">\
 	<label>'+t("Created")+'</label>\
 	<span class="avatar" style="{[this.avatar(this.cUser)]}"></span>\
 	<span>{[fm.date(values.createdAt)]}{ctime}</span><br>\
-	<small>'+t("by")+' <span>{[values.username || this.cUser.displayName]}</span></small>\
+	<small>'+t("by")+' <span>{[this.cUser.displayName]}</span></small>\
 	</p>\
 	<p class="s6">\
 	<label>'+t("Modified")+'</label>\
 	<span class="avatar" style="{[this.avatar(this.mUser)]}"></span>\
 	<span>{[fm.date(values.modifiedAt)]}{mtime}</span><br>\
-	<small>'+t("by")+' <span>{[values.musername || this.mUser.displayName]}</span></small>\
+	<small>'+t("by")+' <span>{[this.mUser.displayName]}</span></small>\
 	</p>',{
 		avatar: function(user) {
 			if(!user) {
@@ -22,42 +23,22 @@ go.panels.CreateModifyTpl = Ext.extend(Ext.Panel, {
 		cUser: null,
 		mUser: null
 	}),
-	initComponent: function () {
-		go.panels.CreateModifyTpl.superclass.initComponent.call(this, arguments);		
 		
-		this.on("afterrender", function() {
-			go.Stores.get('User').on('changes', this.onChanges, this);
-			
-		}, this);
-		
-		this.on("destroy", function() {
-			go.Stores.get('User').un('changes', this.onChanges, this);
-		}, this);
-
-	},
-	
 	update: function(data) {
 		this.cUserId = data.createdBy || data.ownedBy || data.user_id;
 		this.mUserId = data.modifiedBy || data.muser_id;
-		go.Stores.get('User').get([this.cUserId,this.mUserId],function(entities) {
+		this.entityStore.get([this.cUserId,this.mUserId], function(entities) {
+			
 			this.tpl.cUser = entities[0] || {displayName: ''};
 			this.tpl.mUser = entities[1] || {displayName: ''};
+			
 			go.panels.CreateModifyTpl.superclass.update.call(this, data);
 		},this);	
 	},
 	
-	onChanges : function(store, added, changed) {
-		var ids = added.concat(changed),
-			 needToRender = false;
-		for(var i = 0, id; id = ids[i]; i++) {
-			if(id == this.cUserId || id == this.mUserId) {
-				needToRender = true;
-			}
+	onChanges : function(store, added, changed) {		
+		if(added[this.cUserId] || changed[this.cUserId] || added[this.mUserId] || changed[this.mUserId]) {		
+			this.update(this.ownerCt.data); 
 		}
-		if(needToRender) {
-			go.panels.CreateModifyTpl.superclass.update.call(this, this.ownerCt.data);
-			//this.update(this.ownerCt.data); 
-		}
-	}
-	
+	}	
 });
