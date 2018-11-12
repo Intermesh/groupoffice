5a23ed8c8916512da4a9ff7507d636aac6201e6b
tom <tom@intermesh-HPE-520nl>
2018-11-12 10:08:28 +0100

    prepull begin w12 commit

--- www/go/modules/community/pages/views/extjs3/SiteTree.js
+++ www/go/modules/community/pages/views/extjs3/SiteTree.js
@@ -1,5 +1,18 @@
 go.modules.community.pages.SiteTree = Ext.extend(Ext.tree.TreePanel,{
+    enableDD: false,
+    border: true,
+    useArrows: true,
+    root: {
+	nodeType: 'async',
+        text: 'Root',
+        draggable: false,
+        id: 'source',
+	expanded: true
+    },
+    
+	rootVisible: false,
     initComponent : function() {
+	go.modules.community.pages.SiteTree.superclass.initComponent.call(this);
     }
     
 })
