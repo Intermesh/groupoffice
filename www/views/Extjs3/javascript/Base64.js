294fafffc11fc76b0a90d5d1a782f47c5626a7e0
Michael de Hart <mdhart@intermesh.nl>
2018-11-15 09:49:08 +0100

    fixed base64 encode/decode: For email folder names with unicode characters

--- www/views/Extjs3/javascript/Base64.js
+++ www/views/Extjs3/javascript/Base64.js
@@ -1,16 +1,13 @@
-//deprecated, use atob and btoa
-
 GO.util.Base64 = {
  
 	// public method for encoding
-	encode : function (input) {
-		return btoa(input);
+	encode : function (str) {
+		return window.btoa(unescape(encodeURIComponent(str)));
 	},
  
 	// public method for decoding
-	decode : function (input) {
-		return atob(input);
- 
+	decode : function (str) {
+		return decodeURIComponent(escape(window.atob(str)));
+		//return atob(input);
 	}
- 
 }
