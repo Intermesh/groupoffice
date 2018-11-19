9ce33b179db7d158d7538fac20203bf190ffe3c2
Merijn Schering <mschering@intermesh.nl>
2018-11-19 16:39:07 +0100

    update queries

--- www/modules/email/install/install.sql
+++ www/modules/email/install/install.sql
@@ -261,3 +261,31 @@ CREATE TABLE IF NOT EXISTS `em_labels` (
   `default` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `email_template` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `ownedBy` int(11) DEFAULT NULL,
+ `aclId` int(11) NOT NULL,
+ `name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `subject` varchar(190) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+ `body` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `name` (`name`),
+ KEY `ownedBy` (`ownedBy`),
+ KEY `aclId` (`aclId`),
+ CONSTRAINT `email_template_ibfk_1` FOREIGN KEY (`ownedBy`) REFERENCES `core_user` (`id`) ON DELETE CASCADE,
+ CONSTRAINT `email_template_ibfk_2` FOREIGN KEY (`aclId`) REFERENCES `core_acl` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPACT;
+
+
+CREATE TABLE `email_template_attachment` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `templateId` int(11) NOT NULL,
+ `blobId` binary(40) NOT NULL,
+ PRIMARY KEY (`id`),
+ KEY `templateId` (`templateId`),
+ KEY `blobId` (`blobId`),
+ CONSTRAINT `email_template_attachment_ibfk_1` FOREIGN KEY (`blobId`) REFERENCES `core_blob` (`id`),
+ CONSTRAINT `email_template_attachment_ibfk_2` FOREIGN KEY (`templateId`) REFERENCES `email_template` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPACT;
