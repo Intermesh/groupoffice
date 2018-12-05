0caca87ee1dd6f4f170f38ec33745c9fce202033
Merijn Schering <mschering@intermesh.nl>
2018-12-05 09:37:41 +0100

    Support for icon and iconCls in navmenu

--- www/views/Extjs3/javascript/go/NavMenu.js
+++ www/views/Extjs3/javascript/go/NavMenu.js
@@ -1,3 +1,34 @@
+/**
+ * 
+ * Vertical navigation menu.
+ * 
+ * @example
+ * ```
+ * var filterPanel = new go.NavMenu({
+ * 		region:'north',
+ * 		store: new Ext.data.ArrayStore({
+ * 			fields: ['name', 'icon', 'inputValue'], //icon and iconCls are supported.
+ * 			data: [
+ * 				[t("Active", "tasks"), 'content_paste', 'active'],
+ * 				[t("Due in seven days", "tasks"), 'filter_7', 'sevendays'],
+ * 				[t("Overdue", "tasks"), 'schedule', 'overdue'],
+ * 				[t("Incomplete tasks", "tasks"), 'assignment_late', 'incomplete'],
+ * 				[t("Completed", "tasks"), 'assignment_turned_in', 'completed'],
+ * 				[t("Future tasks", "tasks"), 'assignment_return', 'future'],
+ * 				[t("All", "tasks"), 'assignment', 'all'],
+ * 			]
+ * 		}),
+ * 		listeners: {
+ * 			selectionchange: function(view, nodes) {	
+ * 				var record = view.store.getAt(nodes[0].viewIndex);
+ * 				this.gridPanel.store.baseParams['show']=record.data.inputValue;
+ * 				this.gridPanel.store.load();
+ * 			},
+ * 			scope: this
+ * 		}
+ * 	});
+ * ```
+ */
 go.NavMenu = Ext.extend(Ext.DataView,{
 	
 	initComponent: function() {
