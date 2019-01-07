805f3af7598473bdd9d55b6297b9b23071a9ab9b
Merijn Schering <mschering@intermesh.nl>
2019-01-07 16:47:35 +0100

    Don't run old upgrade

--- www/controller/MaintenanceController.php
+++ www/controller/MaintenanceController.php
@@ -382,7 +382,7 @@ class MaintenanceController extends AbstractController {
 		GO::setIgnoreAclPermissions(true);
 		GO::session()->runAsRoot();
 		
-		$this->run("upgrade",$params);		
+		//$this->run("upgrade",$params);		
 		
 		$this->lockAction();
 		
