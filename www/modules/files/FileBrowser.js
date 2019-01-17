d6f0abb0fe5db24d9e2c4a4d3979798a2478e146
Merijn Schering <mschering@intermesh.nl>
2019-01-17 16:53:44 +0100

    Files: Fixed global folder search
    Fixed Search Folder #287

--- www/modules/files/FileBrowser.js
+++ www/modules/files/FileBrowser.js
@@ -1926,6 +1926,15 @@ Ext.extend(GO.files.FileBrowser, Ext.Panel,{
 		{
 			this.on('filebrowserready', fn, scope);
 		}
+	},
+	
+	route: function(id, entity) {
+		
+		var detailViewName = entity + "Detail";
+		
+		this[detailViewName].load(parseInt(id));
+//		mainPanel[detailViewName].show();
+		this.eastPanel.getLayout().setActiveItem(this[detailViewName]);
 	}
 });
 
