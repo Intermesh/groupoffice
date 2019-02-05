52a06f0e738d26cc35e1c60182329c0625f4f2ef
Michael de Hart <mdhart@intermesh.nl>
2019-02-05 09:18:56 +0100

    fix column colored yes no

--- www/views/Extjs3/javascript/grids/ColumnRenderers.js
+++ www/views/Extjs3/javascript/grids/ColumnRenderers.js
@@ -11,13 +11,13 @@ GO.grid.ColumnRenderers = {
 	
 	coloredYesNo : function(val, meta, record, rowIndex, columnIndex, store){
 		if(val == 1){
-			meta.css = 'cellbg-green';
+			meta.style = 'color:green';
 			return t("Yes");
 		}else if(val == 0){
-			meta.css = 'cellbg-red';
+			meta.style = 'color:red';
 			return t("No");
 		}else{
-			meta.css = 'cellbg-blue';
+			meta.style = 'color:blue';
 			return val;
 		}
 	},
@@ -32,7 +32,7 @@ GO.grid.ColumnRenderers = {
 		}
 		
 		if(date < Math.round(new Date().now()/1000)){
-			meta.css = 'cellbg-red';
+			meta.style = 'color:red';
 			return t("No");
 		}
 	},
