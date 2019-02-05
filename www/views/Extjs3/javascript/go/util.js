90dc442485d52d5ca785ff0bb58a0f642d77ba95
wsmits <wsmits@wesley-werk>
2019-02-05 14:58:37 +0100

    Added humanFileSize function

--- www/views/Extjs3/javascript/go/util.js
+++ www/views/Extjs3/javascript/go/util.js
@@ -20,6 +20,29 @@
 	}
 
 	go.util = {
+		
+		/**
+		 * Convert bytes to a user readable format
+		 * 
+		 * @param int bytes
+		 * @param boolean conventionDecimal
+		 * @return {String}
+		 */
+		humanFileSize: function(bytes, conventionDecimal) {
+			 var thresh = conventionDecimal ? 1000 : 1024;
+			 if(Math.abs(bytes) < thresh) {
+				  return bytes + ' B';
+			 }
+			 var units = conventionDecimal
+				  ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+				  : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+			 var u = -1;
+			 do {
+				  bytes /= thresh;
+				  ++u;
+			 } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+			 return bytes.toFixed(1)+' '+units[u];
+		},
 
 		empty: function (v) {
 
