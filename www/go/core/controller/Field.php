5e64b578aba7dfb1580ee4f471569283065b0d59
Merijn Schering <mschering@intermesh.nl>
2019-02-12 15:55:34 +0100

    Removed readonly entity controller and created protecte default api methods

--- www/go/core/controller/Field.php
+++ www/go/core/controller/Field.php
@@ -24,4 +24,45 @@ class Field extends EntityController {
 	protected function getGetQuery($params) {
 		return parent::getGetQuery($params)->orderBy(['sortOrder' => 'ASC']);
 	}
+	
+	/**
+	 * Handles the Foo entity's Foo/query command
+	 * 
+	 * @param array $params
+	 * @see https://jmap.io/spec-core.html#/query
+	 */
+	public function query($params) {
+		return $this->defaultQuery($params);
+	}
+	
+	/**
+	 * Handles the Foo entity's Foo/get command
+	 * 
+	 * @param array $params
+	 * @see https://jmap.io/spec-core.html#/get
+	 */
+	public function get($params) {
+		return $this->defaultGet($params);
+	}
+	
+	/**
+	 * Handles the Foo entity's Foo/set command
+	 * 
+	 * @see https://jmap.io/spec-core.html#/set
+	 * @param array $params
+	 */
+	public function set($params) {
+		return $this->defaultSet($params);
+	}
+	
+	
+	/**
+	 * Handles the Foo entity's Foo/changes command
+	 * 
+	 * @param array $params
+	 * @see https://jmap.io/spec-core.html#/changes
+	 */
+	public function changes($params) {
+		return $this->defaultChanges($params);
+	}
 }
--- www/go/modules/community/tasks/controller/TaskList.php
+++ www/go/modules/community/tasks/controller/TaskList.php
@@ -16,4 +16,45 @@ class TaskList extends EntityController {
 	protected function entityClass() {
 		return model\TaskList::class;
 	}	
+	
+	/**
+	 * Handles the Foo entity's Foo/query command
+	 * 
+	 * @param array $params
+	 * @see https://jmap.io/spec-core.html#/query
+	 */
+	public function query($params) {
+		return $this->defaultQuery($params);
+	}
+	
+	/**
+	 * Handles the Foo entity's Foo/get command
+	 * 
+	 * @param array $params
+	 * @see https://jmap.io/spec-core.html#/get
+	 */
+	public function get($params) {
+		return $this->defaultGet($params);
+	}
+	
+	/**
+	 * Handles the Foo entity's Foo/set command
+	 * 
+	 * @see https://jmap.io/spec-core.html#/set
+	 * @param array $params
+	 */
+	public function set($params) {
+		return $this->defaultSet($params);
+	}
+	
+	
+	/**
+	 * Handles the Foo entity's Foo/changes command
+	 * 
+	 * @param array $params
+	 * @see https://jmap.io/spec-core.html#/changes
+	 */
+	public function changes($params) {
+		return $this->defaultChanges($params);
+	}
 }
