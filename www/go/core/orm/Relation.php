639e9e1a248a218328833fba963a5c8103a6be41
Merijn Schering <mschering@intermesh.nl>
2019-03-05 11:46:11 +0100

    Add validation to Mapping::addRelation() so developers can't map entities.

--- www/go/core/orm/Relation.php
+++ www/go/core/orm/Relation.php
@@ -48,7 +48,7 @@ class Relation {
 	 * Constructor
 	 * 
 	 * @param string $name The name of the relation
-	 * @param string $entityName The class name of the {@see Property} or {@see Entity} this relation points to.
+	 * @param string $entityName The class name of the {@see Property} this relation points to.
 	 * @param array $keys Associative array with key map
 	 * ```
 	 * ['fromColumn' => 'toColumn']
@@ -58,6 +58,15 @@ class Relation {
 	 */
 	public function __construct($name, $entityName, array $keys, $many = false) {
 		$this->name = $name;
+		
+		if(!is_subclass_of($entityName, Property::class, true)) {
+			throw new \Exception($entityName . ' must extend '. Property::class);
+		}
+		
+		if(is_subclass_of($entityName, Entity::class, true)) {
+			throw new \Exception($entityName . ' may not be an '. Entity::class .'. Only '. Property::class .' objects can be mapped.');
+		}
+		
 		$this->entityName = $entityName;
 		$this->keys = $keys;
 		$this->many = $many;
