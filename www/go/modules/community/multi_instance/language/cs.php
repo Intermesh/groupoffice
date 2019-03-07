57fda5714ded0a37ce824b1e6ef6b0f65a180d98
Merijn Schering <mschering@intermesh.nl>
2019-03-07 12:07:45 +0100

    Updated Czech

---
+++ www/go/modules/community/multi_instance/language/cs.php
@@ -0,0 +1,14 @@
+<?php
+return array (
+  'Last login' => 'Poslední přihlášení',
+  'User count' => 'Počet uživatelů',
+  'Maximum users' => 'Max. uživatelů',
+  'Login count' => 'Počet přihlášení',
+  'Admin name' => 'Jméno administrátora',
+  'Admin E-mail' => 'E-mail administrátora',
+  'Storage usage' => 'Využití uložiště',
+  'Login as administrator' => 'Přihlásit jako administrátor',
+  'Deactivate' => 'Deaktivovat',
+  'Deactivate instance' => 'Deaktivovat instanci',
+  'Activate instance' => 'Aktivovat instanci',
+);
