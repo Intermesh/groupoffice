c6ea7a71a109b3c121c003bf3aa477e890c558ca
Merijn Schering <mschering@intermesh.nl>
2019-04-04 12:03:33 +0200

    updated french

---
+++ www/modules/groups/language/fr.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+  'group' => 'groupe',
+  'adminOnlyLabel' => 'adminOnlyLabel',
+  'use' => 'utilisation',
+  'managePermissions' => 'managePermissions',
+  'modulePermissions' => 'modulePermissions',
+  'groups' => 'groupes',
+);
