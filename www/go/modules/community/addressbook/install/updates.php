57b52571ab566bb25cd75fcb37b77250ab766782
Merijn Schering <mschering@intermesh.nl>
2019-04-12 08:25:08 +0200

    Remove comment

--- www/go/modules/community/addressbook/install/updates.php
+++ www/go/modules/community/addressbook/install/updates.php
@@ -18,13 +18,3 @@ $updates['201904021547'][] = "CREATE TABLE IF NOT EXISTS `addressbook_user_setti
   KEY `defaultAddressBookId` (`defaultAddressBookId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPACT;";
 
-//$updates['201901191547'][] = "ALTER TABLE `addressbook_user_settings`
-//  ADD CONSTRAINT `addressbook_user_settings_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `core_user` (`id`) ON DELETE CASCADE,
-//  ADD CONSTRAINT `addressbook_user_settings_ibfk_2` FOREIGN KEY (`defaultAddressBookId`) REFERENCES `addressbook_addressbook` (`id`) ON DELETE SET NULL;";
-
-
-
-
-
-
-// bestgroen ab -4 in core_module !
