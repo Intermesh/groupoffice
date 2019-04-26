5754959d175b9fa603a451d77801f658f9a3c4e0
Merijn Schering <mschering@intermesh.nl>
2019-04-26 14:26:53 +0200

    Fixed Generate document #351

--- www/go/vendor/odtphp/library/zip/pclzip/pclzip.lib.php
+++ www/go/vendor/odtphp/library/zip/pclzip/pclzip.lib.php
@@ -1963,7 +1963,8 @@
     $v_memory_limit = ini_get('memory_limit');
     $v_memory_limit = trim($v_memory_limit);
     $last = strtolower(substr($v_memory_limit, -1));
- 
+    $v_memory_limit = substr($v_memory_limit, 0, -1);
+
     if($last == 'g')
         //$v_memory_limit = $v_memory_limit*1024*1024*1024;
         $v_memory_limit = $v_memory_limit*1073741824;
