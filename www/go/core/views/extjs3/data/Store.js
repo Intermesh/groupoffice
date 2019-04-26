04d11dd9f8084a6f898590fec9e89ba46491d82b
Merijn Schering <mschering@intermesh.nl>
2019-04-26 11:02:20 +0200

    Created StoreTrait for Store and grouping store

--- www/go/core/views/extjs3/data/Store.js
+++ www/go/core/views/extjs3/data/Store.js
@@ -17,30 +17,19 @@
 						store.commitChanges();
  */
 go.data.Store = Ext.extend(Ext.data.JsonStore, {
-	
-	entityStore: null,
-	
+
 	autoDestroy: true,
 	
 	autoSave: false,
-	//writer: {}, // needed for buggy Ext store changes 
-	
-	/**
-	 * true when load or loaddata has been called.
-	 * 
-	 * @var bool
-	 */
-	loaded : false,
-	
-	loading : false,
 	
 	remoteSort : true,
-	
 	
 	constructor: function (config) {
 		
 		config = config || {};
 		config.root = "records";
+
+		Ext.applyIf(this, go.data.StoreTrait);
 		
 		this.addCustomFields(config);
 		
@@ -53,36 +42,9 @@ go.data.Store = Ext.extend(Ext.data.JsonStore, {
 				dir: 'dir'       // The parameter name which specifies the sort direction
 			},
 			proxy: config.entityStore ? new go.data.EntityStoreProxy(config) : new go.data.JmapProxy(config)
-		}));
+		}));        
 		
-
-		this.on({
-			 scope: this,
-			 remove: this.destroyRecord, // will remember destoyed records
-			 //clear: this.onClear
-		});
-        
-		
-		this.setup();
-		
-		//JMAP remote filters. Used by setFilter()
-		this.filters = {};
-	},
-	
-	addCustomFields : function(config) {
-		if(!config.entityStore) {
-			return;
-		}
-		
-		var entity; 
-		if(Ext.isString(config.entityStore)) {
-			entity = go.Entities.get(config.entityStore);
-		} else
-		{
-			entity = config.entityStore.entity;
-		}
-		
-		config.fields = config.fields.concat(go.customfields.CustomFields.getFieldDefinitions(entity.name));	
+		this.setup();		
 	},
 	
 	loadData : function(o, append){
@@ -111,174 +73,6 @@ go.data.Store = Ext.extend(Ext.data.JsonStore, {
 		return go.data.Store.superclass.sort.call(this, fieldName, dir);
 	},
 	
-	//created this because grouping store must share this.
-	setup : function() {
-		if(!this.baseParams) {
-			this.baseParams = {};
-		}
-		
-		if(!this.baseParams.filter) {
-			this.baseParams.filter = {};
-		}		
-		
-		if(this.entityStore) {			
-			this.initEntityStore();
-		}
-		
-		// It's important to realize that if other components register an on load 
-		// listener this.loading will be false. Because this is the first event listener.
-		// ScrollLoader depends on this!
-		this.on("load", function() {
-			this.loading = false;
-			this.loaded = true;
-		}, this);
-	},
-	
-	
-	load : function(params) {
-		this.loading = true;	
-		
-		return go.data.Store.superclass.load.call(this, params);		
-		
-	},
-	
-	initEntityStore : function() {
-		if(Ext.isString(this.entityStore)) {
-			this.entityStore = go.Db.store(this.entityStore);
-			if(!this.entityStore) {
-				throw "Invalid 'entityStore' property given to component"; 
-			}
-		}
-		this.entityStore.on('changes',this.onChanges, this);		
-		
-		//reload if something goes wrong in the entity store.
-		this.entityStore.on("error", this.onError, this);
-
-		this.on('beforedestroy', function() {
-			this.entityStore.un('changes', this.onChanges, this);
-			this.entityStore.un("error", this.onError, this);
-			this.unwatchRelations();
-		}, this);
-
-		this.watchRelations();
-	},
-
-	/**
-	 * Registers a listener for changes on entity stores when fields with type = relation is used.
-	 */
-	watchRelations : function() {
-
-		this.proxy.getEntityFields().forEach(function(field) {
-			var relation =  this.entityStore.entity.findRelation(field.name);
-			go.Db.store(relation.store).on("changes", this.onRelationChanges, this);
-		}, this);
-	},
-
-	unwatchRelations : function() {
-		this.proxy.getEntityFields().forEach(function(field) {
-			var relation = this.entityStore.entity.findRelation(field.name);
-
-			go.Db.store(relation.store).un("changes", this.onRelationChanges, this);
-		}, this);
-	},
-	
-	onError : function() {
-		this.reload();
-	},
-
-	/**
-	 * Reloads grid when a relation that is present in the grid has changed
-	 * 
-	 * @param {*} entityStore 
-	 * @param {*} added 
-	 * @param {*} changed 
-	 * @param {*} destroyed 
-	 */
-	onRelationChanges : function(entityStore, added, changed, destroyed) {
-		if(!this.proxy.watchRelations[entityStore.entity.name]) {
-			return;
-		}
-
-		for(var id in changed) {
-			if(this.proxy.watchRelations[entityStore.entity.name].indexOf(changed[id].id) > -1) {
-				var o = go.util.clone(this.lastOptions);
-				o.params = o.params || {};
-				o.params.position = 0;
-				o.params.limit = this.getCount();
-
-				this.load(o);
-				return;
-			}
-		}
-		
-	},
-
-	onChanges : function(entityStore, added, changed, destroyed) {		
-
-		if(!this.loaded || this.loading) {
-			return;
-		}		
-		
-
-		if(Object.keys(added).length || Object.keys(changed).length) {
-			//we must reload because we don't know how to sort partial data.
-			var o = go.util.clone(this.lastOptions);
-			o.params = o.params || {};
-			o.params.position = 0;
-			o.params.limit = this.getCount();
-			this.load(this.lastOptions);
-		}
-		
-//		for(var i in added) {
-//			if(!this.updateRecord(added[i]) ){
-//				this.reload();
-//				return;
-//			}
-//		}
-//				
-//		for(var i in changed) {
-//			if(!this.updateRecord(changed[i]) ){
-//				this.reload();
-//				return;
-//			}
-//		}
-		
-		for(var i = 0, l = destroyed.length; i < l; i++) {
-			var record = this.getById(destroyed[i]);
-			if(record) {
-				this.remove(record);
-			}
-		}
-
-	},
-	
-	updateRecord : function(entity) {
-		
-		if(!this.data) {
-			return false;
-		}
-		var record = this.getById(entity.id);
-		if(!record) {
-			return false;
-		}
-		
-
-//		if(record.isModified()) {
-//			alert("Someone modified your record!");
-//		}
-		
-		record.beginEdit();
-		this.fields.each(function(field) {
-			if(field.name in entity) {
-				record.set(field.name, entity[field.name]);
-			}
-		});		
-		
-		record.endEdit();
-		record.commit();
-		
-		return true;
-	},
 	destroy : function() {	
 		this.fireEvent('beforedestroy', this);
 		
@@ -287,21 +81,7 @@ go.data.Store = Ext.extend(Ext.data.JsonStore, {
 		this.fireEvent('destroy', this);
 	},
 	
-	/**
-	 * Load entities by id
-	 * 
-	 * @param {int[]} ids
-	 * @returns {void}
-	 */
-	loadByIds : function(ids) {
-		this.entityStore.get(ids, function (items) {
-			this.loadData(items);
-		}, this);
-	},
 	
-	removeById : function(id) {
-		this.remove(this.getById(id));
-	},
 	
 	//override Extjs writer save for entityStore
 	save: function(cb) {
@@ -341,96 +121,7 @@ go.data.Store = Ext.extend(Ext.data.JsonStore, {
 		}
 	},
 	
-	/**
-	 * Set a filter object for a component
-	 * 
-	 * @param {string} cmpId
-	 * @param {object} filter
-	 * @returns {this}
-	 */
-	setFilter : function(cmpId, filter) {
-		if(filter === null) {
-			delete this.filters[cmpId];
-		} else
-		{
-			this.filters[cmpId] = filter;
-		}		
-		
-		this.baseParams.filter = {
-			operator: "AND",
-			conditions: []
-		};
-		
-		for(var cmpId in this.filters) {
-			this.baseParams.filter.conditions.push(this.filters[cmpId]);
-		}
-		
-		return this;
-	},
-	
-	getFilter : function() {
-		return this.baseParams.filter;
-	}
-	
-//	onUpdate : function(store, record, operation) {
-//		//debugger;
-//		if(this.serverUpdate || this.loading) {
-//			return;
-//		}
-//	
-//		if(operation != Ext.data.Record.COMMIT) {
-//			return;
-//		}
-//		
-//		var p = {};
-//		
-//		key = record.phantom ? 'create' : 'update';
-//		p[key] = {};
-//		p[key][record.id] = record.data;
-//		
-//		store.fields.each(function(field){
-//			if(field.submit === false) {
-//				delete record.data[field.name];
-//			}
-//		});
-//		
-//		this.entityStore.set(p, function (options, success, response) {
-//			
-//			var saved = (record.phantom ? response.created : response.updated) || {};
-//			if (saved[record.id]) {
-//
-//				//update client id with server id
-//				if(record.phantom) {
-////					record.id = record.data.id = response.created[record.id].id;
-////					console.log(record.id);
-////					record.phantom = false;
-//						//remove phanto records as ext doesn't support changinhg record id.
-//						this.remove(record);
-//				}
-//
-//			} else
-//			{
-//				//something went wrong
-//				var notSaved = (record.phantom ? response.notCreated : response.notUpdated) || {};
-//				if (!notSaved[id]) {
-//					notSaved[id] = {type: "unknown"};
-//				}
-//
-//				switch (notSaved[id].type) {
-//					case "forbidden":
-//						Ext.MessageBox.alert(t("Access denied"), t("Sorry, you don't have permissions to update this item"));
-//						break;
-//
-//					default:
-//					
-//						
-//						Ext.MessageBox.alert(t("Error"), t("Sorry, something went wrong. Please try again."));
-//						break;
-//				}
-//			}
-//		}, this);
-//		
-//	}
+
 });
 
 Ext.reg('gostore', go.data.Store);
