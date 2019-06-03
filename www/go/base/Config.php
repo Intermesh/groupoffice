183e206ab4f3e8bdc14aea769e210e2000c7f156
Merijn Schering <mschering@intermesh.nl>
2019-06-03 10:32:29 +0200

    6.2.118

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -1059,7 +1059,7 @@ var $billing_clear_payment_method_on_duplicate = true;
 	 * @var     StringHelper
 	 * @access  public
 	 */
-	var $version = '6.2.117';
+	var $version = '6.2.118';
 
 	/**
 	 * Modification date
@@ -1068,7 +1068,7 @@ var $billing_clear_payment_method_on_duplicate = true;
 	 * @access  public
 	 */
 	
-	var $mtime = '20190425';
+	var $mtime = '20190603';
 
 	#group configuration
 	/**
