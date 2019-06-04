4e50d65ea112093fb96365715a97f6aac4ee7df0
Merijn Schering <mschering@intermesh.nl>
2019-06-04 10:17:22 +0200

    Fixed upgrade

--- www/go/modules/community/bookmarks/controller/Bookmark.php
+++ www/go/modules/community/bookmarks/controller/Bookmark.php
@@ -160,7 +160,8 @@ class Bookmark extends EntityController {
 	 */
 	public static function updateLogos() {
 
-		$results = GO()->getDbConnection()->select("*")->from("bm_bookmarks");
+		$results = GO()->getDbConnection()->select("*")->from("bm_bookmarks")->where('category_id IN (select id from bookmarks_category)');
+
 		foreach($results as $result) {
 			$data = [];
 			$data['id'] = $result["id"];
