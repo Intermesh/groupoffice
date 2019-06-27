07f9a5d4c00cadf30daf103927a5e4769a62c3c5
Merijn Schering <mschering@intermesh.nl>
2019-06-27 11:23:29 +0200

    Prefer filename for IMAP attachment over content type name

--- www/modules/email/model/ImapMessage.php
+++ www/modules/email/model/ImapMessage.php
@@ -611,9 +611,9 @@ class ImapMessage extends ComposerMessage {
 				} else {
 					$a->name = \GO\Base\Mail\Utils::mimeHeaderDecode($part['name']);
 					
-					$extension = \GO\Base\Fs\File::getExtension($a->name);
-					if(!empty($part['filename']) && empty($extension)){
-						$a->name = \GO\Base\Mail\Utils::mimeHeaderDecode($part['filename']);
+					//$extension = \GO\Base\Fs\File::getExtension($a->name);
+					if(!empty($part['filename'])){//} && empty($extension)){
+						$a->name = \GO\Base\Fs\File::stripInvalidChars(\GO\Base\Mail\Utils::mimeHeaderDecode($part['filename']));
 					}
 				}
 				
