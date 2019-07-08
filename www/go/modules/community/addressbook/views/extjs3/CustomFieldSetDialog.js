2e665476af062a86491d1e89b3a144ce4fdb4b81
Merijn Schering <mschering@intermesh.nl>
2019-07-08 21:34:17 +0200

    removed debugger

--- www/go/modules/community/addressbook/views/extjs3/CustomFieldSetDialog.js
+++ www/go/modules/community/addressbook/views/extjs3/CustomFieldSetDialog.js
@@ -54,8 +54,6 @@ go.modules.community.addressbook.CustomFieldSetDialog = Ext.extend(go.customfiel
 		if (!this.formPanel.values.filter) {
 			this.formPanel.values.filter = {};
 		}
-
-		debugger;
 		
 		switch (this.formPanel.getForm().findField('enableFor').getValue()) {
 			case 'contacts':
