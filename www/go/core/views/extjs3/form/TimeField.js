35b2d385e1dd0bee543abafd1156640cd7424c95
Merijn Schering <mschering@intermesh.nl>
2019-07-08 13:26:49 +0200

    Fix for safari

--- www/go/core/views/extjs3/form/TimeField.js
+++ www/go/core/views/extjs3/form/TimeField.js
@@ -2,6 +2,19 @@ go.form.TimeField = Ext.extend(Ext.form.TextField, {
 	width: dp(72),
 	defaultAutoCreate : {tag: 'input', type: 'time', size: '20', autocomplete: 'off'},
 
+	initComponent: function() {
+		go.form.TimeField.superclass.initComponent.call(this);
+
+		if(Ext.isSafari) {
+			this.on("blur", function() {
+				var v = this.getRawValue();
+				if(v.indexOf(':') === -1) {
+					this.setValue(v + ':00');
+				}
+			}, this);
+		}
+	},
+
 	setMinutes: function(minutes) {
 		var duration = go.util.Format.duration(minutes);
 		if(duration.length < 5) {
@@ -9,8 +22,17 @@ go.form.TimeField = Ext.extend(Ext.form.TextField, {
 		}
 		this.setRawValue(duration);
 	},
-	getMinutes() {
+
+	getMinutes: function() {
 		return go.util.Format.minutes(this.getRawValue());
+	},
+
+	setValue : function(v) {
+		var parts = v.split(":");
+		if(parts.length == 3) {
+			parts.pop(); //pop seconds
+		}
+		go.form.TimeField.superclass.setValue.call(this, parts.join(":"));
 	},
 
 	getValue: function() {
@@ -21,7 +43,7 @@ go.form.TimeField = Ext.extend(Ext.form.TextField, {
 		if(v.length > 5) {
 			return v;
 		}
-		return v+':00'; // add some second to match mysql time field 
+		return v + ':00'; // add some second to match mysql time field 
 	}
 });
 
