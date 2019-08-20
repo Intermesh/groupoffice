4ddbce692bab55b383210e42403a413af75b7367
Merijn Schering <mschering@intermesh.nl>
2019-08-20 15:44:55 +0200

    GO()->getDbConnection()->exec("SET innodb_stats_on_metadata = 0");

--- www/go/core/jmap/Entity.php
+++ www/go/core/jmap/Entity.php
@@ -344,6 +344,7 @@ abstract class Entity  extends OrmEntity {
 
 			$dbName = GO()->getDatabase()->getName();
 			GO()->getDbConnection()->exec("USE information_schema");
+			GO()->getDbConnection()->exec("SET innodb_stats_on_metadata = 0");
 			//somehow bindvalue didn't work here
 			$sql = "SELECT `TABLE_NAME` as `table`, `COLUMN_NAME` as `column` FROM `KEY_COLUMN_USAGE` where ".
 				"constraint_schema=" . GO()->getDbConnection()->getPDO()->quote($dbName) . 
