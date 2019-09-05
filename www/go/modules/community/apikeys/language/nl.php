7542ce87625909409f44217ed98d1c024dcc8adb
Merijn Schering <mschering@intermesh.nl>
2019-09-05 10:48:04 +0200

    Updated NL

---
+++ www/go/modules/community/apikeys/language/nl.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+  'API Keys' => 'API Sleutels',
+  'API keys can be used for other services to connect to the API. A website feeding contact information for example.' => 'API Sleutels kunnen gebruikt worden door andere services om verbinding te maken. Een website bijvoorbeeld.',
+  'API Key' => 'API Sleutel',
+  'Add key' => 'Sleutel toevoegen',
+  'View access token' => 'Bekijk access token',
+);
--- www/go/modules/community/comments/language/nl.php
+++ www/go/modules/community/comments/language/nl.php
@@ -3,4 +3,5 @@ return array (
   'Upload file' => 'Upload bestand',
   'Add Link' => 'Link toevoegen',
   '{author} wrote at {date}' => '{author} schreef op {date}',
+  'Unknown user' => 'Onbekende gebruiker',
 );
