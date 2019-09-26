52b3254044d30ec5dcb7cfd53c809fd0689ec8fa
Merijn Schering <mschering@intermesh.nl>
2019-09-26 09:05:41 +0200

    Fix for address book not loading

--- www/go/modules/community/addressbook/views/extjs3/ContactGrid.js
+++ www/go/modules/community/addressbook/views/extjs3/ContactGrid.js
@@ -68,15 +68,15 @@ go.modules.community.addressbook.ContactGrid = Ext.extend(go.grid.GridPanel, {
 							if(sortState.field != "name" && sortState.field != "firstName"  && sortState.field != "lastName") {
 								return "";
 							}
-							
-							var lastRecord = rowIndex > 0 ? grid.store.getAt(rowIndex - 1) : false;
-							var lastSortBy = !lastRecord || !lastRecord.data.isOrganization ? go.User.addressBookSettings.sortBy : "name" ;
 
 							//sometimes the field is null.
 							if(!Ext.isString(record.data[sortBy])) {
 								return "";
 							}
 							
+							var lastRecord = rowIndex > 0 ? grid.store.getAt(rowIndex - 1) : false;
+							var lastSortBy = !lastRecord || !lastRecord.data.isOrganization ? go.User.addressBookSettings.sortBy : "name" ;						
+
 							var char = record.data[sortBy].substr(0, 1).toUpperCase();
 							if(!lastRecord || lastRecord.data[lastSortBy].substr(0, 1).toUpperCase() !== char) {
 								return "<h3>" + char + "</h3>";
