1794e3fd1c87dd66fe31bce0b2b1724a32fbc9c9
Merijn Schering <mschering@intermesh.nl>
2019-10-10 12:09:03 +0200

    Updated italian

---
+++ www/go/modules/community/googleauthenticator/language/it.php
@@ -0,0 +1,18 @@
+<?php
+return array (
+  'Enable Google authenticator' => 'Abilita Google Authenticator',
+  'Secret' => 'Segreta',
+  'Secret key for manual input' => 'Chiave Segreta per inserimento manuale',
+  'Verify' => 'verifica',
+  'Google authenticator' => 'Google Authenticator',
+  'Enable google authenticator' => 'Abilita google authenticator',
+  'Disable google authenticator' => 'Disabilita google authenticator ',
+  'When disabling Google autenticator this step will be removed from the login process.' => 'Disabilitando Google authenticator questo step verrà rimosso dal processo di login.',
+  'Provide your current password to disable Google authenticator.' => 'Inserisci la password corrente per disabilitare Google authenticator.',
+  'Disable Google authenticator' => 'Disabilita Google authenticator ',
+  'Provide your current password before you can enable Google authenticator.' => 'Inserisci la password corrente prima di abilitare Google authenticator.',
+  'Get the code from the google authenticator app on your mobile device and fill it in below' => 'Prendi il codice dalla tua app di google authenticator sul tuo dispositivo mobile and inseriscilo qui sotto',
+  'Code' => 'Codice',
+  'Code is required' => 'E\' richiesto il codice',
+  'Invalid code' => 'Codice non valido',
+);
