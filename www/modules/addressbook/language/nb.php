bb349633e15659b551b02a53dfa0b93f4ae73f04
Merijn Schering <mschering@intermesh.nl>
2019-10-21 09:50:33 +0200

    Updated Norwegian translation

---
+++ www/modules/addressbook/language/nb.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+  'Private address' => 'Stjerne',
+  'Primary' => 'Primær',
+  'Mobile Work' => 'Mobil arbeid',
+  'Fax' => 'Fax',
+  'Work fax' => 'Fax arbeid',
+  'Company details' => 'Firmaopplysninger',
+  'Remark' => 'Merknad',
+  'Edit user' => 'Rediger bruker',
+);
