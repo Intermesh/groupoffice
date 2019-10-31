b7f964fac24f35915c5910f79d814544b59af38a
Merijn Schering <mschering@intermesh.nl>
2019-10-31 13:51:12 +0100

    Core: Updated Spanish translation

---
+++ www/modules/reminders/language/es.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+  'Popup reminder' => 'Recordatorio emergente',
+  'Add users' => 'Agregar usuarios',
+  'Add user groups' => 'Agregar grupos de usuarios',
+  'Snooze time' => 'Tiempo de repetición',
+  'Popup reminders' => 'Recordatorios emergentes',
+);
