f0bac69c79df4aa7fec34fc937f5154967a5f70b
Merijn Schering <mschering@intermesh.nl>
2019-11-01 14:57:16 +0100

    wrong query

--- www/go/modules/community/multi_instance/cron/DeactivateTrials.php
+++ www/go/modules/community/multi_instance/cron/DeactivateTrials.php
@@ -1,7 +1,7 @@
 <?php
 namespace go\modules\community\multi_instance\cron;
 
-use go\core\db\Query;
+use go\core\orm\Query;
 use go\core\util\DateTime;
 use go\modules\community\multi_instance\model\Instance;
 use go\core\model\CronJob;
