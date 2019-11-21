9369ee068efcd0e2fe1e08f44d5e6010412d82c9
Merijn Schering <mschering@intermesh.nl>
2019-11-21 10:06:56 +0100

    Merged two html editors

--- www/views/Extjs3/javascript/form/HtmlEditor.js
+++ www/views/Extjs3/javascript/form/HtmlEditor.js
@@ -41,6 +41,55 @@ GO.form.HtmlEditor = function (config) {
 
 Ext.extend(GO.form.HtmlEditor, Ext.form.HtmlEditor, {
 
+	iframePad:0,
+	
+	hideToolbar: false,
+
+	initComponent: function() {
+		GO.form.HtmlEditor.superclass.initComponent.apply(this);
+		
+		this.on('initialize', function(){
+			if(this.hideToolbar) {
+				this.tb.hide();
+			}
+			if(Ext.isEmpty(this.emptyText)) {
+				return;
+			}
+			// Ext.EventManager.on(this.getEditorBody(),{
+			// 	focus:this.handleEmptyText,
+			// 	blur:this.applyEmptyText,
+			// 	scope:this
+			// });
+			
+		},this);
+	},
+
+	emptyTextRegex: '<span[^>]+[^>]*>{0}<\/span>',
+   emptyTextTpl: '<span style="color:#ccc;">{0}</span>',
+   emptyText: '',
+	
+	
+
+	// applyEmptyText: function() {
+	// 	var value = this.getValue();
+	// 	if(Ext.isEmpty(value)) {
+	// 		var emptyText = go.util.Format.string(this.emptyTextTpl,this.emptyText);
+	// 		go.form.HtmlEditor.superclass.setValue.apply(this, [emptyText]);
+	// 	}
+	// },
+	// handleEmptyText: function() {
+	// 	var value = this.getValue(),
+	// 		regex = new RegExp(go.util.Format.string( this.emptyTextRegex,this.emptyText ) );
+	// 	if(!Ext.isEmpty(value) && regex.test(value)) {
+	// 		go.form.HtmlEditor.superclass.setValue.apply(this, ['']);
+	// 	}
+	// },
+	// setValue : function(v){
+	// 	go.form.HtmlEditor.superclass.setValue.apply(this, arguments);
+	// 	//this.applyEmptyText();
+	// 	return this;
+  //  },
+
 	initEditor: function () {
 
 		GO.form.HtmlEditor.superclass.initEditor.call(this);
@@ -518,7 +567,32 @@ Ext.extend(GO.form.HtmlEditor, Ext.form.HtmlEditor, {
 				this.relayCmd("createlink", url);
 			}
 		}
-	}
+	},
+
+	setDesignMode : function(readOnly){
+		this.getEditorBody().contentEditable = readOnly;
+   },
+	
+	onResize : function(w, h){
+	  Ext.form.HtmlEditor.superclass.onResize.apply(this, arguments);
+	  if(this.el && this.iframe){
+			if(Ext.isNumber(w)){
+				 var aw = w - this.wrap.getFrameWidth('lr');
+				 this.el.setWidth(aw);
+				 this.tb.setWidth(aw);
+				 this.iframe.style.width = Math.max(aw, 0) + 'px';
+			}
+			if(Ext.isNumber(h)){
+				 var ah = h - this.wrap.getFrameWidth('tb') - this.tb.el.getHeight();
+				 this.el.setHeight(ah);
+				 this.iframe.style.height = Math.max(ah, 0) + 'px';
+				 var bd = this.getEditorBody();
+				 if(bd){
+					 // bd.style.height = Math.max((ah - (this.iframePad*2)), 0) + 'px';
+				 }
+			}
+		}
+   }
 
 });
 
