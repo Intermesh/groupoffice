54f9f91dd35dcbe1a304e7ea3adb8330162fb6c2
Merijn Schering <mschering@intermesh.nl>
2019-12-12 11:04:52 +0100

    Fixed unknown recipients

--- www/go/core/model/User.php
+++ www/go/core/model/User.php
@@ -3,12 +3,9 @@
 namespace go\core\model;
 
 use Exception;
-use GO;
 use GO\Base\Model\AbstractUserDefaultModel;
 use GO\Base\Model\User as LegacyUser;
 use GO\Base\Util\Http;
-use go\core\acl\model\AclItemEntity;
-use go\core\model\Acl;
 use go\core\App;
 use go\core\auth\Method;
 use go\core\auth\Password;
@@ -18,15 +15,12 @@ use go\core\db\Criteria;
 use go\core\orm\Query;
 use go\core\exception\Forbidden;
 use go\core\jmap\Entity;
-use go\core\model\Module;
 use go\core\orm\CustomFieldsTrait;
 use go\core\util\DateTime;
 use go\core\validate\ErrorCode;
-use go\core\model\Group;
-use go\core\model\Settings;
 
 
-class User extends AclItemEntity {
+class User extends Entity {
 	
 	use CustomFieldsTrait;
 
@@ -196,6 +190,8 @@ class User extends AclItemEntity {
 	
 	protected $last_password_change;
 	public $force_password_change;
+
+	protected $permissionLevel;
 	
 	
 	public function getDateTimeFormat() {
@@ -235,12 +231,13 @@ class User extends AclItemEntity {
 			->addScalar('groups', 'core_user_group', ['id' => 'userId'])
 			->addHasOne('workingWeek', WorkingWeek::class, ['id' => 'user_id']);
 	}
-	
-	/**
-	 * Get the user's personal group used for granting permissions
-	 * 
-	 * @return Group	 
-	 */
+
+  /**
+   * Get the user's personal group used for granting permissions
+   *
+   * @return Group
+   * @throws Exception
+   */
 	public function getPersonalGroup() {
 		return Group::find()->where(['isUserGroupFor' => $this->id])->single();
 	}
@@ -280,7 +277,11 @@ class User extends AclItemEntity {
 	}
 
 	private $currentPassword;
-	
+
+  /**
+   * @param $currentPassword
+   * @throws Exception
+   */
 	public function setCurrentPassword($currentPassword){
 		$this->currentPassword = $currentPassword;
 		
@@ -289,17 +290,18 @@ class User extends AclItemEntity {
 		} 
 	}
 
-	/**
-	 * Check if the password is correct for this user.
-	 * 
-	 * @param string $password
-	 * @return boolean 
-	 */
+  /**
+   * Check if the password is correct for this user.
+   *
+   * @param string $password
+   * @return boolean
+   * @throws Exception
+   */
 	public function checkPassword($password) {		
 		
 		$authenticator = $this->getPrimaryAuthenticator();
 		if(!isset($authenticator)) {
-			throw new \Exception("No primary authenticator found!");
+			throw new Exception("No primary authenticator found!");
 		}
 		$success = $authenticator->authenticate($this->username, $password);		
 		if($success) {
@@ -338,13 +340,14 @@ class User extends AclItemEntity {
 		return !empty($this->password);
 	}
 
-	/**
-	 * Clear the password stored in the database.
-	 * 
-	 * Used by authenticators (IMAP or LDAP) so they can clear it if it's not needed.
-	 * 
-	 * @return bool
-	 */
+  /**
+   * Clear the password stored in the database.
+   *
+   * Used by authenticators (IMAP or LDAP) so they can clear it if it's not needed.
+   *
+   * @return bool
+   * @throws Exception
+   */
 	public function clearPassword() {
 		return go()->getDbConnection()->delete('core_auth_password', ['userId' => $this->id])->execute();
 	}
@@ -353,10 +356,11 @@ class User extends AclItemEntity {
 		return null;
 	}
 
-	/**
-	 * Make sure to call this when changing the password with a recovery hash
-	 * @param string $hash
-	 */
+  /**
+   * Make sure to call this when changing the password with a recovery hash
+   * @param string $hash
+   * @return bool
+   */
 	public function checkRecoveryHash($hash) {
 		if($hash === $this->recoveryHash) {
 			$this->passwordVerified = true;
@@ -467,33 +471,41 @@ class User extends AclItemEntity {
 	
 	protected static function defineFilters() {
 		return parent::defineFilters()
-						->add('showDisabled', function (Criteria $criteria, $value){							
-							if($value === false) {
-								$criteria->andWhere('enabled', '=', true);
-							}
-						})
-						->add('groupId', function (Criteria $criteria, $value, Query $query){
-							$query->join('core_user_group', 'ug', 'ug.userId = u.id')->andWhere(['ug.groupId' => $value]);
-						});
+      ->add('permissionLevel', function(Criteria $criteria, $value, Query $query) {
+        if(!$query->isJoined('core_group', 'g')) {
+          $query->join('core_group', 'g', 'u.id = g.isUserGroupFor');
+        }
+        Acl::applyToQuery($query, 'g.aclId', $value);
+      })
+      ->add('showDisabled', function (Criteria $criteria, $value){
+        if($value === false) {
+          $criteria->andWhere('enabled', '=', true);
+        }
+      })
+      ->add('groupId', function (Criteria $criteria, $value, Query $query){
+        $query->join('core_user_group', 'ug', 'ug.userId = u.id')->andWhere(['ug.groupId' => $value]);
+      });
 	}
-	
 
-	/**
-	 * Check if use is an admin
-	 * 
-	 * @return boolean
-	 */
+
+  /**
+   * Check if use is an admin
+   *
+   * @return boolean
+   * @throws Exception
+   */
 	public function isAdmin() {
 		return (new Query)
 			->select('*')
 			->from('core_user_group')
 			->where(['groupId' => Group::ID_ADMINS, 'userId' => $this->id])->single() !== false;
 	}
-	
-	/**
-	 * Alias for making isAdmin() a public property
-	 * @return bool
-	 */
+
+  /**
+   * Alias for making isAdmin() a public property
+   * @return bool
+   * @throws Exception
+   */
 	public function getIsAdmin() {
 		return $this->isAdmin();
 	}
@@ -528,14 +540,15 @@ class User extends AclItemEntity {
 
 		return $methods;
 	}
-	
-	/**
-	 * Send a password recovery link
-	 * 
-	 * @param string $to
-	 * @param string $redirectUrl If given GroupOffice will redirect to this URL after creating a new password.
-	 * @return boolean
-	 */
+
+  /**
+   * Send a password recovery link
+   *
+   * @param string $to
+   * @param string $redirectUrl If given GroupOffice will redirect to this URL after creating a new password.
+   * @return boolean
+   * @throws Exception
+   */
 	public function sendRecoveryMail($to, $redirectUrl = ""){
 		
 		$this->recoveryHash = bin2hex(random_bytes(20));
@@ -803,7 +816,7 @@ class User extends AclItemEntity {
 	
 	public function setProfile($values) {
 		if(!Module::findByName('community', 'addressbook')) {
-			throw new \Exception("Can't set profile without address book module.");
+			throw new Exception("Can't set profile without address book module.");
 		}
 		
 		$this->contact = $this->getProfile();		
@@ -822,19 +835,4 @@ class User extends AclItemEntity {
 		return $arr;
 	}
 
-  /**
-   * @inheritDoc
-   */
-  protected static function aclEntityClass()
-  {
-    return Group::class;
-  }
-
-  /**
-   * @inheritDoc
-   */
-  protected static function aclEntityKeys()
-  {
-    return ['id' => 'isUserGroupFor'];
-  }
 }
--- www/modules/email/controller/MessageController.php
+++ www/modules/email/controller/MessageController.php
@@ -539,7 +539,7 @@ Settings -> Accounts -> Double click account -> Folders.", "email");
 
 					foreach($to as $email=>$name){
 						
-						$contacts = Contact::findByEmail($email)->filter(['permissionLevel' => GoAcl::LEVEL_WRITE]);
+						$contacts = Contact::findByEmail($email, ['id', 'addressBookId'])->filter(['permissionLevel' => GoAcl::LEVEL_WRITE]);
 
 						foreach($contacts as $contact){
 
@@ -666,13 +666,13 @@ Settings -> Accounts -> Double click account -> Folders.", "email");
 			$recipients->addString($params['bcc']);
 
 			foreach ($recipients->getAddresses() as $email => $personal) {
-				$contact = \go\modules\community\addressbook\model\Contact::findByEmail($email)
+				$contact = \go\modules\community\addressbook\model\Contact::findByEmail($email, ['id', 'addressBookId'])
           ->filter(["permissionLevel" => \go\core\model\Acl::LEVEL_READ])->single();
 				if($contact) {
 				  continue;
         }
 
-				$user = User::find(['id'])->filter(["permissionLevel" => \go\core\model\Acl::LEVEL_READ])->single();
+				$user = User::find(['id'])->where(['email' => $email])->filter(["permissionLevel" => \go\core\model\Acl::LEVEL_READ])->single();
         if($user) {
           continue;
         }
@@ -1808,7 +1808,7 @@ Settings -> Accounts -> Double click account -> Folders.", "email");
 			$from = $imapMessage->from->getAddress();
 
 			
-			$contacts = Contact::findByEmail($from['email'])->filter(['permissionLevel' => GoAcl::LEVEL_WRITE]);
+			$contacts = Contact::findByEmail($from['email'], ['id'])->filter(['permissionLevel' => GoAcl::LEVEL_WRITE]);
 
 			foreach($contacts as $contact) {
 				if($contact && $linkedModels->findKeyBy(function($item) use ($contact) { return $item->equals($contact); } ) === false){						
