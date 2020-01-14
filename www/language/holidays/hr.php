f9a7d004edfa8847d1f19a0a5c8ebe62cf750f2e
mschering <mschering@intermesh.nl>
2020-01-14 15:57:01 +0100

    Updated Croatian holidays

--- www/language/holidays/hr.php
+++ www/language/holidays/hr.php
@@ -3,12 +3,12 @@
 $input_holidays['fix']['01-01'] = 'Nova Godina';
 $input_holidays['fix']['01-06'] = 'Sveta tri kralja';
 $input_holidays['fix']['05-01'] = 'Praznik rada';
+$input_holidays['fix']['05-30'] = 'Dan državnosti';
 $input_holidays['fix']['06-22'] = 'Dan antifašističke borbe';
-$input_holidays['fix']['06-25'] = 'Dan državnosti';
 $input_holidays['fix']['08-05'] = 'Dan domovinske zahvalnosti';
 $input_holidays['fix']['08-15'] = 'Velika Gospa';
-$input_holidays['fix']['10-08'] = 'Dan neovisnosti';
 $input_holidays['fix']['11-01'] = 'Svi sveti';
+$input_holidays['fix']['11-18'] = 'Dan sjećanja na žrtve Domovinskog rata i Dan sjećanja na žrtvu Vukovara i Škabrnje';
 $input_holidays['fix']['12-25'] = 'Božić';
 $input_holidays['fix']['12-26'] = 'Sveti Stjepan';
 
