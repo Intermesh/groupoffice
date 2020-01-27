090bcb4db5e578199b4c353e6dbcc8ac2f08576c
mschering <mschering@intermesh.nl>
2020-01-27 11:51:36 +0100

    Sync: Fixed error in my account when no email module is available

--- www/modules/sync/Settings.js
+++ www/modules/sync/Settings.js
@@ -32,16 +32,19 @@ GO.sync.SettingsPanel = Ext.extend(Ext.Panel,{
 	
 	initComponent: function() {
 		
-		this.items = [new Ext.form.FieldSet({
-			title:'E-mail',
-			labelWidth: 170,
-			items: [
-				this.selectAccount = new GO.email.SelectAccount({
-					hidden: (!GO.settings.modules.email || !GO.settings.modules.email.read_permission),
-					hiddenName:'syncSettings.account_id'					
-				})
-			]})
-		];
+		this.items = [];
+
+		if(go.Modules.isAvailable("legacy", "email")) {
+			this.items.push(new Ext.form.FieldSet({
+				title:'E-mail',
+				labelWidth: 170,
+				items: [
+					this.selectAccount = new GO.email.SelectAccount({
+						hidden: (!GO.settings.modules.email || !GO.settings.modules.email.read_permission),
+						hiddenName:'syncSettings.account_id'
+					})
+				]}));
+		}
 
 		var syncComponents = {calendar: 'Calendar',tasks: 'Tasklist'};
 		
