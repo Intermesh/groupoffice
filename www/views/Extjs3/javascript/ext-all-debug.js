b59d44dda4724196ec1f4e880e242cd2332bdad7
mschering <mschering@intermesh.nl>
2020-04-07 11:38:28 +0200

    Core: Implemented Dark theme html editor style

--- www/views/Extjs3/javascript/ext-all-debug.js
+++ www/views/Extjs3/javascript/ext-all-debug.js
@@ -44828,7 +44828,8 @@ Ext.form.HtmlEditor = Ext.extend(Ext.form.Field, {
     
     getDocMarkup : function(){
         var h = Ext.fly(this.iframe).getHeight() - this.iframePad * 2;
-        return String.format('<html><head><link rel="stylesheet" href="' + BaseHref + 'views/Extjs3/themes/Paper/htmleditor.css"> <style type="text/css">body{border: 0; margin: 0; padding: {0}px; height: {1}px; cursor: text}</style></head><body></body></html>', this.iframePad, h);
+        var theme = go.User ? go.User.theme : "Paper";
+        return String.format('<html><head><link rel="stylesheet" href="' + BaseHref + 'views/Extjs3/themes/' + theme + '/htmleditor.css"> <style type="text/css">body{border: 0; margin: 0; padding: {0}px; height: {1}px; cursor: text}</style></head><body></body></html>', this.iframePad, h);
     },
 
     
