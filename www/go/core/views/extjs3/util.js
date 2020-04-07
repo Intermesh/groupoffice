32cc2c57dc973e3d511b1f1d91818cd096943ed0
mschering <mschering@intermesh.nl>
2020-04-07 13:54:00 +0200

    New way to download file

--- www/go/core/views/extjs3/util.js
+++ www/go/core/views/extjs3/util.js
@@ -304,10 +304,16 @@ go.util =  (function () {
 					window.open(url);
 				} else {
 					// document.location.href = url; //This causes connection errors with SSE or other simulanous XHR requests
+					// if(!downloadFrame) {
+					// 	downloadFrame = document.createElement('iframe');
+					// 	downloadFrame.id="downloader";
+					// 	downloadFrame.style.display = 'none';
+					// 	document.body.appendChild(downloadFrame);
+					// }
+					// downloadFrame.src = url;
 
 					if(!downloadFrame) {
 						var downloadFrame = document.createElement("a");
-						downloadFrame.download = true;
 					}
 					downloadFrame.href = url;
 					downloadFrame.click();
