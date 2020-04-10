3f6f4ab4fa12b65c3f1351f596b713de44c54704
mschering <mschering@intermesh.nl>
2020-04-10 12:11:06 +0200

    Debugging mailbox problem

--- www/go/base/mail/exception/MailboxNotFound.php
+++ www/go/base/mail/exception/MailboxNotFound.php
@@ -6,13 +6,15 @@
 namespace GO\Base\Mail\Exception;
 
 
+use GO\Base\Mail\Imap;
+
 class MailboxNotFound extends \Exception{
 	
-	public function __construct($mailbox,$imap) {
+	public function __construct($mailbox, Imap $imap) {
 		
-	//	$imap->last_error(); // Get the last error
+		$last = $imap->last_error(); // Get the last error
 
-		$message = sprintf(\GO::t("Cannot open the folder \"%s\". Please check your email account settings."),$mailbox);
+		$message = sprintf(\GO::t("Cannot open the folder \"%s\". Please check your email account settings." . $last ),$mailbox);
 		$imap->clear_errors(); // Needed to clear the imap errors
 		
 		parent::__construct($message);
