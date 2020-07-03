00311f71494279d19eef7812467201c5f1d24ecc
mschering <mschering@intermesh.nl>
2020-07-03 09:44:13 +0200

    headers

--- www/go/core/http/Response.php
+++ www/go/core/http/Response.php
@@ -243,7 +243,7 @@ class Response extends Singleton{
 		$this->setHeader("Content-Security-Policy", (new \go\core\webclient\CSP()));
 		$this->setHeader("X-Content-Type-Options","nosniff");
 		$this->setHeader("Strict-Transport-Security"," max-age=31536000");
-		$this->setHeader("X-XSS-Protection", "1");
+		$this->setHeader("X-XSS-Protection", "1;mode=block");
 	}
 
 	public function sendHeaders() {		
