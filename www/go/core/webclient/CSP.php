423c75e48be195301e03bd8c34619461a3cc9b77
mschering <mschering@intermesh.nl>
2020-07-06 13:55:57 +0200

    headers

--- www/go/core/webclient/CSP.php
+++ www/go/core/webclient/CSP.php
@@ -24,18 +24,19 @@ class CSP {
 
   public function __construct()
   {
-    $this->add("default-src", Request::get()->getHost())
+    $this
+	    //->add("default-src", Request::get()->getHost())
     ->add("default-src", "'self'")
     ->add("default-src", "about:")
     ->add("font-src", "'self'")
-    ->add('font-src', Request::get()->getHost())
+    //->add('font-src', Request::get()->getHost())
     ->add('font-src', "data:")
-    ->add("script-src", Request::get()->getHost())
+    //->add("script-src", Request::get()->getHost())
     // ->add("script-src", "'nonce-" . Response::get()->getCspNonce() . "'")
     ->add("script-src", "'unsafe-eval'")
     ->add("script-src", "'self'")
     ->add("script-src", "'unsafe-inline'") //TODO replace all onclick="" in the code and remove this line
-    ->add('img-src', Request::get()->getHost())
+    //->add('img-src', Request::get()->getHost())
     ->add('img-src', "'self'")
     ->add('img-src', "about:")
     ->add('img-src', "data:")
