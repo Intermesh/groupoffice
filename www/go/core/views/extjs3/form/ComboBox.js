296a8333fc3353df6a6e2698521d264a1825f2a3
mschering <mschering@intermesh.nl>
2020-07-24 14:41:40 +0200

    Core: Added allowNew config option to create new records from combo box

--- www/go/core/views/extjs3/form/ComboBox.js
+++ www/go/core/views/extjs3/form/ComboBox.js
@@ -25,10 +25,84 @@ go.form.ComboBox = Ext.extend(Ext.form.ComboBox, {
 	},
 
 	initComponent: function() {
+
+		if(!this.tpl) {
+			this.tpl =
+				'<tpl for=".">'+
+				'<div class="x-combo-list-item" title="{[fm.htmlEncode(values[\'' + this.displayField + '\'] || \'\' )]}"><tpl if="!values.' + this.valueField + '"><b>' + t("Create new") + ':</b> </tpl>{[fm.htmlEncode(values["' + this.displayField + '"] || "" )]}</div>'+
+				'</tpl>';
+		}
+
 		go.form.ComboBox.superclass.initComponent.call(this);
 
 		//Set value promise can be used to do stuff after setvalue completed fetching the entity and loaded the store.
 		this.setValuePromise = Promise.resolve(this);
+
+		if(this.allowNew) {
+			this.store.on("load", this.addCreateNewRecord, this);
+			this.on('beforeselect', function(combo, record, index) {
+				if(!record.data[this.valueField]) {
+					this.createNew(record).then(function() {
+						var record = combo.store.getAt(0);
+						combo.fireEvent('select', combo, record, 0);
+					});
+
+					//cancel select and fire it after create.
+					return false;
+				}
+				return true;
+			}, this);
+		}
+	},
+
+	addCreateNewRecord: function() {
+		var text =  this.getRawValue();
+		if(!text) {
+			return;
+		}
+		var def = Ext.data.Record.create([{
+			name: this.valueField
+		},{
+			name: this.displayField
+		}]);
+
+		var recordData = {};
+		recordData[this.displayField] = text;
+		var record = new def(recordData);
+		this.store.insert(0, record);
+
+		if(this.store.getCount() > 1) {
+			this.select(0);
+		}
+	},
+
+	// expand : function() {
+	// 	debugger;
+	// 	return go.form.ComboBox.superclass.expand.call(this);
+	// },
+
+	createNew : function(record) {
+
+		var data = record.data;
+		if(Ext.isObject(this.allowNew)) {
+			Ext.apply(data, this.allowNew);
+		}
+		var create = {"newid" : data}, me = this;
+
+		//reset store and prevent onChanges call.
+
+		me.collapse();
+		me.store.loaded = false;
+		me.store.removeAll();
+		//Clear text input or it will recreate fake record.
+		me.setRawValue("");
+
+		return this.store.entityStore.set({
+			create: create
+		}).then(function(response) {
+			me.setValue(response.created.newid.id);
+			return me.setValuePromise;
+		});
 	},
 	
 	resolveEntity : function(value) {
@@ -46,7 +120,7 @@ go.form.ComboBox = Ext.extend(Ext.form.ComboBox, {
 				value = null;
 			}
 
-			if(!me.hiddenName || !value) {
+			if(!value) {
 				resolve(me);
 				return go.form.ComboBox.superclass.setValue.call(me, value);
 			}
@@ -57,11 +131,19 @@ go.form.ComboBox = Ext.extend(Ext.form.ComboBox, {
 				me.value = value;
 
 				me.resolveEntity(value).then(function (entity) {
+					//this prevents the list to expand on loading the value
+					var origHasFocus = me.hasFocus;
+
 					me.store.on("load", function() {
-						resolve(me);
+
 						go.form.ComboBox.superclass.setValue.call(me, value);
+
+						me.hasFocus = origHasFocus;
+						resolve(me);
 					}, me, {single: true});
-					me.store.loadData({records:[entity]}, true);
+
+					me.hasFocus = false;
+					me.store.loadData({records:[entity]}, false);
 
 				}).catch(function(e) {
 					console.error(e);
