bc600463878f927ce40ce5274c861a96646bd945
mschering <mschering@intermesh.nl>
2020-09-18 14:29:38 +0200

    Delay loading

--- www/views/Extjs3/javascript/ext-base-debug.js
+++ www/views/Extjs3/javascript/ext-base-debug.js
@@ -1551,7 +1551,7 @@ Ext.TaskMgr.start({
  * (defaults to 10)
  */
 Ext.util.TaskRunner = function(interval){
-    interval = interval || 100;
+    interval = interval || 10;
     var tasks = [], 
     	removeQueue = [],
     	id = 0,
