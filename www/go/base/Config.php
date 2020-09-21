7042b86786e2d398ee25345fa130ffbae45b2ad9
mschering <mschering@intermesh.nl>
2020-09-21 15:09:32 +0200

    6.2.130

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -1059,7 +1059,7 @@ var $billing_clear_payment_method_on_duplicate = true;
 	 * @var     StringHelper
 	 * @access  public
 	 */
-	var $version = '6.2.129';
+	var $version = '6.2.130';
 
 	/**
 	 * Modification date
@@ -1068,7 +1068,7 @@ var $billing_clear_payment_method_on_duplicate = true;
 	 * @access  public
 	 */
 	
-	var $mtime = '20200921';
+	var $mtime = '20200921-1';
 
 	#group configuration
 	/**
