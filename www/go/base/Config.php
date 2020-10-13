50714b7fb40326b776363a569c6f699c0c7a628a
mschering <mschering@intermesh.nl>
2020-10-13 17:31:48 +0200

    - Gota: signed the application with new certificate

--- www/go/base/Config.php
+++ www/go/base/Config.php
@@ -1059,7 +1059,7 @@ var $billing_clear_payment_method_on_duplicate = true;
 	 * @var     StringHelper
 	 * @access  public
 	 */
-	var $version = '6.2.130';
+	var $version = '6.2.131';
 
 	/**
 	 * Modification date
@@ -1068,7 +1068,7 @@ var $billing_clear_payment_method_on_duplicate = true;
 	 * @access  public
 	 */
 	
-	var $mtime = '20200921-1';
+	var $mtime = '20201013';
 
 	#group configuration
 	/**
