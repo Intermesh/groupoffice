7dbb85472f8f5c1551be315cf36ecc8fa9cf7e2d
mschering <mschering@intermesh.nl>
2020-12-15 14:54:35 +0100

    Fixed upgrade

--- www/go/modules/community/addressbook/install/updates.php
+++ www/go/modules/community/addressbook/install/updates.php
@@ -4,7 +4,7 @@ use go\modules\community\addressbook\Module;
 $updates = [];
 
 $updates['201811272011'][] = function() {
-	\go\core\db\Utils::runSQLFile(\go()->getEnvironment()->getInstallFolder()->getFile("go/modules/community/addressbook/install/install.sql"));
+	\go\core\db\Utils::runSQLFile(\go()->getEnvironment()->getInstallFolder()->getFile("go/modules/community/addressbook/install/upgrade.sql"));
 };
 $updates['201811272011'][] = "DROP TABLE addressbook_contact_custom_fields";
 
