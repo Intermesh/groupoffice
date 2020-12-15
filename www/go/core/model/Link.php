57230368e1b755518fb64a1fa0d73dfda1ea0bd4
mschering <mschering@intermesh.nl>
2020-12-15 20:54:34 +0100

    Revert "Error in /usr/share/groupoffice/go/core/model/Link.php at line 305: Call to undefined method go\core\model\Link::updateDataFromSearch()"
    This reverts commit caf2eb70

--- www/go/core/model/Link.php
+++ www/go/core/model/Link.php
@@ -301,6 +301,10 @@ class Link extends Entity
 	 * @return int
 	 */
 	public function getPermissionLevel() {
+		if($this->isNew()) {			
+			$this->updateDataFromSearch();
+		}
+
 		if(!isset($this->permissionLevel)) {
 			$this->permissionLevel = Acl::getUserPermissionLevel($this->aclId, App::get()->getAuthState()->getUserId());
 		}
