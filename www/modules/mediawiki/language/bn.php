d5e338f899e7955643e21c3ee39a8fb00b893c4e
Merijn Schering <mschering@intermesh.nl>
2021-01-08 11:32:09 +0100

    Delete media wiki module

--- www/modules/mediawiki/language/bn.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'name' => 'মিডিয়া উইকি',
-  'description' => ' এটি মিডিয়াউইকি এবং  {product_name} এর সাথে সংযুক্ত করে এবং স্বয়ংক্রিয়ভাবে {product_name} ব্যবহারকারিদেরকে মিডিয়াউইকিতে প্রবেশের সুযোগ দেয় । আরও বিস্তারিত জানতে  modules/mediawiki/INSTALL.TXT দেখুন ।',
-);
--- www/modules/mediawiki/language/vi.php
+++
@@ -1,4 +0,0 @@
-<?php
-return array (
-  'description' => 'Module này tích hợp tự động với Mediawiki khi người dùng đăng nhập vào hệ thống.',
-);
