de9a223fe57b7968a7a6bf497cfbccbd90166cfa
Merijn Schering <mschering@intermesh.nl>
2021-02-22 17:43:54 +0100

    Started with cron

---
+++ www/go/core/views/extjs3/cron/NewCronDialog.js
@@ -0,0 +1,30 @@
+/* global go */
+
+go.cron.NewCronDialog = Ext.extend(go.form.Dialog, {
+	title: t('Scheduled tasks'),
+	entityStore: "CronJobSchedule",
+	// width: dp(1000),
+	// height: dp(800),
+	resizable: true,
+	maximizable: true,
+	collapsible: true,
+	modal: false,
+
+	initFormItems: function () {
+		return [{
+				xtype: 'textfield',
+				fieldLabel: t('Description'),
+				name: 'description'
+			},{
+				xtype: 'textfield',
+				fieldLabel: t('Expression'),
+				name: 'expression'
+			},{
+				xtype: 'checkbox',
+				fieldLabel: t('Enabled'),
+				name: 'enabled'
+			}];
+	}
+});
+
+
