bd8c78279776ac1bc984579fd4da390593bc2365
Merijn Schering <mschering@intermesh.nl>
2021-03-12 16:53:40 +0100

    check if dirs exist

--- build/build.php
+++ build/build.php
@@ -319,8 +319,14 @@ class Builder {
             }
 
             run('cp ' . $moduleFile . ' ' . $this->buildDir . "/" . $this->packageName . '/go/modules/business/' . $moduleName . '/');
-            run('cp ' . $this->sourceDir . '/business/' . $moduleName . '/language/* ' . $this->buildDir . "/" . $this->packageName . '/go/modules/business/' . $moduleName . '/language/');
-            run('cp -r ' . $this->sourceDir . '/business/' . $moduleName . '/install/* ' . $this->buildDir . "/" . $this->packageName . '/go/modules/business/' . $moduleName . '/install/');
+
+	        if (is_dir($this->sourceDir . '/business/' . $moduleName . '/language')) {
+                run('cp ' . $this->sourceDir . '/business/' . $moduleName . '/language/* ' . $this->buildDir . "/" . $this->packageName . '/go/modules/business/' . $moduleName . '/language/');
+            }
+
+	        if (is_dir($this->sourceDir . '/business/' . $moduleName . '/install')) {
+                run('cp -r ' . $this->sourceDir . '/business/' . $moduleName . '/install/* ' . $this->buildDir . "/" . $this->packageName . '/go/modules/business/' . $moduleName . '/install/');
+            }
         }
 
         run('rm -rf ' . $this->buildDir . "/" . $this->packageName . '/go/modules/business/.git*');
