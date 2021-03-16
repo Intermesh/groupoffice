3428c4c1cf96360ae5952a983b57934ae2ce7cda
Merijn Schering <mschering@intermesh.nl>
2021-03-16 13:43:38 +0100

    Better error reporting in auth

--- www/go/core/auth/Authenticate.php
+++ www/go/core/auth/Authenticate.php
@@ -5,6 +5,8 @@ namespace go\core\auth;
 use go\core\App;
 use go\core\ErrorHandler;
 use go\core\exception\Forbidden;
+use go\core\exception\Unavaiable;
+use go\core\exception\Unavailable;
 use go\core\jmap\State;
 use go\core\model\AuthAllowGroup;
 use go\core\model\Token;
@@ -169,7 +171,7 @@ class Authenticate {
 		}
 
 		if(go()->getSettings()->maintenanceMode && !$user->isAdmin()) {
-			throw new Forbidden(go()->t("You're account has been disabled."));
+			throw new Unavailable(go()->t("Service unavailable. Maintenance mode is enabled."));
 		}
 
 		go()->getCache()->set($cacheKey, [$user, $authenticator], true, self::CACHE_PASSWORD_LOGIN);
--- www/go/core/exception/NotFound.php
+++ www/go/core/exception/NotFound.php
@@ -4,6 +4,6 @@ namespace go\core\exception;
 class NotFound extends \Exception {
 	
 	public function __construct($message = "The item was not found", $code = 0, $previous = null) {
-		parent::__construct($message, $code, $previous);
+		parent::__construct(go()->t($message), $code, $previous);
 	}
 }
