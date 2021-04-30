8125e957321ec5dadd84c2ef245807f2e06f18ee
Merijn Schering <mschering@intermesh.nl>
2021-04-30 11:05:42 +0200

    Small bug fix

--- www/modules/postfixadmin/scripts/RenameDomain.sql
+++ www/modules/postfixadmin/scripts/RenameDomain.sql
@@ -3,8 +3,8 @@
 
 #Execute first part on the mailserver
 set names utf8mb4 COLLATE utf8mb4_unicode_ci;
-set @old_domain = 'intermesh.localhost';
-set @new_domain = 'groupoffice.localhost';
+set @old_domain = 'groupoffice.localhost';
+set @new_domain = 'intermesh.localhost';
 
 # make sure aliases are unique.
 drop index address on pa_aliases;
@@ -28,21 +28,19 @@ update pa_mailboxes set
                         maildir = replace(maildir,  @old_domain, @new_domain),
                         homedir = replace(homedir, @old_domain, @new_domain);
 
-update pa_aliases set
-                      address = replace(address, concat('@', @old_domain), concat('@', @new_domain)),
-                      goto = replace(goto, concat('@', @old_domain), concat('@', @new_domain));
+update pa_aliases set goto = replace(goto, concat('@', @old_domain), concat('@', @new_domain));
 
 insert ignore into pa_aliases
-select null,domain_id,replace(address, concat('@', @new_domain), concat('@', @old_domain)),goto,ctime,mtime,active from pa_aliases;
+select null,domain_id,replace(address, concat('@', @old_domain), concat('@', @new_domain)),goto,ctime,mtime,active from pa_aliases;
 
 insert ignore into pa_aliases
 select null,domain_id,username,replace(username, concat('@', @new_domain), concat('@', @old_domain)),unix_timestamp(),unix_timestamp(),1 from pa_mailboxes;
 
 
 # Execute this on the client groupoffice instance (might be the same db)
-set names utf8mb4 COLLATE utf8mb4_unicode_ci;
-set @old_domain = 'intermesh.localhost';
-set @new_domain = 'groupoffice.localhost';
+# set names utf8mb4 COLLATE utf8mb4_unicode_ci;
+# set @old_domain = 'intermesh.localhost';
+# set @new_domain = 'groupoffice.localhost';
 
 update em_accounts set username = replace(username, concat('@', @old_domain), concat('@', @new_domain)), smtp_username = replace(smtp_username, concat('@', @old_domain), concat('@', @new_domain));
 update em_aliases set email = replace(email, concat('@', @old_domain), concat('@', @new_domain));
