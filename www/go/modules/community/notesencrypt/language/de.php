80ccea91678a09fb47356df0f92234d070e5e58b
Merijn Schering <mschering@intermesh.nl>
2021-05-10 13:38:53 +0200

    UPdated German translation

--- www/go/modules/community/notesencrypt/language/de.php
+++ www/go/modules/community/notesencrypt/language/de.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  'Encrypted data' => 'Verschlüsselte daten',
+  'Encrypted data' => 'Verschlüsselte Daten',
   'Enter password to decrypt' => 'Passwort eingeben zum entschlüsseln',
   'Wrong password' => 'Falsches Passwort',
   'Notes encrypt' => 'Notiz verschlüsselt',
   'Sorry, you can\'t use images in encrypted notes' => 'Sie können keine Bilder in verschlüsselten Notizen benutzen',
-  'Cryptographic functions are not available. Please run Group-Office with SSL.' => 'Cryptographic functions are not available. Please run {product_name} with SSL.',
+  'Cryptographic functions are not available. Please run Group-Office with SSL.' => 'Kryptografische Funktionen sind nicht verfügbar. Bitte führen Sie {produkt_name} mit SSL aus.',
 );
