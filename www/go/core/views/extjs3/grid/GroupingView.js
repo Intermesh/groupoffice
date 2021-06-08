d81f3a8aefecc408dd7f3b8f87dee6debe9e27db
Merijn Schering <mschering@intermesh.nl>
2021-06-08 09:12:35 +0200

    Grouping remotely

--- www/go/core/views/extjs3/grid/GroupingView.js
+++ www/go/core/views/extjs3/grid/GroupingView.js
@@ -1,13 +1,11 @@
 Ext.define('go.grid.GroupingView', {
-  	extend: 'Ext.grid.GroupingView',
-
+	extend: 'Ext.grid.GroupingView',
+	htmlEncode: true,
 	totalDisplay: false,
 	totalCount: 0,
 
 	masterTpl: go.grid.GridView.prototype.masterTpl,
 	initElements: go.grid.GridView.prototype.initElements,
 	setTotalCount: go.grid.GridView.prototype.setTotalCount,
-	initActionButton : function() {
-  		//todo
-	}
+	initActionButton : go.grid.GridView.prototype.initActionButton
 });
