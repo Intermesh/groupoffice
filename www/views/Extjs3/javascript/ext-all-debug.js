ebac2e03718bc9184619d2d6611729d2b8ef835b
Merijn Schering <mschering@intermesh.nl>
2021-06-29 12:18:34 +0200

    log validation errors

--- www/views/Extjs3/javascript/ext-all-debug.js
+++ www/views/Extjs3/javascript/ext-all-debug.js
@@ -43189,6 +43189,7 @@ Ext.form.BasicForm = Ext.extend(Ext.util.Observable, {
         var valid = true;
         this.items.each(function(f){
            if(!f.validate()){
+           		console.warn("Invalid form  field", f);
                valid = false;
            }
         });
