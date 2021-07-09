bffa8db9cbdc7aaf89b205069124afee15557bea
Merijn Schering <mschering@intermesh.nl>
2021-07-09 09:51:31 +0200

    add ignore

--- www/go/modules/community/googleauthenticator/install/updates.php
+++ www/go/modules/community/googleauthenticator/install/updates.php
@@ -2,4 +2,4 @@
 $updates['202012210922'][] = "ALTER TABLE `googleauth_secret` DROP FOREIGN KEY `googleauth_secret_user`; ";
 $updates['202012210922'][] = "ALTER TABLE `googleauth_secret` ADD CONSTRAINT `googleauth_secret_user` FOREIGN KEY (`userId`) REFERENCES `core_user`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;";
 
-$updates['202107090900'][] = "insert into core_acl_group select aclId, '2', 10 from core_module where package='community' and name ='googleauthenticator'";
+$updates['202107090900'][] = "insert ignore into core_acl_group select aclId, '2', 10 from core_module where package='community' and name ='googleauthenticator'";
