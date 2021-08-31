2cb993b98941e3f821d7870d6cbebc9208f80e17
Merijn Schering <mschering@intermesh.nl>
2021-08-31 10:10:09 +0200

    Email: Fixed broken base64 encoded messages

--- www/go/base/mail/Imap.php
+++ www/go/base/mail/Imap.php
@@ -2286,6 +2286,12 @@ class Imap extends ImapBodyStruct {
 			return $this->_uudecode($uid, $part_no, $peek, $fp);
 		}
 
+
+//		if(strtolower($encoding) == 'base64') {
+//			$part = $this->get_message_part($uid, $part_no, $encoding, $peek);
+//			//return base64_decode($part);
+//		}
+
 		$str = '';
 		$this->get_message_part_start($uid, $part_no, $peek);
 
@@ -2296,7 +2302,7 @@ class Imap extends ImapBodyStruct {
 
 			switch (strtolower($encoding)) {
 				case 'base64':
-					$line = trim($leftOver.$line);
+					$line = trim($leftOver).trim($line);
 					$leftOver = "";
 
 					if(strlen($line) % 4 == 0){
