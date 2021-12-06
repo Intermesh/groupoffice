300b66574f7722b274a5b2b753eed65fad6078af
Merijn Schering <mschering@intermesh.nl>
2021-12-06 11:20:28 +0100

    Googleauthenticator: Use title for QR image so app takes over name

--- www/go/modules/community/googleauthenticator/model/Googleauthenticator.php
+++ www/go/modules/community/googleauthenticator/model/Googleauthenticator.php
@@ -4,6 +4,7 @@ namespace go\modules\community\googleauthenticator\model;
 
 use Exception;
 use go\core\App;
+use go\core\fs\File;
 use go\core\orm\Property;
 
 require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'core'.DIRECTORY_SEPARATOR.'util'.DIRECTORY_SEPARATOR.'QRcode.php';
@@ -202,7 +203,7 @@ class Googleauthenticator extends Property {
 			return null;
 		}
 		
-		$name = empty($name) ? "qr.png" : $name;
+		$name = empty($name) ? File::stripInvalidChars(go()->getSettings()->title) : $name;
 		$secret = empty($secret)?$this->secret:$secret;
 
 		$level = QR_ECLEVEL_M;
