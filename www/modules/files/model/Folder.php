48dc8d61adc408a3755facae251aedb026a12cb6
Merijn Schering <mschering@intermesh.nl>
2021-12-09 11:52:37 +0100

    Added comment

--- www/modules/files/model/Folder.php
+++ www/modules/files/model/Folder.php
@@ -905,6 +905,7 @@ class Folder extends \GO\Base\Db\ActiveRecord {
 							//this will update timestamp and size of file
 
 							//todo: how can it be that $file->fsFile->exists() is needed here?
+							// it happens when the file on disk has a trailing space and the db name doesn't.
 							if($file->fsFile->exists() && $file->mtime != $file->fsFile->mtime()){
 								$file->save();
 							}
