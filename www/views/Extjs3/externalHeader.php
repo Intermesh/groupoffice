4c3e8f1654aeec1b70d1d14570250d2a2335c7ef
Merijn Schering <mschering@intermesh.nl>
2022-04-07 13:24:52 +0200

    Check for theme existance

--- www/views/Extjs3/externalHeader.php
+++ www/views/Extjs3/externalHeader.php
@@ -2,9 +2,14 @@
 		
 
 $theme = \GO::user() ? \GO::user()->theme : \GO::config()->theme;
-		
 
-require 'themes/' . $theme . '/pageHeader.php';
+$file = __DIR__ . '/themes/' . $theme . '/pageHeader.php';
+
+if(!file_exists($file)) {
+	$file = __DIR__ . '/themes/Paper/pageHeader.php';
+}
+
+require $file;
 ?>
 <section>
     <div class="card">
