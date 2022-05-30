d1fda4f327fe901815fafb9d7d6eda29b4153ec4
Merijn Schering <mschering@intermesh.nl>
2022-05-30 14:38:26 +0200

    Core: fixed removing permissions in group dialog

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -39,7 +39,7 @@ try {
 	$installDb = !go()->isInstalled() ? INSTALL_NEW : INSTALL_NONE;
 
 	// Always install
-	$installDb = INSTALL_NEW;
+//	$installDb = INSTALL_NEW;
 
 //	For testing upgrades use:
 //	$installDb = INSTALL_UPGRADE;
