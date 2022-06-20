92b81018398c5d68b5066d84e2ee8d22be385e7e
Merijn Schering <mschering@intermesh.nl>
2022-06-20 11:25:41 +0200

    Function to replace array key for sorting

--- www/go/core/util/ArrayUtil.php
+++ www/go/core/util/ArrayUtil.php
@@ -5,4 +5,30 @@ class ArrayUtil {
 //  public static function isAssociative($array) {
 //		return !empty(array_filter(array_keys($array), 'is_string'));
 //	}
+
+	/**
+	 * Rename string array key and maintain position
+	 *
+	 * @param string $old
+	 * @param string $new
+	 * @return bool
+	 */
+	public static function renameKey($array, string $old, string $new): array
+	{
+
+		$i = array_search($old, array_keys($array));
+		if($i === false) {
+			return false;
+		}
+
+		$value = $array[$old];
+
+		return array_merge(
+			array_slice($array, 0, $i),
+			[$new => $value],
+			array_slice($array, $i + 1)
+		);
+
+
+	}
 }
