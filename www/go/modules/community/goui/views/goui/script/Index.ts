10130800ed6b87e6a2394fdcb63282c7f3a5d8c8
Merijn Schering <mschering@intermesh.nl>
2022-07-31 20:50:34 +0200

    code cleanup

--- www/go/modules/community/goui/views/goui/script/Index.ts
+++ www/go/modules/community/goui/views/goui/script/Index.ts
@@ -2,23 +2,23 @@ import {modules, ModuleConfig} from "@go-core/Modules.js";
 import {Notes} from "./Notes.js";
 import {router} from "@go-core/Router.js";
 
-
-type NoteConfig = ModuleConfig & {notes?:Notes}
 modules.register(  {
 	package: "community",
 	name: "goui",
 	init () {
 
+		let notes: Notes;
+
 		router.add(/^goui-notes\/(\d+)$/, (noteId) => {
 			modules.openMainPanel("goui-notes");
-			this.notes.showNote(parseInt(noteId));
+			notes.showNote(parseInt(noteId));
 		});
 
 		modules.addMainPanel("goui-notes", "GOUI Notes", () => {
 
 			//this will lazy load Notes when module panel is opened.
-			this.notes = new Notes();
-			return this.notes;
+			notes = new Notes();
+			return notes;
 		});
 	}
-} as NoteConfig);
+});
