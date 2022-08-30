958bf5174218adb3649855701386bec68ce2a478
Merijn Schering <mschering@intermesh.nl>
2022-08-30 15:16:54 +0200

    Faster interval

--- www/go/core/model/PushDispatcher.php
+++ www/go/core/model/PushDispatcher.php
@@ -30,7 +30,7 @@ class PushDispatcher
 	/**
 	 * Interval in seconds between every check for changes to push
 	 */
-	const CHECK_INTERVAL = 30;
+	const CHECK_INTERVAL = 5;
 
 	private $map = [];
 	private $entityTypes = [];
