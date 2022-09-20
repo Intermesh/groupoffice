260ca43e5c63ab673a1c7211690611339e54de63
Merijn Schering <mschering@intermesh.nl>
2022-09-20 11:41:40 +0200

    VIES module

--- www/go/core/validate/CountryCode.php
+++ www/go/core/validate/CountryCode.php
@@ -1,18 +1,22 @@
 <?php
 namespace go\core\validate;
 
+use Exception;
+
 class CountryCode {
-	
+
 	/**
 	 * Validate ISO country code
-	 * 
+	 *
 	 * @param string $isoCode eg. "NL"
 	 * @return bool
+	 * @throws Exception
 	 */
-	public static function validate($isoCode) {
+	public static function validate(string $isoCode): bool
+	{
 		
 		if($isoCode != strtoupper($isoCode)) {
-			throw new \Exception("Country codes must be upper case");
+			throw new Exception("Country codes must be upper case");
 		}
 		
 		$countries = go()->t('countries');
