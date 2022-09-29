e4e07ca16783a247f7d08b09a21cfe589bca1712
dehart <derinus@gmail.com>
2022-09-29 13:46:12 +0200

    fixed for State getPageUrl() when called from old framework.

--- www/go/core/jmap/State.php
+++ www/go/core/jmap/State.php
@@ -149,6 +149,10 @@ class State extends AbstractState {
 	private function getBaseUrl() {
 		$url = Request::get()->isHttps() ? 'https://' : 'http://';
 		$url .= Request::get()->getHost(false) . dirname($_SERVER['PHP_SELF']);
+		// HACK for old framework index.php
+		if(substr(dirname($_SERVER['PHP_SELF']), -4) !== '/api'){
+			$url .= '/api';
+		}
 		return $url;
 	}
 	
