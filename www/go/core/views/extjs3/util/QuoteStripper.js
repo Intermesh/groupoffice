bbf425888933f309b4d4e774e227da321b3c55bf
Merijn Schering <mschering@intermesh.nl>
2022-10-27 17:19:16 +0200

    Strip quotes

--- www/go/core/views/extjs3/util/QuoteStripper.js
+++ www/go/core/views/extjs3/util/QuoteStripper.js
@@ -1,110 +1,86 @@
-'use strict';
-
-go.util.QuoteStripper = function (body) {
+class QuoteStripper {
+	constructor(body) {
+		this.body = body;
+	}
+	getBodyWithoutQuote() {
+		if (this.quote === undefined) {
+			this.split();
+		}
+		return this.bodyWithoutQuote;
+	}
+	getQuote() {
+		if (this.quote === undefined) {
+			this.split();
+		}
+		return this.quote;
+	}
+	split() {
+		let quoteIndex = this.findByBlockQuote();
+		if (quoteIndex === -1) {
+			quoteIndex = this.findByGreaterThan();
+		}
+		if (quoteIndex === -1) {
+			quoteIndex = this.findQuoteByHeaderBlock();
+		}
+		if (quoteIndex > -1) {
+			this.bodyWithoutQuote = this.body.substring(0, quoteIndex);
+			this.quote = this.body.substring(quoteIndex);
+		}
+		else {
+			this.bodyWithoutQuote = this.body;
+			this.quote = "";
+		}
+	}
+	findByGreaterThan() {
+		const pattern = /\n&gt;/;
+		const match = pattern.exec(this.body);
+		if (match) {
+			return pattern.lastIndex;
+		}
+		return -1;
+	}
+	;
+	findByBlockQuote() {
+		this.quoteIndex = this.body.indexOf("<blockquote");
+		return this.quoteIndex;
+	}
+	;
+	splitLines() {
+		if (!this.lines) {
+			const br = '|BR|';
+			const html = this.body
+				.replace(/<\/p>/ig, br + "$&")
+				.replace(/<\/div>/ig, br + "$&")
+				.replace(/<br[^>]*>/ig, br + "$&");
+			this.lines = html.split(br);
+		}
+		return this.lines;
+	}
 	/**
-	 * @type string
+	 * eg
+	 *
+	 * Van: Merijn Schering [mailto:mschering@intermesh.nl]
+      Verzonden: donderdag 20 november 2014 16:40
+      Aan: Someone
+      Onderwerp: Subject
+	 *
+	 * @return int|boolean
 	 */
-	this.body = body;
-	this.bodyWithoutQuote = null;
-	this.quote = null;
+	findQuoteByHeaderBlock() {
+		const lines = this.splitLines();
+		let pos = 0;
+		for (let i = 0, c = lines.length; i < c; i++) {
+			const plain = lines[i].replace(/(<([^>]+)>)/ig, ""); //strip html tags
+			const pattern = /[a-z]+:\s*[a-z0-9\._\-+\&]+@[a-z0-9\.\-_]+/i;
+			//Match:
+			//ABC: email@domain.com
+			if (plain.match(pattern)) {
+				return pos;
+			}
+			pos += lines[i].length;
+		}
+		return -1;
+	}
 }
 
-go.util.QuoteStripper.prototype.getBodyWithoutQuote = function () {
-	if (this.quote === null) {
-		this._split();
-	}
-
-	return this.bodyWithoutQuote;
-};
-
-go.util.QuoteStripper.prototype.getQuote = function () {
-	if (this.quote === null) {
-		this._split();
-	}
-
-	return this.quote;
-};
-
-go.util.QuoteStripper.prototype._split = function () {
-	var quoteIndex = this._findByBlockQuote();
-
-	if (quoteIndex === -1) {
-		quoteIndex = this._findByGreaterThan();
-	}
-
-	if (quoteIndex === -1) {
-		quoteIndex = this._findQuoteByHeaderBlock();
-	}
-
-	if (quoteIndex > -1) {
-		this.bodyWithoutQuote = this.body.substring(0, quoteIndex);
-		this.quote = this.body.substring(quoteIndex);
-	} else {
-		this.bodyWithoutQuote = this.body;
-		this.quote = "";
-	}
-};
-
-go.util.QuoteStripper.prototype._findByGreaterThan = function () {
-	var pattern = /\n&gt;/;
-
-	var match = pattern.exec(this.body);
-
-	if (match) {
-		return pattern.lastIndex;
-	}
-
-	return -1;
-};
-
-go.util.QuoteStripper.prototype._findByBlockQuote = function () {
-	this.quoteIndex = this.body.indexOf("<blockquote");
-
-	return this.quoteIndex;
-};
-
-go.util.QuoteStripper.prototype._splitLines = function () {
-	if (!this.lines) {
-		var br = '|BR|';
-
-		var html = this.body
-			.replace(/<\/p>/ig, br + "$&")
-			.replace(/<\/div>/ig, br + "$&")
-			.replace(/<br[^>]*>/ig, br + "$&");
-
-		this.lines = html.split(br);
-	}
-	return this.lines;
-};
-
-
-/**
- * eg
- *
- * Van: Merijn Schering [mailto:mschering@intermesh.nl]
-	  Verzonden: donderdag 20 november 2014 16:40
-	  Aan: Someone
-	  Onderwerp: Subject
- *
- * @return int|boolean
- */
-go.util.QuoteStripper.prototype._findQuoteByHeaderBlock = function() {
-
-	var lines = this._splitLines(this.body);
-
-	var pos = 0;
-
-	for (var i = 0, c = lines.length; i < c; i++) {
-
-		var plain = lines[i].replace(/(<([^>]+)>)/ig,""); //strip html tags
-		var pattern = /[a-z]+:\s*[a-z0-9\._\-+\&]+@[a-z0-9\.\-_]+/i;
-		//Match:
-		//ABC: email@domain.com
-		if (plain.match(pattern)) {
-			return pos;
-		}
-
-		pos += lines[i].length;
-	}
-	return -1;
-};
+go.util.QuoteStripper = QuoteStripper;
