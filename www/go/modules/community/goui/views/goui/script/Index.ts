6d7e46227ca78ffe3d95103c3368c8e4e93765c1
Merijn Schering <mschering@intermesh.nl>
2022-11-03 14:17:32 +0100

    Support client without tasks module

--- www/go/modules/community/goui/views/goui/script/Index.ts
+++ www/go/modules/community/goui/views/goui/script/Index.ts
@@ -1,11 +1,21 @@
 import {modules} from "@go-core/Modules.js";
 import {Main} from "./Main.js";
 import {router} from "@go-core/Router.js";
+import {authManager} from "@go-core/auth/AuthManager.js";
+import {client} from "@goui/jmap/Client.js";
 
 modules.register(  {
 	package: "community",
 	name: "goui",
-	init () {
+	async init () {
+
+		// TODO
+		// const user = await authManager.requireLogin();
+		//
+		// if(!client.session.capabilities["go:community:goui"]) {
+		// 	// User has no access to this module
+		// 	return;
+		// }
 
 		let notes: Main;
 
