d21f4359d3f077be65f388062a450fa72ddfb5f9
Merijn Schering <mschering@intermesh.nl>
2022-11-17 10:48:46 +0100

    Range download

--- www/go/core/fs/File.php
+++ www/go/core/fs/File.php
@@ -377,7 +377,12 @@ class File extends FileSystemObject {
 				$r->setHeader('Pragma', 'no-cache');
 				$r->removeHeader('Last-Modified');
 			}
-		}		
+		}
+
+		if (isset($_SERVER['HTTP_RANGE'])){
+			$this->rangeDownload($this->getPath());
+			return;
+		}
 		
 		if(ob_get_contents() != '') {			
 			throw new Exception("Could not output file because output has already been sent. Turn off output buffering to find out where output has been started.");
@@ -399,7 +404,93 @@ class File extends FileSystemObject {
 			flush();
 		}
 	}
-	
+
+	private function rangeDownload($file) {
+
+		$fp = fopen($file, 'rb');
+
+		$size   = filesize($file); // File size
+		$length = $size;           // Content length
+		$start  = 0;               // Start byte
+		$end    = $size - 1;       // End byte
+		Response::get()->setHeader("Accept-Ranges", "0-$length");
+		// header('Accept-Ranges: bytes');
+		// multipart/byteranges
+		// http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
+		if (isset($_SERVER['HTTP_RANGE'])) {
+
+			$c_start = $start;
+			$c_end   = $end;
+			// Extract the range string
+			list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+			// Make sure the client hasn't sent us a multibyte range
+			if (strpos($range, ',') !== false) {
+
+				// (?) Shoud this be issued here, or should the first
+				// range be used? Or should the header be ignored and
+				// we output the whole content?
+				header('HTTP/1.1 416 Requested Range Not Satisfiable');
+				Response::get()->setHeader("Content-Range", "bytes $start-$end/$size");
+				// (?) Echo some info to the client?
+				exit;
+			}
+			// If the range starts with an '-' we start from the beginning
+			// If not, we forward the file pointer
+			// And make sure to get the end byte if spesified
+			if ($range[0] == '-') {
+
+				// The n-number of the last bytes is requested
+				$c_start = $size - substr($range, 1);
+			}
+			else {
+
+				$range  = explode('-', $range);
+				$c_start = $range[0];
+				$c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+			}
+			/* Check the range and make sure it's treated according to the specs.
+			 * http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
+			 */
+			// End bytes can not be larger than $end.
+			$c_end = ($c_end > $end) ? $end : $c_end;
+			// Validate the requested range and return an error if it's not correct.
+			if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+
+				header('HTTP/1.1 416 Requested Range Not Satisfiable');
+				Response::get()->setHeader("Content-Range,", "bytes $start-$end/$size");
+				// (?) Echo some info to the client?
+				exit;
+			}
+			$start  = $c_start;
+			$end    = $c_end;
+			$length = $end - $start + 1; // Calculate new content length
+			fseek($fp, $start);
+			header('HTTP/1.1 206 Partial Content');
+		}
+		// Notify the client the byte range we'll be outputting
+		Response::get()->setHeader("Content-Range", "bytes $start-$end/$size");
+		Response::get()->setHeader("Content-Length", "$length");
+
+		// Start buffered download
+		$buffer = 1024 * 8;
+		while(!feof($fp) && ($p = ftell($fp)) <= $end) {
+
+			if ($p + $buffer > $end) {
+
+				// In case we're only outputtin a chunk, make sure we don't
+				// read past the length
+				$buffer = $end - $p + 1;
+			}
+			set_time_limit(0); // Reset time limit for big files
+			echo fread($fp, $buffer);
+			flush(); // Free up memory. Otherwise large files will trigger PHP's memory limit.
+		}
+
+		fclose($fp);
+
+	}
+
+
 	/**
 	 * Open file pointer
 	 * 
