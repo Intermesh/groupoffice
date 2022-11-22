24b06ea034ec48c0b43879010ae98c55593888c2
Merijn Schering <mschering@intermesh.nl>
2022-11-22 09:14:22 +0100

    Renamed google authenticator to OTP authenticator

--- www/go/modules/community/googleauthenticator/install/updates.inc.php
+++ www/go/modules/community/googleauthenticator/install/updates.inc.php
@@ -3,7 +3,10 @@ $updates['202012210922'][] = "ALTER TABLE `googleauth_secret` DROP FOREIGN KEY `
 $updates['202012210922'][] = "ALTER TABLE `googleauth_secret` ADD CONSTRAINT `googleauth_secret_user` FOREIGN KEY (`userId`) REFERENCES `core_user`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;";
 
 $updates['202107090900'][] = "insert ignore into core_acl_group select aclId, '2', 10 from core_module where package='community' and name ='googleauthenticator'";
-
+$updates['202211211602'][] = "";
+$updates['202211211602'][] = "";
+$updates['202211211602'][] = "";
+$updates['202211211602'][] = "";
 
 $updates['202211211602'][] = "rename table googleauth_secret to otp_secret;";
 $updates['202211211602'][] = "UPDATE core_auth_method t SET t.id = 'otpauthenticator' WHERE t.id LIKE 'googleauthenticator'";
