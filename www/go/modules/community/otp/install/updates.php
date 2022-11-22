0c733bb9ed7b18a19502a1c8a6a23f51b0ef7486
Merijn Schering <mschering@intermesh.nl>
2022-11-22 09:26:29 +0100

    Renamed google authenticator to OTP authenticator

--- www/go/modules/community/otp/install/updates.php
+++ www/go/modules/community/otp/install/updates.php
@@ -1,11 +1,14 @@
 <?php
-$updates['202012210922'][] = "ALTER TABLE `otp_secret` DROP FOREIGN KEY `otp_secret_user`; ";
-$updates['202012210922'][] = "ALTER TABLE `otp_secret` ADD CONSTRAINT `otp_secret_user` FOREIGN KEY (`userId`) REFERENCES `core_user`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;";
+$updates['202012210922'][] = "ALTER TABLE `googleauth_secret` DROP FOREIGN KEY `googleauth_secret_user`; ";
+$updates['202012210922'][] = "ALTER TABLE `googleauth_secret` ADD CONSTRAINT `googleauth_secret_user` FOREIGN KEY (`userId`) REFERENCES `core_user`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;";
 
-$updates['202107090900'][] = "insert ignore into core_acl_group select aclId, '2', 10 from core_module where package='community' and name ='otp'";
-
-$updates['202209191423'][] = "alter table otp_secret
+$updates['202107090900'][] = "insert ignore into core_acl_group select aclId, '2', 10 from core_module where package='community' and name ='googleauthenticator'";
+$updates['202209191423'][] = "alter table googleauth_secret
     add verified bool default false not null;";
 
-$updates['202209191423'][] = "update otp_secret
+$updates['202209191423'][] = "update googleauth_secret
     set verified = true;";
+
+$updates['202211211602'][] = "rename table googleauth_secret to otp_secret;";
+$updates['202211211602'][] = "UPDATE core_auth_method t SET t.id = 'otpauthenticator' WHERE t.id LIKE 'googleauthenticator'";
+$updates['202211211602'][] = "UPDATE core_module t SET t.name = 'otp', version=0 WHERE t.name = 'googleauthenticator';";
