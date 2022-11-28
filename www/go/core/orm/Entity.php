e98557d9ce354f375fa7ab403f38f3fb6022831e
Merijn Schering <mschering@intermesh.nl>
2022-11-28 08:56:46 +0100

    type in exists function

--- www/go/core/orm/Entity.php
+++ www/go/core/orm/Entity.php
@@ -208,13 +208,11 @@ abstract class Entity extends Property {
 	 * Check if an ID exists in the database in the most efficient way. It also caches the result
 	 * during the same request.
 	 *
-	 * @param $id
+	 * @param string|null $id
 	 * @return bool
-	 * @throws Exception
 	 */
-	public static function exists($id): bool
+	public static function exists(?string $id): bool
 	{
-
 		if(empty($id)) {
 			return false;
 		}
