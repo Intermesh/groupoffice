8d2a6ba82710a74a17d32cdca7957cfdb1057ea9
Merijn Schering <mschering@intermesh.nl>
2022-12-16 12:11:11 +0100

    Autodiscover

--- www/modules/z-push/backend/go/autodiscover.php
+++ www/modules/z-push/backend/go/autodiscover.php
@@ -25,16 +25,19 @@ if(strpos($_SERVER['PHP_AUTH_USER'], '@') !== false) {
 function error($text) {
 	file_put_contents( 'request.log', $_SERVER['PHP_AUTH_USER'].': ' . $text .  "\n", FILE_APPEND );
 	http_response_code(500);
-    exit();
+	exit();
 }
 
 const SCHEMA = 'http://schemas.microsoft.com/exchange/autodiscover/mobilesync/responseschema/2006';
+$asHost = 'https://' . ($_SERVER['HTTP_HOST'] ? : $_SERVER['SERVER_NAME']) . '/Microsoft-Server-ActiveSync';
+
+
+file_put_contents('request.log', var_export(apache_request_headers(), true), FILE_APPEND);
 
 // optional debug log
-//file_put_contents( 'request.log', $request, FILE_APPEND );
+file_put_contents( 'request.log', $request . "\n\n", FILE_APPEND );
 
 // retrieve email address from client request
-
 preg_match( '/<EMailAddress>(.*?)<\/EMailAddress>/', $request, $email );
 
 preg_match("/<AcceptableResponseSchema>(.*?)<\/AcceptableResponseSchema>/", $request, $schema);
@@ -45,7 +48,8 @@ if(!isset($schema[1])) {
 if(!isset($email[1])) {
 	error('No E-Mail provided');
 }
-//file_put_contents( 'request', $email[1] .' '. $_SERVER['PHP_AUTH_USER'] . '; ', FILE_APPEND );
+
+file_put_contents( 'request', $email[1] .' '. $_SERVER['PHP_AUTH_USER'] . "\n\n", FILE_APPEND );
 
 // check for invalid mail, to prevent XSS
 if (filter_var($email[1], FILTER_VALIDATE_EMAIL) === false) {
@@ -54,27 +58,28 @@ if (filter_var($email[1], FILTER_VALIDATE_EMAIL) === false) {
 if($schema[1] !== SCHEMA) {
 	error('Invalid schema requested');
 }
-//list($alias, $domain) = explode('@', $email);
-// get domain from email address
-//$domain = substr( strrchr( $email, "@" ), 1 );
-//set Content-Type
-header( 'Content-Type: text/xml; charset=utf-8' );
-?><?xml version="1.0" encoding="utf-8"?>
+
+$response = '<?xml version="1.0" encoding="utf-8"?>
 <Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006">
-    <Response xmlns="<?=SCHEMA?>">
+    <Response xmlns="'.SCHEMA.'">
         <Culture>en:us</Culture>
         <User>
-            <DisplayName><?=$email[1]?></DisplayName>
-            <EMailAddress><?=$email[1]?></EMailAddress>
+            <DisplayName>'.$email[1].'</DisplayName>
+            <EMailAddress>'.$email[1].'</EMailAddress>
         </User>
         <Action>
             <Settings>
                 <Server>
                     <Type>MobileSync</Type>
-                    <Url>https://<?=$_SERVER['SERVER_NAME']?>/Microsoft-Server-ActiveSync</Url>
-                    <Name>https://<?=$_SERVER['SERVER_NAME']?>/Microsoft-Server-ActiveSync</Name>
+                    <Url>' . $asHost . '</Url>
+                    <Name>' . $asHost . '</Name>
                 </Server>
             </Settings>
         </Action>
     </Response>
-</Autodiscover>
+</Autodiscover>';
+
+file_put_contents( 'request.log', $response ."\n\n", FILE_APPEND );
+
+header( 'Content-Type: text/xml; charset=utf-8' );
+echo $response;
