eb8849f745a317903272e9466563e61f3fe35bb6
Merijn Schering <mschering@intermesh.nl>
2023-01-06 10:51:06 +0100

    Auth

--- www/api/auth.php
+++ www/api/auth.php
@@ -56,10 +56,7 @@ function finishLogin(Token $token, string $rememberMeToken = null) {
 	$authState->setToken($token);
 	go()->setAuthState($authState);
 
-	if(!$token->isAuthenticated()) {
-		$token->setAuthenticated();
-	}
-	$token->setCookie();
+	$token->setAuthenticated();
 
 	$response = $authState->getSession();
 
