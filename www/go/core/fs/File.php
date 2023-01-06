286c12cc2011db7c33001b39ef601f5affaa098a
Merijn Schering <mschering@intermesh.nl>
2023-01-06 10:08:00 +0100

    Etag
    (cherry picked from commit f5fac97ec1b76368083888b11f1e7fd4f450964a)

--- www/go/core/fs/File.php
+++ www/go/core/fs/File.php
@@ -368,7 +368,7 @@ class File extends FileSystemObject {
 					$r->setModifiedAt($this->getModifiedAt());
 				}
 				if(!$r->hasHeader('Etag')) {
-					$r->setETag($this->getMd5Hash());
+					$r->setETag('"' . $this->getMd5Hash() . '"');
 				}
 				$r->abortIfCached();
 			} else{
