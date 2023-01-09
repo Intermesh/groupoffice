6ec41b353633dfab271b018a090ec738c185d113
Merijn Schering <mschering@intermesh.nl>
2023-01-09 14:40:33 +0100

    Archive user removes them from groups

--- www/go/core/model/User.php
+++ www/go/core/model/User.php
@@ -518,9 +518,8 @@ class User extends AclItemEntity {
 			}
 		}
 
-		if($this->isModified('groups')) {	
-			
-			
+		if($this->isModified('groups')) {
+
 			if(!in_array(Group::ID_EVERYONE, $this->groups)) {
 				$this->groups[] = Group::ID_EVERYONE;
 				// $this->setValidationError('groups', ErrorCode::INVALID_INPUT, go()->t("You can't remove group everyone"));
@@ -761,6 +760,9 @@ class User extends AclItemEntity {
 	
 	protected function internalSave(): bool
 	{
+		if($this->archive) {
+			$this->groups = [Group::ID_EVERYONE, $this->getPersonalGroup()->id];
+		}
 		
 		if(isset($this->plainPassword)) {
 			$this->password = $this->passwordHash($this->plainPassword);
