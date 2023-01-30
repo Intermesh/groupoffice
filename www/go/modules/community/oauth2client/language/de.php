f27a3c065142563c53bf7335c815d1dcffbb3ce9
Merijn Schering <mschering@intermesh.nl>
2023-01-30 12:23:57 +0100

    Updated German translation

---
+++ www/go/modules/community/oauth2client/language/de.php
@@ -0,0 +1,7 @@
+<?php
+return array (
+  'OAuth2 Client Settings' => 'OAuth2-Client-Einstellungen',
+  'Get a refresh token' => 'Token aktualisieren',
+  'OAuth2 client' => 'OAuth2-Client',
+  'OAuth2 Connection' => 'OAuth2-Verbindung',
+);
