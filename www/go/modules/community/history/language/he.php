a541232c23182f90e568c12d872cfb79ae629aa7
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2023-03-20 11:29:32 +0100

    Cherry-pick Hebrew translations into 6.7

---
+++ www/go/modules/community/history/language/he.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  'History' => 'היסטוריה',
+  'Changes' => 'שינויים',
+  'ID' => 'מזהה (ID)',
+  'Item was never modified' => 'הפריט מעולם לא השתנה',
+  'Delete entries after' => 'מחיקת ערכים לאחר',
+  'All users' => 'כל המשתמשים',
+  'Create' => 'יצירת',
+  'Update' => 'עדכון',
+  'Bad login' => 'ההתחברות נכשלה',
+  'Types' => 'סוגים',
+);
