cc2b2f1290dc04824316a109184a974e40a3720a
Merijn Schering <mschering@intermesh.nl>
2023-03-31 13:56:40 +0200

    Cleanup

--- www/api/jmap.php
+++ www/api/jmap.php
@@ -1,5 +1,6 @@
 <?php
 use go\core\App;
+use go\core\jmap\Response;
 use go\core\jmap\Router;
 use go\core\jmap\Request;
 use go\core\jmap\State;
@@ -12,22 +13,18 @@ App::get()->setAuthState(new State());
 switch(Request::get()->getMethod() ) {
 
 	case 'POST':
-	$router = new Router();
-	$router->run();
-
+		$router = new Router();
+		$router->run();
 	break;
 
 	case 'GET':
-
 		App::get()->getAuthState()->outputSession();
-
 		break;
 
 	case 'OPTIONS':
-		\go\core\jmap\Response::get()->sendHeaders();
+		Response::get()->sendHeaders();
 		break;
 
 	default:
-
-	throw new Exception("Method " . Request::get()->getMethod() . " not supported");
+		throw new Exception("Method " . Request::get()->getMethod() . " not supported");
 }
