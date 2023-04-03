3dd447df73c462eddd358a662e7b287441bc6457
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2023-04-03 09:24:11 +0200

    30741 Japanese translations

---
+++ www/go/modules/community/apikeys/language/jp.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+  'Add key' => 'キーを追加',
+  'View access token' => 'アクセストークンを表示',
+  'API Keys' => 'APIキー',
+  'API keys can be used for other services to connect to the API. A website feeding contact information for example.' => 'API キーは、他のサービスが API に接続するために使用できます。たとえば、連絡先情報を提供する Web サイトです。',
+  'API Key' => 'APIキー',
+);
---
+++ www/go/modules/community/serverclient/language/jp.php
@@ -0,0 +1,6 @@
+<?php
+return array (
+  'Create a mailbox for domain' => 'ドメインのメールボックスを作成する',
+  'Mailboxes' => 'メールボックス',
+  'The mailbox couldn\'t be created' => 'メールボックスを作成できませんでした',
+);
