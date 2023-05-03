8ea9fffb5c4e0848784e309ae6dd204bc9e00f12
dehart <derinus@gmail.com>
2023-05-03 12:23:52 +0200

    only show the alerts when triggerAt is larger then or equal to now

--- www/go/core/views/extjs3/Alert.js
+++ www/go/core/views/extjs3/Alert.js
@@ -64,7 +64,10 @@
 		},
 
 		show : function(alert) {
-			const now = new Date(), id = 'core-alert-' + alert.id;
+			const now = new Date(), triggerDate = new Date (alert.triggerAt), id = 'core-alert-' + alert.id;
+			debugger;
+
+			if(triggerDate > now) return;
 
 			go.Db.store(alert.entity).single(alert.entityId).then((entity) => {
 
