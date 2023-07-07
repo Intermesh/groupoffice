8eb6e833a577563f1250965c168c11feba9adbdd
Merijn Schering <mschering@intermesh.nl>
2023-07-07 11:58:45 +0200

    General group-office components

--- www/go/modules/community/goui/views/goui/script/NoteDialog.ts
+++ www/go/modules/community/goui/views/goui/script/NoteDialog.ts
@@ -1,161 +1,60 @@
 import {notebookcombo} from "./NoteBookCombo.js";
-import {
-	btn,
-	CardContainer,
-	cardmenu,
-	cards,
-	comp,
-	containerfield,
-	Fieldset,
-	fieldset,
-	form,
-	Form,
-	htmlfield,
-	Notifier,
-	root,
-	t,
-	tbar,
-	textfield,
-	Window,
-	EntityID, datasourceform, DataSourceForm
-} from "@intermesh/goui";
-import {client, Image, jmapds} from "@intermesh/groupoffice-core";
+import {comp, htmlfield, Notifier, root, t, textfield} from "@intermesh/goui";
+import {client, FormWindow, Image} from "@intermesh/groupoffice-core";
 
-export class NoteDialog extends Window {
-	readonly form: DataSourceForm;
-
-	private currentId?: EntityID;
-	private cards: CardContainer;
-	private general: Fieldset;
+export class NoteDialog extends FormWindow {
 
 	constructor() {
-		super();
+		super("Note");
 
-		this.cls = "vbox";
 		this.title = t("Note");
-		this.width = 600;
-		this.height = 400;
+
 		this.stateId = "note-dialog";
 		this.maximizable = true;
 
-		this.items.add(
-			this.form = datasourceform(
-				{
-					dataSource: jmapds("Note"),
-					cls: "vbox",
-					flex: 1,
-					listeners: {
-						submit: ()=> {
-							this.close();
-						}
-					}
-				},
-				cardmenu(),
+		this.generalTab.items.add(
+			comp({cls: "hbox gap"},
+				textfield({
+					flex: 2,
+					name: "name",
+					label: t("Name"),
+					required: true
+				}),
 
-				this.cards = cards({flex: 1},
-					this.general = fieldset({cls: "scroll fit", title: t("General")},
+				notebookcombo({
+					flex: 1
+				}),
+			),
 
-						comp({cls: "hbox gap"},
-							textfield({
-								flex: 2,
-								name: "name",
-								label: t("Name"),
-								required: true
-							}),
+			htmlfield({
+				name: "content",
+				listeners: {
 
-							notebookcombo({
-								flex: 1
-							}),
-						),
+					setvalue: (field, newValue, oldValue) => {
+						Image.replaceImages(field.el);
+					},
 
-						htmlfield({
-							name: "content",
-							listeners: {
+					insertimage: (htmlfield, file, img) => {
+						root.mask();
 
-								setvalue: (field, newValue, oldValue) => {
-									Image.replaceImages(field.el);
-								},
-
-								insertimage: (htmlfield, file, img) => {
-									root.mask();
-
-									client.upload(file).then(r => {
-										if (img) {
-											img.dataset.blobId = r.id;
-											img.removeAttribute("id");
-										}
-										Notifier.success("Uploaded " + file.name + " successfully");
-									}).catch((err) => {
-										console.error(err);
-										Notifier.error("Failed to upload " + file.name);
-									}).finally(() => {
-										root.unmask();
-									});
-								}
+						client.upload(file).then(r => {
+							if (img) {
+								img.dataset.blobId = r.id;
+								img.removeAttribute("id");
 							}
-						}),
-
-
-
-					)
-				),
-
-
-				tbar({cls: "border-top"},
-					"->",
-					btn({
-						type: "submit",
-						text: t("Save")
-					})
-				)
-			)
-		)
+							Notifier.success("Uploaded " + file.name + " successfully");
+						}).catch((err) => {
+							console.error(err);
+							Notifier.error("Failed to upload " + file.name);
+						}).finally(() => {
+							root.unmask();
+						});
+					}
+				}
+			})
+		);
 
 		this.addCustomFields();
 	}
 
-	public async load(id: EntityID) {
-
-		this.mask();
-
-		try {
-			await this.form.load(id);
-			this.currentId = id;
-		} catch (e) {
-			void Window.alert(t("Error"), e + "");
-		} finally {
-			this.unmask();
-		}
-
-		return this;
-	}
-
-
-	private addCustomFields() {
-		const es = "Note"
-		if (go.Entities.get(es).customFields) {
-			const fieldsets = go.customfields.CustomFields.getFormFieldSets(es);
-			fieldsets.forEach((fs: any) => {
-
-				//replace customFields. because we will use a containerfield here.
-				fs.cascade((item: any) => {
-					if (item.getName) {
-						let fieldName = item.getName().replace('customFields.', '');
-						item.name = item.hiddenName =  fieldName;
-					}
-				});
-
-				if (fs.fieldSet.isTab) {
-					fs.title = null;
-					fs.collapsible = false;
-
-					this.cards.items.add(containerfield({name: "customFields", cls: "scroll", title: fs.fieldSet.name}, fs));
-				} else {
-					//in case formPanelLayout is set to column
-					fs.columnWidth = 1;
-					this.general.items.add(containerfield({name: "customFields"}, fs));
-				}
-			}, this);
-		}
-	}
 }
