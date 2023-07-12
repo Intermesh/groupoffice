4baac78d620dd908ccfa0b619e3e4f8912c4bcc5
dehart <derinus@gmail.com>
2023-07-12 17:20:20 +0200

    email SMTP in newsletter selectable
    (cherry picked from commit 6c649f19106f36fec44bfb6de3e29d9f37b87eb6)

--- www/go/core/views/extjs3/smtp/AccountCombo.js
+++ www/go/core/views/extjs3/smtp/AccountCombo.js
@@ -16,7 +16,28 @@ go.smtp.AccountCombo = Ext.extend(go.form.ComboBox, {
 		xtype: "gostore",
 		fields: ['id', 'fromName', 'fromEmail', 'host', {name: 'name', convert: function(v, rec) {return rec.fromName + ": " + rec.fromEmail;}}],
 		entityStore: "SmtpAccount",
-		sortBy: 'name'
+		sortBy: 'name',
+		listeners: {
+			'load': (store) => {
+				// fetch the rest from emai laccount and append
+				GO.request({
+					url: 'email/account/store',
+					success: (options, response, result) =>
+					{
+						for(const item of result.results) {
+							store.add(new store.recordType({
+								id:'old_'+item.id,
+								host: item.smtp_host,
+								name: item.name + ' - ' + item.email,
+								fromName: item.name,
+								fromEmail: item.email
+							}, 'old_'+item.id));
+						}
+					}
+				});
+
+			}
+		}
 	}
 });
 
