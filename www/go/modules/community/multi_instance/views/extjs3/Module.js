f65ba74bcdfa4552ab73d46fe0e3f081fb0b79ea
Merijn Schering <mschering@intermesh.nl>
2023-08-15 16:53:01 +0200

    Sent automated followup for instances

--- www/go/modules/community/multi_instance/views/extjs3/Module.js
+++ www/go/modules/community/multi_instance/views/extjs3/Module.js
@@ -3,7 +3,38 @@ Ext.ns('go.modules.community.multi_instance');
 go.Modules.register("community", 'multi_instance', {
 	mainPanel: "go.modules.community.multi_instance.MainPanel",
 	title: t("Multi instance"),
-	entities: ["Instance"],
+	entities: [{
+		name:"Instance",
+		filters: [
+			{
+				wildcards: false,
+				name: 'text',
+				type: "string",
+				multiple: false,
+				title: t("Query")
+			},
+			{
+				title: t("Modified at"),
+				name: 'modifiedat',
+				multiple: false,
+				type: 'date'
+			}, {
+				title: t("Modified by"),
+				name: 'modifiedBy',
+				multiple: true,
+				type: 'string'
+			}, {
+				title: t("Created at"),
+				name: 'createdat',
+				multiple: false,
+				type: 'date'
+			}, {
+				title: t("Created by"),
+				name: 'createdby',
+				multiple: true,
+				type: 'string'
+			}]
+	}],
 	initModule: function () {	
 		
 	}
