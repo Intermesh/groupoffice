3dc63311c6b7bfc3a1befb005c7b38c7c3af69b7
Merijn Schering <mschering@intermesh.nl>
2023-10-24 10:34:33 +0200

    Don't write to disk cache when using apcu

--- www/go/core/cache/Apcu.php
+++ www/go/core/cache/Apcu.php
@@ -52,7 +52,9 @@ class Apcu implements CacheInterface {
 	 */
 	public function set($key, $value, $persist = true, $ttl = 0) {
 
-		$this->getDiskCache()->set($key, $value, $persist, $ttl);
+		if(!$this->apcuEnabled) {
+			return $this->getDiskCache()->set($key, $value, $persist, $ttl);
+		}
 		if($persist) {
 			apcu_store($this->prefix . '-' .$key, $value, $ttl);
 		}
