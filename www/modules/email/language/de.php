982b46a0e0fb7b85a851792318a5f859affe37d5
Merijn Schering <mschering@intermesh.nl>
2024-01-29 10:21:21 +0100

    Updated German translation

--- www/modules/email/language/de.php
+++ www/modules/email/language/de.php
@@ -355,5 +355,8 @@ Settings -> Accounts -> Double click account -> Folders.' => 'Ihre Mailbox ist v
   'None' => 'Ohne',
   'Failed to send the request to the server. Please check your internet connection.' => 'Die Anforderung konnte nicht an den Server gesendet werden. Bitte überprüfen Sie Ihre Internetverbindung.',
   'Email address succesfully added to contact' => 'Die E-Mail-Adresse wurde dem Kontakt erfolgreich hinzugefügt',
-	'' => '',
+  '' => '',
+  'Refresh token' => 'Token aktualisieren',
+  'Your token has possibly expired. A new window will be opened in which you can renew your token.' => 'Ihr Token ist möglicherweise abgelaufen. Es wird ein neues Fenster geöffnet, in dem Sie Ihren Token erneuern können.',
+  'Template group' => 'Vorlagengruppe',
 );
