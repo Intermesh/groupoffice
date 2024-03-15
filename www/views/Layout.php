b1e00732514338068069553d543191b9e4b45efe
Merijn Schering <mschering@intermesh.nl>
2024-03-15 13:14:32 +0100

    auth firing

--- www/views/Layout.php
+++ www/views/Layout.php
@@ -64,8 +64,8 @@ $lang = go()->getLanguage()->getIsoCode();
 <?php endif;?>
 
 <?php if($loadGoui): ?>
-    <link rel="stylesheet" media="screen,print" href="<?= $webclient->getBaseUrl();?>views/goui/dist/goui/style/<?= $gouiStyleSheet ?>" />
-    <link rel="stylesheet" media="screen" href="<?= $webclient->getBaseUrl();?>views/goui/dist/groupoffice-core/style/style.css" />
+<!--    <link rel="stylesheet" media="screen,print" href="--><?php //= $webclient->getBaseUrl();?><!--views/goui/dist/goui/style/--><?php //= $gouiStyleSheet ?><!--" />-->
+    <link rel="stylesheet" media="screen" href="<?= $webclient->getBaseUrl();?>views/goui/dist/groupoffice-core/style/<?= $gouiStyleSheet ?>" />
 <?php endif; ?>
 
 <?php if(!empty(GO()->getConfig()['custom_css_url'])): ?>
