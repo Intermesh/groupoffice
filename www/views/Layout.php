a170342382fb4272f9c2f8904974ab6646b6837d
Merijn Schering <mschering@intermesh.nl>
2024-03-15 13:31:43 +0100

    Switchted to older branch

--- www/views/Layout.php
+++ www/views/Layout.php
@@ -65,7 +65,7 @@ $lang = go()->getLanguage()->getIsoCode();
 
 <?php if($loadGoui): ?>
 <!--    <link rel="stylesheet" media="screen,print" href="--><?php //= $webclient->getBaseUrl();?><!--views/goui/dist/goui/style/--><?php //= $gouiStyleSheet ?><!--" />-->
-    <link rel="stylesheet" media="screen" href="<?= $webclient->getBaseUrl();?>views/goui/dist/groupoffice-core/style/<?= $gouiStyleSheet ?>" />
+    <link rel="stylesheet" media="screen" href="<?= $webclient->getBaseUrl();?>views/goui/dist/goui/style/<?= $gouiStyleSheet ?>" />
 <?php endif; ?>
 
 <?php if(!empty(GO()->getConfig()['custom_css_url'])): ?>
