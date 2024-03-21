f227d0d2af03ab2118c5c4e2193b70177bc76c73
Merijn Schering <mschering@intermesh.nl>
2024-03-21 09:13:06 +0100

    pwnd icon

--- www/go/modules/community/pwned/language/en.php
+++ www/go/modules/community/pwned/language/en.php
@@ -1,5 +1,5 @@
 <?php
 return array (
 	"name" => "Have I been Pwned",
-	"description" => "Improves security by checking haveibeenpwned.com on login and change password."
+	"description" => "Improves security by checking haveibeenpwned.com on web login and when a user changes his password."
 );
