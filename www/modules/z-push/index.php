4c0ec2503356e9f84f9799c439464f954469abcc
Merijn Schering <mschering@intermesh.nl>
2024-04-11 11:17:11 +0200

    Version update

--- www/modules/z-push/index.php
+++ www/modules/z-push/index.php
@@ -1,5 +1,5 @@
 <?php
-define("ZPUSH_VERSION", "2.6.1");
+define("ZPUSH_VERSION", "2.7.1");
 define("ZPUSH_DIR", dirname(__DIR__, 2) . "/go/modules/community/activesync/Z-Push/src/");
 
 require(ZPUSH_DIR . 'vendor/autoload.php');
