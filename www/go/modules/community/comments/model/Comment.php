62c0d3f2bd1438cb35be348c117f1d5cdfc014dd
Merijn Schering <mschering@intermesh.nl>
2024-05-21 08:58:04 +0200

    Filter out private comments

--- www/go/modules/community/comments/model/Comment.php
+++ www/go/modules/community/comments/model/Comment.php
@@ -177,12 +177,22 @@ class Comment extends AclItemEntity {
 	}
 
 	/**
-	 * @param Entity|ActiveRecord $entity
+	 * Find comments for a given entity
+	 *
+	 * Note: this method finds all comments regardless of the {@see $section}
+	 *
+	 *
+	 * @example To filter by section:
+	 * ```
+	 * $comments = Comment::findFor($entity)->where('section', '=', 'foo');
+	 * ```
+	 *
+	 * @param ActiveRecord|Entity $entity
 	 * @param array $properties
-	 * @return Query|Comment[]
+	 * @return Query<Comment>
 	 * @throws Exception
 	 */
-	public static function findFor($entity, array $properties = []) {
+	public static function findFor(ActiveRecord|Entity $entity, array $properties = []) : Query {
 		$entityTypeId = $entity->entityType()->getId();
 		$entityId = $entity->id;
 
