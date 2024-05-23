08eb8edd1a28df5ec7aea7bc51cdba1d90f89b24
Merijn Schering <mschering@intermesh.nl>
2024-05-23 14:52:28 +0200

    init submodules
    (cherry picked from commit c1182bf10789a564a50c5c20a46ba27a0d19022a)

--- build/build.php
+++ build/build.php
@@ -136,8 +136,8 @@ class Builder
 		cd(dirname(__DIR__));
 
 		run("git fetch");
-		run("git checkout " . $this->gitBranch);
-		run("git pull --recurse-submodules");
+		run("git pull");
+        run("git submodule init --update");
 
 		cd($this->sourceDir);
 
