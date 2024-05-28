3f6f07a45fdca5a07f850b176668711567e8c308
Merijn Schering <mschering@intermesh.nl>
2024-05-28 10:45:33 +0200

    Deploy

--- scripts/deploy.php
+++ scripts/deploy.php
@@ -14,7 +14,7 @@ echo "Running: " . $rsyncCmd . "\n";
 system($rsyncCmd);
 
 chdir($target);
-$upgradeCmd = "sudo -u www-data php cli.php core/System/upgrade -c=".$manageConfig;
+$upgradeCmd = "sudo -u www-data php www/cli.php core/System/upgrade -c=".$manageConfig;
 echo "Running: " . $upgradeCmd . "\n";
 system($upgradeCmd);
 
