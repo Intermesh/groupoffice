44ed11ba4cd3a2fc4dde17437ecf46b79e4c09c7
Merijn Schering <mschering@intermesh.nl>
2024-06-07 16:18:40 +0200

    Load goui

--- www/views/Extjs3/themes/Paper/pageHeader.php
+++ www/views/Extjs3/themes/Paper/pageHeader.php
@@ -2,7 +2,7 @@
 
 $bodyCls = $bodyCls ?? 'go-page';
 $loadExt = $loadExt ?? false;
-$loadGoui = $loadGoui ?? false;
+$loadGoui = $loadGoui ?? true;
 require(go\core\webclient\Extjs3::get()->getBasePath().'/views/Layout.php');?>
 
 <header>
