26ffa3b29b4dfe458e296d71b3e713ea36e030d2
Merijn Schering <mschering@intermesh.nl>
2024-06-11 09:51:23 +0200

    Remove duplicate function

--- www/go/core/orm/Entity.php
+++ www/go/core/orm/Entity.php
@@ -678,15 +678,6 @@ abstract class Entity extends Property {
 		static::$entityType = [];
 	}
 
-
-	public static function clearCache() : void
-	{
-		parent::clearCache();
-
-		static::$entityType = [];
-	}
-
-
 	/**
 	 * Gets an ID from the database for this class used in database relations and
 	 * routing short routes like "Note/get"
