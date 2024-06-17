770fb8d0c3dd6bb2e935d95e4d37b52451951892
Merijn Schering <mschering@intermesh.nl>
2024-06-17 13:14:12 +0200

    Fixed: Notes exportation, small request - category name #786

--- www/go/modules/community/notes/convert/Spreadsheet.php
+++ www/go/modules/community/notes/convert/Spreadsheet.php
@@ -3,16 +3,29 @@
 
 namespace go\modules\community\notes\convert;
 
-use Exception;
 use go\core\data\convert;
-use go\core\fs\File;
-use go\core\model\Acl;
-use go\core\orm\Entity;
 use go\modules\community\notes\model\Note;
+use go\modules\community\notes\model\NoteBook;
+use go\modules\community\tasks\model\TaskList;
 
 
 class Spreadsheet extends convert\Spreadsheet
 {
 	public static $excludeHeaders = [ 'password', 'images'];
+
+	protected function init() {
+		$this->addColumn('notebook', go()->t('Notebook', 'community', 'notes'));
+	}
+	protected function exportList(Note $note) {
+		$nb = NoteBook::findById($note->noteBookId, ['name']);
+		return $nb->name;
+	}
+
+	protected function importList(Note $note, $value, array $values) {
+		$nb = TaskList::find(['id'])->where('name', '=', $value);
+		if($nb) {
+			$note->noteBookId = $nb->id;
+		}
+	}
 
 }
