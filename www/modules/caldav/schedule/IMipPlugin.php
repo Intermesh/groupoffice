b411d86843876b7da0e3c1f93065dc4063a5e1d3
Merijn Schering <mschering@intermesh.nl>
2024-08-05 17:04:31 +0200

    Caldav: Fixed sending IMIP email invitations to participants with a comma in them

--- www/modules/caldav/schedule/IMipPlugin.php
+++ www/modules/caldav/schedule/IMipPlugin.php
@@ -5,6 +5,7 @@ namespace GO\Caldav\Schedule;
 
 
 use go\core\ErrorHandler;
+use go\core\mail\Address;
 use go\core\mail\AddressList;
 use go\core\model\Module;
 
@@ -56,8 +57,14 @@ class IMipPlugin extends \Sabre\CalDAV\Schedule\IMipPlugin{
 					break;
 			}
 
-			$recipients = new AddressList($to);
-			$to = $recipients[0];
+
+			//Sabredav does not format the address correctly. When a , inside the name the parsing will go wrong. They shoukd
+			// use quotes: https://www.rfc-editor.org/rfc/rfc5322.html#section-3.2
+			// For example : John, Doe <johndoe@foo.com> should be  "John, Doe" <johndoe@foo.com>
+
+			if(preg_match("/(.*)\s<(.*)>/", $to, $toParts)) {
+				$to = new Address($toParts[2], $toParts[1]);
+			}
 
 			$message = \GO\Base\Mail\Message::newInstance($subject)
 				->setFrom(\GO::user()->email, \GO::user()->name)
