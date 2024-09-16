3b71f3a48e165daa194e07661217c48946d71188
Merijn Schering <mschering@intermesh.nl>
2024-09-16 12:00:59 +0200

    Update German language

--- www/go/modules/community/dev/language/de.php
+++ www/go/modules/community/dev/language/de.php
@@ -1,4 +1,5 @@
 <?php
 return array (
   'Enter \'dummy\' to pass.' => 'Geben Sie \'dummy\' ein.',
+  '\'"Enter \'\'dummy\'\' to pass."\'' => '\'"Geben Sie \'\'dummy\'\' ein."\'',
 );
--- www/go/modules/community/notesencrypt/language/de.php
+++ www/go/modules/community/notesencrypt/language/de.php
@@ -6,4 +6,5 @@ return array (
   'Notes encrypt' => 'Notiz verschlüsselt',
   'Sorry, you can\'t use images in encrypted notes' => 'Sie können keine Bilder in verschlüsselten Notizen benutzen',
   'Cryptographic functions are not available. Please run Group-Office with SSL.' => 'Kryptografische Funktionen sind nicht verfügbar. Bitte führen Sie {produkt_name} mit SSL aus.',
+  '\'"Sorry, you can\'\'t use images in encrypted notes"\'' => 'Sie können keine Bilder in verschlüsselten Notizen benutzen',
 );
