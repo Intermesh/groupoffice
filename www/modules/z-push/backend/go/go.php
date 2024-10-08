d4c925338a8fc42ba436a1fa91a667b5999f9b70
Merijn Schering <mschering@intermesh.nl>
2024-10-08 08:47:41 +0200

    instanceID

--- www/modules/z-push/backend/go/go.php
+++ www/modules/z-push/backend/go/go.php
@@ -228,13 +228,13 @@ class BackendGO extends Backend implements IBackend, ISearchProvider {
 	 * @return string       id of the created/updated calendar obj
 	 * @throws StatusException
 	 */
-	public function MeetingResponse($requestid, $folderid, $response, $instanceOd) {
+	public function MeetingResponse($requestid, $folderid, $response, $instanceId) {
 		ZLog::Write(LOGLEVEL_DEBUG, "Combined->MeetingResponse($requestid , $folderid , $response)");
 		$backend = $this->GetBackend($folderid);
 		if ($backend === false)
 			return false;
 		ZLog::Write(LOGLEVEL_DEBUG, "Combined->MeetingResponse($requestid , $folderid , $response) success");
-		return $backend->MeetingResponse($requestid, $this->GetBackendFolder($folderid), $response, $instanceOd);
+		return $backend->MeetingResponse($requestid, $this->GetBackendFolder($folderid), $response, $instanceId);
 	}
 
 
