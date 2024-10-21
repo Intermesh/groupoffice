55f2c4e894e7bdc8eaf1b336a8f330ddc719b446
Merijn Schering <mschering@intermesh.nl>
2024-10-21 09:06:11 +0200

    Uptime monitor

---
+++ www/api/up.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Checks if system is OK. Can be used for uptime monitoring
+ */
+use go\core\App;
+use go\core\ErrorHandler;
+use go\core\http\Response;
+
+require("../vendor/autoload.php");
+
+$minSpace = 1024 * 1024 * 1024; // start erroring on 1G disk space left
+
+try {
+	// check read db
+	$settings = App::get()->getDbConnection()->select()->from("core_setting")->limit(1)->single();
+
+	if(disk_free_space(go()->getDataFolder()->getPath()) < $minSpace) {
+		Response::get()->setStatus(500);
+		ErrorHandler::log("Out of disk space");
+	}
+
+
+	if(disk_free_space(go()->getTmpFolder()->getPath()) < $minSpace) {
+		Response::get()->setStatus(500);
+		ErrorHandler::log("Out of disk space");
+	}
+
+	echo "All OK";
+
+
+} catch(Exception $e) {
+	Response::get()->setStatus(500);
+	echo "Internal Server Error";
+	ErrorHandler::logException($e);
+}
