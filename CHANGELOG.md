c47ee39bc8cf05e859ec1fe31a1f8d9e3fc69706
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2024-10-28 09:44:54 +0100

    Ticket 33263 - bugfix edit fieldset for individual entity

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+- Core: bugfix editing individual field set in entity
+
 24-10-2024: 6.8.79
 - E-mail: Server side sort was disabled by accident since May 2024
 - Caldav: Fixed CalDAV sync with DAVx5 error #1192
--- www/go/core/views/extjs3/customfields/EditFieldSetDialog.js
+++ www/go/core/views/extjs3/customfields/EditFieldSetDialog.js
@@ -11,6 +11,26 @@ go.customfields.EditFieldSetDialog = Ext.extend(go.form.Dialog, {
 		fs[0].title = false;
 		return items.concat(fs);
 	},
+
+	/**
+	 * override the getCustomFieldSets function as the original filters out the field sets that are rendered in their own
+	 * tabs. In this use case, a field set should always be returned.
+	 *
+	 * @returns {*[]}
+	 */
+	getCustomFieldSets : function() {
+		const items = [];
+		const fieldsets = go.customfields.CustomFields.getFormFieldSets(this.entityStore);
+		fieldsets.forEach(function(fs) {
+			if(fs.fieldSet.permissionLevel <= 10) {
+				return;
+			}
+			fs.columnWidth = 1;
+			items.push(fs);
+		}, this);
+
+		return items;
+	},
 });
 
 
