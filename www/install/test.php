2f6f78665ff8e5b7a95786e9e54833b52bc9f466
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2024-11-11 16:57:26 +0100

    Check for headers in test script upon clean install

--- www/install/test.php
+++ www/install/test.php
@@ -6,6 +6,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST" || go()->getConfig()['servermanager']) {
 	header('Location: configfile.php');
 	exit();	
 }
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 require('header.php');
 ?>
