a332648142b6473d48ebe2af519863d86b527447
polarismail <83369329+polarismail@users.noreply.github.com>
2024-11-14 23:03:31 -0500

    Update HtmlEditor.js
    add NEW Table Plugin to HtmlEditor
    
    we called it NEW because there is already one similarly named as part of the VinylFox plugins which is included in this project

--- www/views/Extjs3/javascript/form/HtmlEditor.js
+++ www/views/Extjs3/javascript/form/HtmlEditor.js
@@ -19,6 +19,7 @@ GO.form.HtmlEditor = function (config) {
 	var ioDentPlugin = new Ext.ux.form.HtmlEditor.IndentOutdent();
 	var ssScriptPlugin = new Ext.ux.form.HtmlEditor.SubSuperScript();
 	var rmFormatPlugin = new Ext.ux.form.HtmlEditor.RemoveFormat();
+	var tblPlugin = new Ext.ux.form.HtmlEditor.NEWTablePlugin();
 
 
 	if (GO.settings.pspellSupport) {
@@ -26,6 +27,7 @@ GO.form.HtmlEditor = function (config) {
 	}
 
 	config.plugins.unshift(
+					tblPlugin,
 					ioDentPlugin,
 					rmFormatPlugin,
 					// wordPastePlugin,
