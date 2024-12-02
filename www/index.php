59af4560748e9de24873e52b529e8cb56649c01a
dehart <derinus@gmail.com>
2024-12-02 10:25:06 +0100

    added slash to click here to launch installer because if go is in a subdirectory the next step to test.php will not go to install/test.php if it is missing

--- www/index.php
+++ www/index.php
@@ -37,7 +37,7 @@ function errorHander($e) {
 			echo "DEBUGGER: Showing error message because debug is enabled. Normally we would have redirected to install. I you're doing a fresh install and your database is empty then you can safely ignore this.:<br /><br />";
 			echo $msg;
 			echo "<pre>" . $e->getTraceAsString() . "</pre>";
-			echo '<br /><br /><a href="install">Click here to launch the installer</a>';
+			echo '<br /><br /><a href="install/">Click here to launch the installer</a>';
 			exit();
 		}
 
