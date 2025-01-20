5650a1447c7915db0e17284787e378c7976626d5
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2025-01-20 12:19:38 +0100

    33682 minor code maintenance

--- www/go/modules/community/addressbook/model/UserSettings.php
+++ www/go/modules/community/addressbook/model/UserSettings.php
@@ -2,6 +2,7 @@
 
 namespace go\modules\community\addressbook\model;
 
+use go\core\db\DbException;
 use go\core\model\User;
 use go\core\orm\exception\SaveException;
 use go\core\orm\Mapping;
@@ -9,7 +10,6 @@ use go\core\orm\Property;
 use go\modules\community\addressbook\model\Settings as AddresBookModuleSettings;
 use go\core\model;
 use go\core\model\Acl;
-use phpDocumentor\Reflection\Types\Boolean;
 
 class UserSettings extends Property {
 
@@ -45,53 +45,64 @@ class UserSettings extends Property {
 		return parent::defineMapping()->addTable("addressbook_user_settings", "abs");
 	}
 
-	public function getDefaultAddressBookId() {
-		if(isset($this->defaultAddressBookId)) {
+	/**
+	 *
+	 * @return ?int the default addressbook ID or null if module not available...
+	 *
+	 * @throws SaveException
+	 * @throws DbException
+	 * @throws \Exception
+	 */
+	public function getDefaultAddressBookId(): ?int
+	{
+		if (isset($this->defaultAddressBookId)) {
 			return $this->defaultAddressBookId;
 		}
 
-		if(!model\Module::isAvailableFor('community', 'addressbook', $this->userId)) {
+		if (!model\Module::isAvailableFor('community', 'addressbook', $this->userId)) {
 			return null;
 		}
 
-		if(AddresBookModuleSettings::get()->createPersonalAddressBooks){
+		if (AddresBookModuleSettings::get()->createPersonalAddressBooks) {
 			$addressBook = AddressBook::find()->where('createdBy', '=', $this->userId)->single();
-			if(!$addressBook) {
+			if (!$addressBook) {
 				$user = User::findById($this->userId, ['displayName', 'enabled']);
-				if(!$user || !$user->enabled) {
+				if (!$user || !$user->enabled) {
 					return null;
 				}
 				$addressBook = new AddressBook();
 				$addressBook->createdBy = $this->userId;
 				$addressBook->name = $user->displayName;
-				if(!$addressBook->save()) {
+				if (!$addressBook->save()) {
 					throw new SaveException($addressBook);
 				}
 			}
 		} else {
 			$addressBook = AddressBook::applyAclToQuery(
-				AddressBook::find(['id'])
-				, Acl::LEVEL_WRITE, $this->userId)
+				AddressBook::find(['id']),
+				Acl::LEVEL_WRITE,
+				$this->userId)
 				->single();
 		}
 
-		if($addressBook) {
+		if ($addressBook) {
 			$this->defaultAddressBookId = $addressBook->id;
 			go()->getDbConnection()->update("addressbook_user_settings", ['defaultAddressBookId' => $this->defaultAddressBookId], ['userId' => $this->userId])->execute();
 		}
 
 		return $this->defaultAddressBookId;
-		
+
 	}
 
-	public function setDefaultAddressBookId($id)
+	public function setDefaultAddressBookId(?int $id): void
 	{
 		$this->defaultAddressBookId = $id;
 	}
 
 
 
-	public function getSortBy() {
+	public function getSortBy(): string
+	{
 		return isset($this->userId) ? (User::findById($this->userId, ['sort_name'])->sort_name == 'first_name' ? 'name' : 'lastName') : 'name';
 	}
 
