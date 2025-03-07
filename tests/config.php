7cbfc85c4f3b3de4476f923136a5d2b9390676bc
Merijn Schering <mschering@intermesh.nl>
2025-03-07 14:49:47 +0100

    @mdhart broke the unit tests

--- tests/config.php
+++ tests/config.php
@@ -2,6 +2,6 @@
 $config['db_name'] = "groupoffice_phpunit";
 //$config['db_host'] = "mysql8"; //mysql 8
 //$config['db_host'] = "mysql"; //mysql 5.7
-$config['db_host'] = "localhost"; //mariadb
+$config['db_host'] = "db"; //mariadb
 $config['db_user'] = "root";
-$config['db_pass'] = "";
+$config['db_pass'] = "groupoffice";
