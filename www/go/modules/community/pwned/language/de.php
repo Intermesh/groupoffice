db4362554f7ab906592c4dbb37867dec3e1e0dcb
Merijn Schering <mschering@intermesh.nl>
2025-03-21 10:02:14 +0100

    Updated German translation thanks to University of Greifswald

--- www/go/modules/community/pwned/language/de.php
+++ www/go/modules/community/pwned/language/de.php
@@ -3,4 +3,5 @@ return array (
   '\'"Enable \'\'Have I Been Pwned\'\' checking for users in a specific group"\'' => '\'„Aktivieren Sie die \'\'Have I Been Pwned\'\'-Prüfung für Benutzer einer bestimmten Gruppe“.\'',
   'Enable \'Have I Been Pwned\' checking for users in a specific group' => 'Aktivieren Sie die \'Have I Been Pwned\'-Prüfung für Benutzer in einer bestimmten Gruppe',
   '`' => '-- Have I been Pwned',
+  '`;-- Have I been Pwned' => '`;-- Have I been Pwned',
 );
