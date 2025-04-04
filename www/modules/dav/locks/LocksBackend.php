e33033e8dee997322c5ed1da9394bc728fe3037e
mschering <mschering@intermesh.nl>
2025-04-04 14:43:04 +0200

    Fixed broken webdav. GO 25.0.11 WebDAV broken #1251

--- www/modules/dav/locks/LocksBackend.php
+++ www/modules/dav/locks/LocksBackend.php
@@ -1,27 +1,32 @@
 <?php
+
 namespace GO\Dav\Locks;
+
 use GO\Dav\Fs\File;
+use Sabre\DAV\Exception\NotFound;
 use Sabre\DAV\Locks\LockInfo;
 use Sabre\DAV\Server;
 
-class LocksBackend extends \Sabre\DAV\Locks\Backend\PDO {
-    public $tableName = 'dav_locks';
+class LocksBackend extends \Sabre\DAV\Locks\Backend\PDO
+{
+	public $tableName = 'dav_locks';
 
-    private Server $server;
+	private Server $server;
 	private bool $isLocking = false;
 
 	public function __construct(Server $server)
-    {
-        $this->server = $server;
+	{
+		$this->server = $server;
 
-        parent::__construct(go()->getDbConnection()->getPDO());
-    }
+		parent::__construct(go()->getDbConnection()->getPDO());
+	}
 
-		public function getLocks($uri, $returnChildLocks)
-		{
-			$locks = parent::getLocks($uri, $returnChildLocks);
+	public function getLocks($uri, $returnChildLocks)
+	{
+		$locks = parent::getLocks($uri, $returnChildLocks);
+		try {
 			$file = $this->server->tree->getNodeForPath($uri);
-			if(!$this->isLocking && $file instanceof File) {
+			if (!$this->isLocking && $file instanceof File) {
 
 				$fileModel = $file->getFile();
 				$exists = false;
@@ -34,7 +39,7 @@ class LocksBackend extends \Sabre\DAV\Locks\Backend\PDO {
 					}
 
 					if (!$exists) {
-						go()->debug("Adding lock from GO ". $fileModel->lock_id);
+						go()->debug("Adding lock from GO " . $fileModel->lock_id);
 						$lockInfo = new LockInfo();
 						$lockInfo->token = $fileModel->lock_id;
 						$lockInfo->owner = "go-" . $fileModel->locked_user_id;
@@ -46,33 +51,37 @@ class LocksBackend extends \Sabre\DAV\Locks\Backend\PDO {
 					}
 				}
 			}
-
-			return $locks;
+		} catch (NotFound $e) {
+			//ignore
 
 		}
 
-		public function lock($uri, \Sabre\DAV\Locks\LockInfo $lockInfo)
-    {
-        $file = $this->server->tree->getNodeForPath($uri);
+		return $locks;
 
-        $file->lock($lockInfo->token);
+	}
 
-				$this->isLocking = true;
-        $success = parent::lock($uri, $lockInfo);
-				$this->isLocking = false;
+	public function lock($uri, \Sabre\DAV\Locks\LockInfo $lockInfo)
+	{
+		$file = $this->server->tree->getNodeForPath($uri);
 
-				return $success;
-    }
+		$file->lock($lockInfo->token);
 
-    public function unlock($uri, \Sabre\DAV\Locks\LockInfo $lockInfo)
-    {
-        $file = $this->server->tree->getNodeForPath($uri);
-				$fileModel = $file->getFile();
+		$this->isLocking = true;
+		$success = parent::lock($uri, $lockInfo);
+		$this->isLocking = false;
+
+		return $success;
+	}
+
+	public function unlock($uri, \Sabre\DAV\Locks\LockInfo $lockInfo)
+	{
+		$file = $this->server->tree->getNodeForPath($uri);
+		$fileModel = $file->getFile();
 
 
-				if($fileModel->lock_id == $lockInfo->token) {
-					$file->unlock();
-				}
-        return parent::unlock($uri, $lockInfo);
-    }
+		if ($fileModel->lock_id == $lockInfo->token) {
+			$file->unlock();
+		}
+		return parent::unlock($uri, $lockInfo);
+	}
 }
