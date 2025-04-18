946703a69031d07a7ec1eb9eb45550e536974435
Merijn Schering <mschering@intermesh.nl>
2025-04-18 12:23:12 +0200

    only update git and remove old stuff

--- scripts/update-git.sh
+++ scripts/update-git.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-# This script updates all git repo's and builds SASS, installs composer packages and build's the GOUI modules
-
+# This script updates all git repo's
 set -e
 
 CONFIG=$1
-
-SASS="sass --no-source-map"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/../;
 DIR="$(pwd)";
 
-
 # pull promodules
-
 if [ -d "www/promodules" ]; then
   echo "Pulling promodules"
   cd  www/promodules
@@ -49,23 +44,5 @@ echo "Pulling main repository"
 git pull
 git submodule update --init
 
-for line in $(find views/Extjs3 go/modules modules \( -name style.scss -o -name style-mobile.scss -o -name htmleditor.scss \) -not -path '*/goui/*' | sort -r );
-do
-  replace1=${line/src\/style.scss/style.css};
-  replace2=${replace1/src\/style-mobile.scss/style-mobile.css};
-  replace3=${replace2/src\/htmleditor.scss/htmleditor.css};
-  echo $line - $replace3;
-	$SASS $line $replace3;
-done
-
-
-../scripts/build.sh
-
-if [ -z "$CONFIG" ]; then
-  echo NOTE: Not upgrading database because no config file was passed. eg. ./update-git.sh /etc/groupoffice/multi_instance/manage.group-office.com/config.php
-  exit 1
-else
-  sudo -u www-data php cli.php core/System/upgrade -c=$CONFIG
-fi
 
 
