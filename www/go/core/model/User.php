4d30d8aaa050e281261a6736669a226373d38be5
Merijn Schering <mschering@intermesh.nl>
2025-05-30 13:59:56 +0200

    Core: Fixed creating double user groups

--- www/go/core/model/User.php
+++ www/go/core/model/User.php
@@ -1086,15 +1086,11 @@ public function historyLog(): bool|array
 	private function checkPersonalGroup()
 	{
 		if ($this->isNew() || $this->isModified(['groups', 'username'])) {
-			if ($this->isNew()) {// !in_array($this->getPersonalGroup()->id, $groupIds)) {
-				$personalGroup = $this->createPersonalGroup();
-			} else {
-				$personalGroup = $this->getPersonalGroup();
-				if ($this->isModified('username')) {
-					$personalGroup->name = $this->username;
-					if (!$this->appendNumberToGroupNameIfExists($personalGroup)) {
-						throw new SaveException($personalGroup);
-					}
+			$personalGroup = $this->getPersonalGroup();
+			if (!$this->isNew() && $this->isModified('username')) {
+				$personalGroup->name = $this->username;
+				if (!$this->appendNumberToGroupNameIfExists($personalGroup)) {
+					throw new SaveException($personalGroup);
 				}
 			}
 
