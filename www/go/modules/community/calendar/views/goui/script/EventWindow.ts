9df156636c725d37094b5e8eadf32ec211c59067
Merijn Schering <mschering@intermesh.nl>
2025-05-30 12:34:05 +0200

    Calendar: removed displayfield() for description as it had some unexpected results when the description was overridden for an occurrence

--- www/go/modules/community/calendar/views/goui/script/EventWindow.ts
+++ www/go/modules/community/calendar/views/goui/script/EventWindow.ts
@@ -223,46 +223,13 @@ export class EventWindow extends FormWindow {
 				dlg.show(this.item, this.form.modified);
 			} }),
 			this.alertField,
+
 			textarea({
 				name:'description',
 				label: t('Description'),
-				autoHeight: true,
-				hidden: true,
-				listeners: {
-					render: (comp) => {
-						comp.input!.addEventListener("blur", () => {
-
-
-							const field = comp.nextSibling() as DisplayField;
-							field.value = comp.value;
-							comp.hide();
-							field.show();
-						})
-					}
-				}
+				autoHeight: true
 			}),
-			displayfield({
-				tabIndex: 0,
-				listeners: {
-					render: comp1 => {
-						comp1.el.addEventListener("focus", () => {
 
-							const field = comp1.previousSibling() as TextAreaField;
-							field.height = comp1.height;
-							comp1.hide();
-
-							field.show();
-							field.focus();
-						})
-					}
-				},
-				name:'description',
-				label: t('Description'),
-				cls: "pit",
-				escapeValue: false,
-				hideWhenEmpty: false,
-				renderer: (v, field) => Format.textToHtml(v)}
-			),
 			autocompletechips({
 				list: table({fitParent: true, headers: false, store: datasourcestore({dataSource:categoryStore.dataSource}),
 					rowSelectionConfig: {multiSelect: true},
