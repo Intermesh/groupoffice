dcb78a31054d7a15562b221f2d726a3168dab431
dehart <derinus@gmail.com>
2025-06-10 10:26:47 +0200

    entity controller default get will toArray the properties the user asked for

--- www/go/core/jmap/EntityController.php
+++ www/go/core/jmap/EntityController.php
@@ -503,7 +503,7 @@ abstract class EntityController extends Controller {
 		foreach($query as $e) {
 			if($this->canRead($e)) {
 				try {
-					$arr = $e->toArray();
+					$arr = $e->toArray($params['properties'] ?? null);
 					// make sure the entity result has an ID property. Otherwise it's difficult to identify objects in the "list" array.
 					$arr['id'] = $e->id();
 					$unsorted[$arr['id']] = $arr;
