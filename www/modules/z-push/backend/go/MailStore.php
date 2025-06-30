4f46cf3f4f9e5be6d4edde77677f9377c6e91f88
Merijn Schering <mschering@intermesh.nl>
2025-06-30 09:01:33 +0200

    Return false is IMAP fails

--- www/modules/z-push/backend/go/MailStore.php
+++ www/modules/z-push/backend/go/MailStore.php
@@ -35,6 +35,9 @@ class MailStore extends Store implements ISearchProvider {
 			//$this->imap_reopenFolder($folderid);
 
 			$imap = $this->_imapLogon();
+			if(!$imap) {
+				return false;
+			}
 
 			//remove m/ from the combined stuff
 			if (!empty($folderid)) {
