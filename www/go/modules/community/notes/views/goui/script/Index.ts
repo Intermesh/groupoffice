01c2ddf3458ec81dee6f057a99fad38058ed3972
Merijn Schering <mschering@intermesh.nl>
2025-07-03 14:26:23 +0200

    Link window in GOUI

--- www/go/modules/community/notes/views/goui/script/Index.ts
+++ www/go/modules/community/notes/views/goui/script/Index.ts
@@ -2,6 +2,8 @@ import {client, modules, router} from "@intermesh/groupoffice-core";
 import {Main} from "./Main.js";
 import {t} from "@intermesh/goui";
 import {SettingsPanel} from "./SettingsPanel.js";
+import {NoteDialog} from "./NoteDialog";
+import {NoteDetail} from "./NoteDetail";
 
 modules.register({
 	package: "community",
@@ -31,5 +33,83 @@ modules.register({
 			});
 
 		});
-	}
+	},
+	entities: [{
+		name: "Note",
+		filters: [
+			{
+				name: 'text',
+				type: "string",
+				multiple: false,
+				title: t("Query")
+			},
+			{
+				name: 'name',
+				type: "string",
+				multiple: true,
+				title: t("Name")
+			},
+			{
+				name: 'content',
+				type: "string",
+				multiple: true,
+				title: t("Content")
+			},
+			{
+				title: t("Has links to..."),
+				name: 'link',
+				multiple: false,
+				type: 'go.links.FilterLinkEntityCombo'
+			},
+			{
+				title: t("Commented at"),
+				name: 'commentedat',
+				multiple: false,
+				type: 'date'
+			}, {
+				title: t("Modified at"),
+				name: 'modifiedat',
+				multiple: false,
+				type: 'date'
+			}, {
+				title: t("Modified by"),
+				name: 'modifiedBy',
+				multiple: true,
+				type: 'string'
+			}, {
+				title: t("Created at"),
+				name: 'createdat',
+				multiple: false,
+				type: 'date'
+			}, {
+				title: t("Created by"),
+				name: 'createdby',
+				multiple: true,
+				type: 'string'
+			}
+		],
+		links: [{
+			iconCls: 'entity ic-note yellow',
+			/**
+			 * Opens a dialog to create a new linked item
+			 *
+			 * @param {string} entity eg. "Note"
+			 * @param {string|int} entityId
+			 */
+			linkWindow:  (entity, entityId) => {
+				return new NoteDialog();
+			},
+
+			/**
+			 * Return component for the detail view
+			 *
+			 * @returns {go.detail.Panel}
+			 */
+			linkDetail: ()=> {
+				return new NoteDetail();
+			}
+		}]
+	}, {
+		name: "NoteBook", title: t("Note book")
+	}],
 });
