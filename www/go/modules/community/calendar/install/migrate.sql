42ad899ab9e593539f8954c11e9cdf6abaedfe8d
Merijn Schering <mschering@intermesh.nl>
2025-08-11 09:25:02 +0200

    Calendar: Only subscribe personal calendars in new calendar migration to avoid an excessive list of calendars

--- www/go/modules/community/calendar/install/migrate.sql
+++ www/go/modules/community/calendar/install/migrate.sql
@@ -424,17 +424,22 @@ INSERT INTO calendar_calendar
   cal_calendars cal LEFT JOIN cal_calendar_user_colors cu ON cal.id = cu.calendar_id GROUP BY cal.id;
 
 -- subscribe to calendars user has access to
+-- INSERT IGNORE INTO calendar_calendar_user
+-- (id, userId, isSubscribed, isVisible, color, sortOrder, timeZone, includeInAvailability, modSeq)
+-- SELECT
+--     cal.id, ug.userId, 1, 1, IFNULL(background,SUBSTRING('#CDAD00#E74C3C#9B59B6#8E44AD#2980B9#3498DB#1ABC9C#16A085#27AE60#2ECC71#F1C40F#F39C12#E67E22#D35400#95A5A6#34495E#808B96#1652a1', (cal.id MOD 18) * 7 + 2 ,6)), 0, null, 'all', 1
+-- from core_acl_group ag
+--          inner join core_user_group ug on ug.groupId = ag.groupId
+--          inner join cal_calendars cal on cal.acl_id = ag.aclId
+-- where cal.group_id=1
+-- group by cal.id,ug.userId;
+
+-- subscribe only personal calendars
 INSERT IGNORE INTO calendar_calendar_user
 (id, userId, isSubscribed, isVisible, color, sortOrder, timeZone, includeInAvailability, modSeq)
 SELECT
-    cal.id, ug.userId, 1, 1, IFNULL(background,SUBSTRING('#CDAD00#E74C3C#9B59B6#8E44AD#2980B9#3498DB#1ABC9C#16A085#27AE60#2ECC71#F1C40F#F39C12#E67E22#D35400#95A5A6#34495E#808B96#1652a1', (cal.id MOD 18) * 7 + 2 ,6)), 0, null, 'all', 1
-from core_acl_group ag
-         inner join core_user_group ug on ug.groupId = ag.groupId
-         inner join cal_calendars cal on cal.acl_id = ag.aclId
-where cal.group_id=1
-group by cal.id,ug.userId;
-
-
+    cal.id, cal.user_id, 1, 1, IFNULL(background,SUBSTRING('#CDAD00#E74C3C#9B59B6#8E44AD#2980B9#3498DB#1ABC9C#16A085#27AE60#2ECC71#F1C40F#F39C12#E67E22#D35400#95A5A6#34495E#808B96#1652a1', (cal.id MOD 18) * 7 + 2 ,6)), 0, null, 'all', 1
+    from cal_calendars cal where cal.group_id=1;
 
 
 INSERT IGNORE INTO calendar_default_alert_with_time
