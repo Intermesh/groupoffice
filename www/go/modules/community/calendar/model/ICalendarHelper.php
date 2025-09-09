c4eaac88701d20b87ad1e09e7011c4dddc0ac9b5
Merijn Schering <mschering@intermesh.nl>
2025-09-09 09:36:38 +0200

    Testing imip

--- www/go/modules/community/calendar/model/ICalendarHelper.php
+++ www/go/modules/community/calendar/model/ICalendarHelper.php
@@ -65,10 +65,10 @@ class ICalendarHelper {
 	{
 
 		if($vcalendar === null) {
-			$vcalendar = new VCalendar(['PRODID' => $event->prodId]);
-		} else {
-			$vcalendar->PRODID = CalendarEvent::prodId();
+			$vcalendar = new VCalendar();
 		}
+
+		$vcalendar->PRODID = CalendarEvent::prodId();
 
 		//TODO prodid always GO??? gmail doesn't process our reply but it does work with the sabre imipplugin
 
