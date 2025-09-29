f93580b542cf4c42676b4d21cfc2dfaa5f9c8061
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2025-09-29 10:21:30 +0200

    Ticket 35674 - PL translations. Thank you Krzysztof

---
+++ www/go/modules/community/pwned/language/pl.php
@@ -0,0 +1,5 @@
+<?php
+return array (
+  '`;-- Have I been Pwned' => '`;-- Have I been Pwned',
+  'Enable \'Have I Been Pwned\' checking for users in a specific group' => 'Włącz sprawdzanie w serwisie \'Have I Been Pwned\' dla użytkowników z wybranej grupy',
+);
