8de277d3c7d7ba9100603cb4ae0505eb85359dc2
Merijn Schering <mschering@intermesh.nl>
2025-10-20 13:57:19 +0200

    Disable email field when an authenticator is used

--- www/go/core/views/extjs3/usersettings/AccountSettingsPanel.js
+++ www/go/core/views/extjs3/usersettings/AccountSettingsPanel.js
@@ -297,6 +297,7 @@ go.usersettings.AccountSettingsPanel = Ext.extend(Ext.Panel, {
 		this.usernameField.setDisabled(!visible);
 		this.passwordField1.setDisabled(!visible);
 		this.passwordField2.setDisabled(!visible);
+		this.emailField.setDisabled(!visible);
 
 		this.passwordFieldset.setVisible(visible);
 	},
