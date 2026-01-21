ea2adfa3264c136d3f9befc7abfa2458b2631ddc
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2026-01-21 15:43:29 +0100

    1514 - Feedback DvO 16-1-26

--- www/go/modules/community/otp/install/updates.php
+++ www/go/modules/community/otp/install/updates.php
@@ -1,4 +1,7 @@
 <?php
+
+use go\modules\community\otp\cron\ClearExpired;
+
 $updates['202012210922'][] = "ALTER TABLE `googleauth_secret` DROP FOREIGN KEY `googleauth_secret_user`; ";
 $updates['202012210922'][] = "ALTER TABLE `googleauth_secret` ADD CONSTRAINT `googleauth_secret_user` FOREIGN KEY (`userId`) REFERENCES `core_user`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;";
 
