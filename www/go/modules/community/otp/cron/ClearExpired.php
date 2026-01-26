788e2026bccfcb43a0b654e5563bf48ef2b4971e
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2026-01-26 14:52:38 +0100

    Save OTP with default system timezone

--- www/go/modules/community/otp/cron/ClearExpired.php
+++ www/go/modules/community/otp/cron/ClearExpired.php
@@ -10,6 +10,7 @@ final class ClearExpired extends CronJob
 	public function run(CronJobSchedule $schedule): void
 	{
 		$dt = new \DateTime();
+		$dt->setTimezone(new \DateTimeZone(go()->getSettings()->defaultTimezone));
 
 		go()->getDbConnection()->delete('otp_secret',
 			(new Query())->where('expiresAt',  '<=', $dt)
