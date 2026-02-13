1f40232786de32589fde2fb9fcffac3871880b1d
jadema <ademajoost@hotmail.com>
2026-02-13 13:26:27 +0100

    Remove old tester var

--- www/go/modules/community/tasks/views/goui/script/Index.ts
+++ www/go/modules/community/tasks/views/goui/script/Index.ts
@@ -1,36 +1,32 @@
 import {client, modules, router} from "@intermesh/groupoffice-core";
 import {Main} from "./Main.js";
-import { t } from "@intermesh/goui";
+import {t} from "@intermesh/goui";
 import {SettingsPanel} from "./SettingsPanel.js";
 
-const tester = true;
+modules.register({
+	package: "community",
+	name: "tasks",
+	async init() {
 
-if (tester) {
-	modules.register({
-		package: "community",
-		name: "tasks",
-		async init() {
+		let tasks: Main;
+		client.on("authenticated", ({session}) => {
+			if (!session.capabilities["go:community:tasks"]) {
+				return;
+			}
 
-			let  tasks: Main;
-			client.on("authenticated", ({session}) => {
-				if (!session.capabilities["go:community:tasks"]) {
-					return;
-				}
-
-				router.add(/^tasks\/(\d+)$/, (taskId) => {
-					modules.openMainPanel("tasks");
-					tasks.showTask(taskId);
-				});
-
-				modules.addMainPanel("community", "tasks", "tasks", "Tasks", () => {
-					tasks = new Main();
-					return tasks;
-				});
-
-				modules.addAccountSettingsPanel("community", "tasks", "tasks", t("Tasks"), "check", () => {
-					return new SettingsPanel();
-				});
+			router.add(/^tasks\/(\d+)$/, (taskId) => {
+				modules.openMainPanel("tasks");
+				tasks.showTask(taskId);
 			});
-		}
-	})
-}
+
+			modules.addMainPanel("community", "tasks", "tasks", "Tasks", () => {
+				tasks = new Main();
+				return tasks;
+			});
+
+			modules.addAccountSettingsPanel("community", "tasks", "tasks", t("Tasks"), "check", () => {
+				return new SettingsPanel();
+			});
+		});
+	}
+});
