45f677ad538a594c1225acdfae28c44ef2234430
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2026-02-18 13:55:57 +0100

    No ticket - prevent creating default ActiveRecord model for calendar

--- www/modules/summary/model/CalendarActiveRecord.php
+++ www/modules/summary/model/CalendarActiveRecord.php
@@ -2,12 +2,31 @@
 
 namespace GO\Summary\Model;
 
-class CalendarActiveRecord extends \GO\Base\Model\AbstractUserDefaultModel {
-	public function aclField() {
+use GO\Base\Model\AbstractUserDefaultModel;
+use GO\Base\Model\User;
+
+class CalendarActiveRecord extends AbstractUserDefaultModel
+{
+	public function aclField()
+	{
 		return 'aclId';
 	}
 
-	public function tableName() {
+	public function tableName()
+	{
 		return 'calendar_calendar';
+	}
+
+	/**
+	 * Creates a default model for the user.
+	 *
+	 * Actually nope. The default calendar is already created through JMAP
+	 *
+	 * @param User $user
+	 * @return false
+	 */
+	public function getDefault(User $user, &$createdNew = false)
+	{
+		return false;
 	}
 }
