74e0360fe382cacc6e3776586b7a2c8066687746
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2026-02-24 16:49:19 +0100

    Fix syntax error in tosklistdialog

--- www/go/modules/community/tasks/views/extjs3/TasklistDialog.js
+++ www/go/modules/community/tasks/views/extjs3/TasklistDialog.js
@@ -1,48 +1,63 @@
 go.modules.community.tasks.TasklistDialog = Ext.extend(go.form.Dialog, {
-	title: t("List"),
-	entityStore: "TaskList", // or "SupportList" for support module
-	titleField: "name",
-	width: dp(800),
-	height: dp(600),
-	redirectOnSave: false,
-	hidePermissions: false,
-	initFormItems: function () {
-		if(!this.hidePermissions) {
-			this.addPanel(new go.permissions.SharePanel());
-		} else
-		{
-			this.height = null;
-			this.autoHeight = true;
-			this.width = dp(600);
+    title: t("List"),
+    entityStore: "TaskList", // or "SupportList" for support module
+    titleField: "name",
+    width: dp(800),
+    height: dp(600),
+    redirectOnSave: false,
+    hidePermissions: false,
+    initFormItems: function () {
+        if (!this.hidePermissions) {
+            this.addPanel(new go.permissions.SharePanel());
+        } else {
+            this.height = null;
+            this.autoHeight = true;
+            this.width = dp(600);
 
-		}
+        }
 
-		return [{
-			xtype: 'fieldset',
-			items: [{
-				xtype: 'hidden',
-				allowBlank: false,
-				value: 'list',
-				name: 'role'
-			},{
-				xtype: 'hidden',
-				allowBlank: false,
-				value: null,
-				name: 'projectId'
-			},{
-				xtype: 'textfield',
-				name: 'name',
-				fieldLabel: t("Name"),
-				anchor: '100%',
-				allowBlank: false
-			},{
-				xtype:'colorfield',
-				name: 'color',
-				fieldLabel: t('Color')
-			}
-			, new go.modules.community.tasks.TaskListGroupingCombo(),
-			{xtype:'checkbox', name: 'syncToDevice', boxLabel: t('Sync to device'), hint: t('Make tasklist available in CalDAV and ActiveSync')}
-			]
-		}];
-	}
+        return [{
+            xtype: 'fieldset',
+            items: [{
+                xtype: 'hidden',
+                allowBlank: false,
+                value: 'list',
+                name: 'role'
+            }, {
+                xtype: 'hidden',
+                allowBlank: false,
+                value: null,
+                name: 'projectId'
+            }, {
+                xtype: 'textfield',
+                name: 'name',
+                fieldLabel: t("Name"),
+                anchor: '100%',
+                allowBlank: false
+            }, {
+                xtype: 'colorfield',
+                name: 'color',
+                fieldLabel: t('Color')
+            },
+                {
+                    xtype: 'checkbox',
+                    name: 'syncToDevice',
+                    boxLabel: t('Sync to device'),
+                    hint: t('Make tasklist available in CalDAV and ActiveSync')
+                },
+
+
+                this.selectUser = new go.users.UserCombo({
+                    disabled: !go.User.isAdmin,
+                    hidden: !go.User.isAdmin,
+                    fieldLabel: t("Owner"),
+                    hiddenName: "createdBy",
+                    value: GO.settings.user_id,
+                    anchor: '100%'
+                }),
+
+
+                new go.modules.community.tasks.TaskListGroupingCombo()]
+        }];
+    }
 });
