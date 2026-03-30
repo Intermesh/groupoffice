4f606cdca23b9c273379a5ff4c4a5d73dd9acdae
Merijn Schering <mschering@intermesh.nl>
2026-03-30 15:50:11 +0200

    Core: Set 'to' if you are an admin in core/Notify/mail controller

--- www/go/core/controller/Notify.php
+++ www/go/core/controller/Notify.php
@@ -2,13 +2,9 @@
 
 namespace go\core\controller;
 
-use Exception;
-use GO;
 use go\core\Controller;
-use go\core\jmap\Response;
+use go\core\exception\Forbidden;
 use go\core\mail\Address;
-use go\core\mail\AddressList;
-use go\core\model;
 
 class Notify extends Controller {
 
@@ -19,15 +15,24 @@ class Notify extends Controller {
 	 * @return true[]
 	 * @throws \PHPMailer\PHPMailer\Exception
 	 */
-	public function mail($params) {		
+	public function mail($params) {
+
+		if(!empty($params['to']) && !go()->getAuthState()->isAdmin()) {
+			throw new Forbidden("You're not allowed to provide the 'to' recipients");
+		}
 		
 		$settings = go()->getSettings();
-		
+
 		$message = go()->getMailer()->compose()
 						->setFrom($settings->systemEmail, $settings->title)
-						->setTo(new Address($settings->systemEmail, $settings->title))
 						->setSubject($params['subject'] ?? "")
 						->setBody($params['body'] ?? "", $params['contentType'] ?? 'text/plain');
+
+		if(empty($params['to'])) {
+			$message->setTo(new Address($settings->systemEmail, $settings->title));
+		} else {
+			$message->setTo($params['to']);
+		}
 
 		if(isset($params['replyTo'])) {
 			$message->setReplyTo($params['replyTo']);
