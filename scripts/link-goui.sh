c7aa62cf5b1df128c9d63d8e669f579f785bb2ed
Merijn Schering <mschering@intermesh.nl>
2026-04-09 13:40:02 +0200

    link-goui.sh

---
+++ scripts/link-goui.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+sync_repo() {
+    local REPO_URL="${1:?sync_repo requires a repo URL}"
+    local TARGET_DIR="${2:-$(basename "$REPO_URL" .git)}"
+
+    if [ -d "$TARGET_DIR/.git" ]; then
+        echo "Updating existing repo in '$TARGET_DIR'..."
+        git -C "$TARGET_DIR" pull
+    else
+        echo "Cloning '$REPO_URL' into '$TARGET_DIR'..."
+        git clone "$REPO_URL" "$TARGET_DIR"
+    fi
+}
+
+cd ../
+mkdir -p links
+cd  links
+
+sync_repo "git@github.com:Intermesh/goui.git"
+sync_repo "git@github.com:Intermesh/groupoffice-core.git"
+
+
+echo "Linking @intermesh/goui and @intermesh/groupoffice-core in groupoffice"
+cd ../www
+npm link @intermesh/goui @intermesh/groupoffice-core
+
+echo Done
