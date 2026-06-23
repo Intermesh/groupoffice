f8f646a9904091bd28fd7350f70b0e0ca7f1ef9f
Merijn Schering <mschering@intermesh.nl>
2026-06-23 11:03:58 +0200

    Core: APCu cache only clears entries prefixed for the groupoffice instance
    (cherry picked from commit 18d374b9ba5e11bfc756d3b2ff83fae3e13017bd)

--- www/go/core/cache/Apcu.php
+++ www/go/core/cache/Apcu.php
@@ -5,9 +5,6 @@ namespace go\core\cache;
 
 use APCUIterator;
 use Exception;
-use go\core\Environment;
-use go\core\ErrorHandler;
-use go\core\http\Client;
 
 /**
  * Cache implementation that uses serialized objects in files on disk.
@@ -38,10 +35,6 @@ class Apcu implements CacheInterface {
 		$this->keepInMemory = false;
 		$this->getDiskCache()->disableMemory();
 	}
-
-	public function freeMemory(array $preserveKeys = ['entity-types']):void {
-		$this->cache = array_intersect_key($this->cache, array_flip($preserveKeys));
-	}
 	
 	public function __construct() {
 		$this->prefix = go()->getConfig()['db_name'];
@@ -56,7 +49,6 @@ class Apcu implements CacheInterface {
 	{
 		if(!isset($this->disk)) {
 			$this->disk = new Disk();
-			$this->disk->disableMemory();
 		}
 
 		return $this->disk;
@@ -74,9 +66,6 @@ class Apcu implements CacheInterface {
 	 */
 	public function set(string $key, $value, bool $persist = true, int $ttl = 0):void
 	{
-		if($this->keepInMemory) {
-			$this->cache[$key] = $value;
-		}
 
 		if(!$this->apcuEnabled) {
 			$this->getDiskCache()->set($key, $value, $persist, $ttl);
@@ -84,6 +73,10 @@ class Apcu implements CacheInterface {
 		}
 		if($persist) {
 			apcu_store($this->prefix . '-' .$key, $value, $ttl);
+		}
+
+		if($this->keepInMemory) {
+			$this->cache[$key] = $value;
 		}
 	}
 
@@ -100,12 +93,12 @@ class Apcu implements CacheInterface {
 	 */
 	public function get(string $key) {
 
-		if($this->keepInMemory && isset($this->cache[$key])) {
-			return $this->cache[$key];
-		}
-
 		if(!$this->apcuEnabled) {
 			return $this->getDiskCache()->get($key);
+		}
+
+		if($this->keepInMemory && isset($this->cache[$key])) {
+			return $this->cache[$key];
 		}
 
 		$success = false;
@@ -152,19 +145,7 @@ class Apcu implements CacheInterface {
 		}
 		$this->cache = [];
 		//		apcu_clear_cache();
-		if(apcu_enabled()) {
-			apcu_delete(new APCUIterator('/^' . preg_quote($this->prefix, '/') . '-/'));
-		} else if(Environment::get()->isCli() && go()->isInstalled() && !empty(go()->getSettings()->URL)) {
-			$http = new Client();
-			$http->setOption(CURLOPT_SSL_VERIFYHOST, false);
-			$http->setOption(CURLOPT_SSL_VERIFYPEER, false);
-
-			try {
-				$http->get(go()->getSettings()->URL . '/install/clearcache.php');
-			}catch(\Throwable $e) {
-				ErrorHandler::logException($e, "Failed to clear APCu cache via http request");
-			}
-		}
+		apcu_delete(new APCUIterator('/^'.preg_quote($this->prefix, '/').'-/'));
 
 		$this->getDiskCache()->flush(false);
 	}
