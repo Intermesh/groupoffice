b1ad8c2105916960606ad4c52ab051aa16500743
Merijn Schering <mschering@intermesh.nl>
2026-06-25 14:18:11 +0200

    Boot

--- www/views/goui/script/Index.ts
+++ www/views/goui/script/Index.ts
@@ -3,25 +3,11 @@ import {authManager, client, customFields, main, modules} from "@intermesh/group
 
 client.uri = BaseHref + "api/";
 
+// Loads all panels in the main view
+await main.boot();
 
-// Loads all module scripts before authentication
-await modules.loadCapabilities();
+// Add the Main view component holding all the module panels
+root.items.add(main);
 
-// Authenticate
-authManager.requireLogin().then(async () => {
-
-	// Load custom fields and server modules
-	await Promise.all([
-		customFields.init(),
-		modules.init()
-	])
-
-	// Loads all panels in the main view
-	main.load();
-
-	// Add the Main view component holding all the module panels
-	root.items.add(main);
-
-	// fire off the router
-	void router.start();
-});
+// fire off the router
+void router.start();
