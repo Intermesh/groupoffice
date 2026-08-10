7ef301d274ec1a628b2e5e219bfcbc5f5e761a57
dehart <derinus@gmail.com>
2026-08-10 14:10:09 +0200

    fixed setting calendar event filters so are remembered when the custom filters are turned on and off

--- www/go/modules/community/calendar/views/goui/script/Main.ts
+++ www/go/modules/community/calendar/views/goui/script/Main.ts
@@ -167,7 +167,7 @@ export class Main extends Component {
 					filterpanel({
 						entityName: "CalendarEvent",
 						store: this.adapter!.byType('event').store
-					})
+					}).on('filterchange', (f) => {  })
 				)
 			),
 			splitter({
@@ -363,8 +363,7 @@ export class Main extends Component {
 		const store = this.adapter.byType('event').store;
 
 		//const calendarIds = Object.keys(this.inCalendars).filter(key => this.inCalendars[key])
-
-		Object.assign(store.queryParams.filter ||= {}, {
+		store.setFilter('inCalendars', {
 			inCalendars: calendarIds
 		});
 
@@ -506,13 +505,10 @@ export class Main extends Component {
 				const store = this.adapter.byType('event').store;
 				const categoryIds = Object.keys(selected);
 
-				if(categoryIds.length) {
-					Object.assign(store.queryParams.filter ||= {}, {
-						inCategories: categoryIds
-					});
-				} else {
-					delete store.queryParams.filter?.inCategories;
-				}
+				store.setFilter('inCategories', categoryIds.length ? {
+					inCategories: categoryIds
+				} : undefined)
+
 				this.updateView();
 			};
 		return list({
@@ -737,10 +733,11 @@ export class Main extends Component {
 				}
 			},
 			load(start:DateTime,end:DateTime) {
-				Object.assign(this.store!.queryParams.filter ||= {}, {
+				this.store!.setFilter('period', {
 					after: start.format('Y-m-d'),
 					before: end.format('Y-m-d')
 				});
+
 				return this.store!.load();
 			}
 		});
