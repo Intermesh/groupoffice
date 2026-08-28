18dc2002f8dd0057c7a133cbd455ecbbc7468f39
Joachim van de Haterd <jvdhaterd@intermesh.nl>
2026-08-28 15:39:42 +0200

    Clean up JMAP cronjob schedule controller

--- www/go/core/controller/CronJobSchedule.php
+++ www/go/core/controller/CronJobSchedule.php
@@ -3,14 +3,17 @@
 namespace go\core\controller;
 
 use go\core\jmap\EntityController;
+use go\core\jmap\exception\InvalidArguments;
 use go\core\model;
+use go\core\util\ArrayObject;
 
 
-class CronJobSchedule extends EntityController {
-	
+class CronJobSchedule extends EntityController
+{
+
 	/**
 	 * The class name of the entity this controller is for.
-	 * 
+	 *
 	 * @return string
 	 */
 	protected function entityClass(): string
@@ -20,42 +23,55 @@ class CronJobSchedule extends EntityController {
 
 	/**
 	 * Handles the Foo entity's Foo/query command
-	 * 
+	 *
 	 * @param array $params
+	 * @return ArrayObject
+	 * @throws InvalidArguments
 	 * @see https://jmap.io/spec-core.html#/query
 	 */
-	public function query($params) {
+	public function query(array $params): ArrayObject
+	{
 		return $this->defaultQuery($params);
 	}
-	
+
 	/**
 	 * Handles the Foo entity's Foo/get command
-	 * 
+	 *
 	 * @param array $params
+	 * @return ArrayObject
+	 * @throws \Exception
 	 * @see https://jmap.io/spec-core.html#/get
 	 */
-	public function get($params) {
+	public function get(array $params): ArrayObject
+	{
 		return $this->defaultGet($params);
 	}
-	
+
 	/**
 	 * Handles the Foo entity's Foo/set command
-	 * 
+	 *
 	 * @see https://jmap.io/spec-core.html#/set
 	 * @param array $params
-	 */
-	public function set($params) {
+	 * @return ArrayObject
+	 * @throws InvalidArguments
+	 * @throws \Exception
+ */
+	public function set(array $params): ArrayObject
+	{
 		return $this->defaultSet($params);
 	}
-	
-	
+
+
 	/**
 	 * Handles the Foo entity's Foo/changes command
-	 * 
+	 *
 	 * @param array $params
+	 * @return ArrayObject
+	 * @throws InvalidArguments
 	 * @see https://jmap.io/spec-core.html#/changes
 	 */
-	public function changes($params) {
+	public function changes(array $params): ArrayObject
+	{
 		return $this->defaultChanges($params);
 	}
 }
