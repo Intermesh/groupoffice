8c106dd26aa2c515487a030d4b3ba865498b2b89
Merijn Schering <mschering@intermesh.nl>
2026-09-07 14:38:39 +0200

    Calendar: The server handles the event->replyTo setting better. On owned calendars the calendar owner's e-mail address is used and on shared calendar either the participant with owner = true or the event creator's email address is used

--- www/go/modules/community/calendar/model/CalendarEvent.php
+++ www/go/modules/community/calendar/model/CalendarEvent.php
@@ -695,10 +695,19 @@ class CalendarEvent extends AclItemEntity {
 				}
 			}
 			if (empty($this->replyTo)) {
-				$owner = $this->organizer();
-				if (!empty($owner)) {
-					$this->replyTo = $owner->email;
+
+				// In calendars without an owner the creator will be the organizer. Otherwise the calendar owner is
+				// the organizer.
+				$calendar = Calendar::findById($this->calendarId, ['ownerId']);
+				if($calendar->isOwned()) {
+					$owner = Principal::findById($calendar->getOwnerId(), ['email']);
+				} else {
+					$owner = $this->organizer();
+					if (empty($owner)) {
+						$owner = go()->getAuthState()->getUser(['email']);
+					}
 				}
+				$this->replyTo = $owner->email;
 			}
 		}
 
