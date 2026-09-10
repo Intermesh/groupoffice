d8932f099352a7a79ebe3136fe0363191fc83d6b
Merijn Schering <mschering@intermesh.nl>
2026-09-10 13:48:17 +0200

    revert merge mistake

--- www/views/Extjs3/javascript/MainLayout.js
+++ www/views/Extjs3/javascript/MainLayout.js
@@ -1,14 +1,14 @@
 
 /* global GO, Ext, go */
 
-/** 
+/**
  * Copyright Intermesh
- * 
+ *
  * This file is part of Group-Office. You should have received a copy of the
  * Group-Office license along with Group-Office. See the file /LICENSE.TXT
- * 
+ *
  * If you have questions write an e-mail to info@intermesh.nl
- * 
+ *
  * @copyright Copyright Intermesh
  * @version $Id: MainLayout.js 22429 2018-02-27 16:02:26Z mschering $
  * @author Merijn Schering <mschering@intermesh.nl>
@@ -16,11 +16,11 @@
 
 
 /**
- * 
- * 
- * 
+ *
+ *
+ *
  * @returns {undefined}
- * 
+ *
  */
 GO.MainLayout = function () {
 
@@ -29,7 +29,7 @@ GO.MainLayout = function () {
 		 * Deprecated. Use boot instead
 		 */
 		'ready': true,
-		
+
 		/**
 		 * Fires when main tab panel is rendered
 		 */
@@ -37,7 +37,7 @@ GO.MainLayout = function () {
 		'linksDeleted': true,
 		'focus': true,
 		'blur': true,
-		
+
 		/**
 		 * Fires on login
 		 */
@@ -46,7 +46,7 @@ GO.MainLayout = function () {
 		 * Fires lastly when either the tab panel is rendered or the login screen is shown
 		 */
 		'boot': true,
-		
+
 		/**
 		 * Fires when user is authenticated. Can be before boot too when user was already logged in when reloading.
 		 */
@@ -64,7 +64,7 @@ Ext.extend(GO.MainLayout, Ext.util.Observable, {
 	rendered: false,
 
 	/**
-	 * 
+	 *
 	 * @deprecated
 	 */
 	onReady: function (fn, scope) {
@@ -74,39 +74,7 @@ Ext.extend(GO.MainLayout, Ext.util.Observable, {
 			fn.call(scope, this);
 		}
 	},
-	
-	/**
-	 * Always called after all scripts are loaded in Ext.onReady();
-	 * @returns {undefined}
-	 */
-	boot : async function() {
 
-		window.GOUI = await import(BaseHref + "views/goui/dist/goui/script/index.js?v=" + GO.version);
-		window.groupofficeCore = await import(BaseHref + "views/goui/dist/groupoffice-core/script/index.js?v=" + GO.version);
-
-		var me = this;
-		go.browserStorage.connect().finally(function() {
-			Ext.QuickTips.init();
-			Ext.apply(Ext.QuickTips.getQuickTip(), {
-				dismissDelay: 0,
-				maxWidth: 500
-			});
-
-			Ext.Ajax.defaultHeaders = {'Accept-Language': GO.lang.iso};
-
-			go.User.authenticate().then((user) => {
-				me.on('render', function () {
-					me.fireEvent('boot', me);
-				}, me, {single: true});
-				me.onAuthentication(); // <- start Group-Office
-			}).catch((e) => {
-				console.warn(e);
-				me.fireEvent("boot", me);
-				go.Router.check();
-			})
-		});
-
-	},
 
 	saveState: function () {
 		Ext.state.Manager.getProvider().set('open-modules', this.getOpenModules());
@@ -131,476 +99,51 @@ Ext.extend(GO.MainLayout, Ext.util.Observable, {
 
 	},
 
-	createTabPanel: function (items) {
-
-		this.tabPanel = new Ext.TabPanel({
-			cls: "go-main-tab-panel",
-			region: 'center',
-			titlebar: false,
-			enableTabScroll: true,
-			border: false,
-			tabPosition: 'top',
-			items: items,
-			deferedRender:true
-		});
-
-		this.tabPanel.setActiveTab(null);
-
-
-		//blur active form fields on tab change. Otherwise auto complete combo boxes
-		//will remain focussed but the autocomplete functionality fails.
-		this.tabPanel.on('beforetabchange', function (tabpanel, newTab) {
-
-			if (!newTab) {
-				return;
-			}
-
-			//update hash if not already set.
-			if (!go.Router.routing) {
-				window.go.Router.goto(newTab.moduleName);
-			}
-
-			if (document.activeElement && typeof document.activeElement.blur === 'function')
-				if (document.activeElement.tagName === 'TEXTAREA' || document.activeElement.tagName === 'INPUT') {
-					document.activeElement.blur();
-				}
-		}, this);
-
-		const me = this, closeMenu = new Ext.menu.Menu({
-			items: [{
-				iconCls: "ic-close",
-				text: t("Close"),
-				handler: function() {
-					var openModules = me.getOpenModules();
-
-					const panel = this.ownerCt.panel,tp = me.tabPanel;
-
-					//don't hide last tab
-					if (openModules.length > 1) {
-
-						tp.hideTabStripItem(panel);
-						panel.hide();
-
-						if (panel == tp.activeTab) {
-							var next = tp.stack.next();
-							if (next) {
-								tp.setActiveTab(next);
-							} else if (tp.items.getCount() > 0) {
-								tp.setActiveTab(0);
-							} else {
-								tp.activeTab = null;
-							}
-						}
-						me.saveState();
-					}
-				}
-			}]
-		})
-
-		this.tabPanel.on('contextmenu', function (tp, panel, e) {
-
-			if (panel.closable) {
-				return false;
-			}
-
-			closeMenu.panel = panel;
-			closeMenu.showAt(e.getXY());
-
-
-		}, this);
-	},
-
 	getModulePanel: function (moduleName, unhide = true) {
-
-		this.initModule(moduleName, unhide);
-		var panelId = 'go-module-panel-' + moduleName;
-		if (this.tabPanel.items.map[panelId]) {
-			return this.tabPanel.items.map[panelId];
-		}
-
-		return false;
-	},
-
-	//overridable
-	beforeRender: function () {
-
-	},
-	
-	loadLegacyModuleScripts : function() {
-
-		return new Promise(function(resolve, reject) {
-				//legacy scripts loaded from scripts.inc.php
-			var script = document.createElement('script');
-			script.type = 'text/javascript';
-			script.setAttribute('src', BaseHref + "views/Extjs3/modulescripts.php?mtime=" + go.User.modifiedAt + "-" + go.User.apiVersion);
-			script.charset = 'utf-8';
-			script.id = 'testing';
-			script.defer = true;
-			script.async = true;
-			script.onload = function () {
-				resolve();
-			};
-			script.onerror = function () {
-				reject();
-			};
-
-			document.body.appendChild(script);
-		});
-		
-	},
-	
-	/**
-	 * Add module panel after rendering of main layout
-	 *  
-	 * @example 
-	 * ```
-	 * initModule: function () {
-	 * 		
-	 * 		setTimeout(function() {
-	 * 			
-	 * 			var test = Ext.extend(Ext.Panel, {
-	 * 				title: "Test",
-	 * 				html: "Dit is een test"
-	 * 			})
-	 * 			
-	 * 			GO.mainLayout.addModulePanel("test", test);
-	 * 			
-	 * 		}, 2000);
-	 * 		
-	 * 	}
-	 * 	```
-	 * 
-	 * @param {type} moduleName
-	 * @param {type} panelClass
-	 * @param {type} panelConfig
-	 * @returns {MainLayoutAnonym$1.initModule@pro;tabPanel@pro;items@arr;map|MainLayoutAnonym$1.initModule@pro;tabPanel@call;insert|MainLayoutAnonym$1.initModule.panel|Boolean}
-	 */
-	addModulePanel : function(moduleName, panelClass, panelConfig) {
-
-		panelConfig =panelConfig || {}
-		panelConfig.package = panelClass.prototype.package;
-
-		GO.moduleManager._addModule(moduleName, panelClass, panelConfig);
-				
-		go.Router.add(new RegExp('^(' + moduleName + ")$"), function (name) {
-			var pnl = GO.mainLayout.openModule(name);
-			if(pnl.routeDefault) {
-				pnl.routeDefault();
-			}
-		});
-	},
-
-	onAuthentication: function (password) {
-
-		//check if authRedirecUrl was given.
-		var urlParams = new URLSearchParams(window.location.search);
-		var oauthAuthorize = urlParams.get('oauthAuthorize');
-
-		if(oauthAuthorize) {
-			document.location.replace(BaseHref + "api/oauth.php/authorize");
-			return;
-		}
-
-		
-		//load state
-		if(!GO.util.isMobileOrTablet()) {
-			Ext.state.Manager.setProvider(new GO.state.HttpProvider());
-		} else
-		{
-			Ext.state.Manager.setProvider(new Ext.state.CookieProvider({
-				expires: new Date(new Date().getTime()+(1000*60*60*24*30)), //30 days
-			}));
-		}
-		document.documentElement.cls('compact',go.User.theme === 'Compact');
-		window.GOUI.DateTime.staticInit(go.User.language.substring(0,2), go.User.firstWeekday);
-
-		GO.util.density = parseFloat(window.getComputedStyle(document.documentElement).fontSize) / 10;
-
-		var me = this;
-
-		return go.Modules.init().then(function() {
-			go.User.loadLegacyModules();
-			return Promise.all([
-				go.customfields.CustomFields.init(),				
-				me.loadLegacyModuleScripts()
-			]).then(function(){
-				go.Entities.init();
-
-				window.groupofficeCore.client.authenticate().then(() => {
-					me.fireEvent('authenticated', this, go.User, password);
-					me.renderUI();
-					Ext.getBody().unmask();
-					setTimeout(() => {
-						//give "authenticated" listeners above a change to add routes
-						me.addDefaultRoutes();
-						go.Router.check();
-					})
-				});
-			});
-		}).catch(function(error) {
-			// Ext.getBody().unmask();
-			GO.errorDialog.show(error);
-		});
-		
-		
-		
-	},
-	
-	addDefaultRoutes : function() {
-		var me = this;
-		var c = go.User.capabilities['go:core:core'] || {};
-		go.Router.add(/systemsettings\/?([a-z0-9-_]*)?/i, function(tabId) {
-			me.openSystemSettings().setActiveItem(tabId);
-		});
-
-
-		//Add these default routes on boot so they are added as last options for sure.
-		//
-		//default route for entities		
-		go.Router.add(/([a-zA-Z0-9]*)\/([0-9]*)/, function(entity, id) {
-			var entityObj = go.Entities.get(entity);
-			if(!entityObj) {
-				console.log("Entity ("+entity+") not found in default entity route")
-				return false;
-			}
-			
-			var module = entityObj.module; 
-			var mainPanel = GO.mainLayout.getModulePanel(module);
-			var detailViewName = entity.charAt(0).toLowerCase() + entity.slice(1) + "Detail";
-
-			if (mainPanel.route) {
-				mainPanel.route(id, entityObj);
-			} else if(mainPanel[detailViewName]) {
-				mainPanel.show();
-				mainPanel[detailViewName].load(id);
-				mainPanel[detailViewName].show();
-			} else {
-				console.log("Default entity route failed because " + detailViewName + " or 'route' function not found in mainpanel of " + module + ":", mainPanel);
-				console.log(arguments);
-			}
-		});
-	},
-
-	renderUI : function() {
-		var me = this;
-
-		GO.checker = new GO.Checker();
-
-		this.fireReady();
-
-
-		var allPanels = GO.moduleManager.getAllPanelConfigs();
-
-		var items = [];
-
-		/*
-		 * Start global Add Button Code
-		 */
-		me.globalAddMenu = new GO.menu.JsonMenu({
-			id: 'globalAddMenu',
-			enableScrolling: false,
-			store: new Ext.data.JsonStore({
-				root: 'root',
-				proxy: new Ext.data.MemoryProxy(),
-				fields: ['id', 'text', 'iconCls', 'handler'],
-				sortInfo: {
-					field: 'text',
-					direction: 'ASC'
-				}
-			}),
-			listeners: {
-				afterrender: function(menu, e) {
-					menu.getEl().dom.addEventListener("click", (e) => {
-						menu.hide();
-					});
-				},
-				show: function() {
-
-				},
-				itemclick : function(item, e) {
-				},
-				scope: me
-
-			},
-			updateMenuItems: function() {
-				const gaMenu = me.globalAddMenu;
-				if(gaMenu.rendered){
-
-					gaMenu.removeAll();
-					gaMenu.el.sync();
-
-					const records = gaMenu.store.getRange();
-
-					for(let i= 0, len=records.length; i<len; i++){
-						gaMenu.add(records[i].json);
-					}
-
-					gaMenu.fireEvent('load', gaMenu, records);
-					gaMenu.loaded = true;
-				}
-
-			}
-		});
-		me.globalAddMenu.on('afterrender', (menu) => {
-			menu.updateMenuItems();
-
-		});
-		this.globalAddMenuItems = [];
-		go.Entities.getLinkConfigs().filter(function(l) {
-			return !!l.linkWindow;
-		}).forEach(function (l) {
-			this.globalAddMenuItems.push({
-				iconCls: l.iconCls,
-				text: l.title,
-				handler: function () {
-					const window = l.linkWindow.call(l.scope, null, 0, {},{});
-					if (!window) {
-						return;
-					}
-					window.redirectOnSave = false;
-
-					if (!window.isVisible()) {
-						window.show();
-					}
-					window.on("save", function (window, entity) {
-						go.Notifier.flyout({
-							title: t("Success"),
-							description: t("The data was saved successfully"),
-							time: 2000
-						});
-						// TODO: The code snippet works perfectly. If 'the field' asks for a redirect, put it back as a link...
-						// let id;
-						// const goEntity = go.Entities.get(l.entity);
-						//
-						// if(Ext.isNumber(entity)) {
-						// 	id = entity;
-						// } else if (window.event_id) {
-						// 	// for some reason, calendar events have a different onSave
-						// 	id = window.event_id;
-						// } else if(entity.id) {
-						// 	id = entity.id;
-						// }
-						//
-						// if(go.util.empty(id)) {
-						// 	console.warn("Unable to open new " + l.entity);
-						// 	return false;
-						// }
-						// goEntity.goto(id);
-					}, this, {single: true});
-
-					window.on("send", function(window) {
-						go.Notifier.flyout({
-							title: t("Success"),
-							description: t("Your message was sent successfully"),
-							time: 2000
-						});
-						// if(l.title !== "E-mail") {
-						// 	return false;
-						// }
-						// me.openModule("email");
-					}, this, {});
-				}
-			});
-		}, this);
-
-		me.globalAddMenu.store.loadData({root:this.globalAddMenuItems});
-		/*
-		 * END global add menu code
-		 */
-
-		/*
-		 * Begin start menu code
-		 */
-		me.startMenuSearchField = new Ext.menu.SearchFieldItem({
-			cls: 'go-menu-search-field',
-			placeholder: t("Module name") + "...",
-			listeners: {
-				keyup: {
-					fn: function(field, e) {
-						if (e.keyCode === e.ENTER) {
-							me.startMenu.openModule(me.startMenu.store.getAt(0).get('moduleName'));
-							return;
-						}
-						me.startMenu.store.filter('searchText', field.getValue().toLowerCase(), true);
-						me.startMenu.updateMenuItems();
-					},
-					buffer: 200,
-				},
-				clear: function () {
-					me.startMenu.store.clearFilter();
-					me.startMenu.updateMenuItems();
-				},
-				scope: me,
-			}
-		});
-
-		me.startMenu = new GO.menu.JsonMenu({
-			id: 'startMenu',
-			enableScrolling: false,
-			store: new Ext.data.JsonStore({
-				root: 'root',
-				proxy: new Ext.data.MemoryProxy(),
-				fields: ['id', 'moduleName', 'text', 'iconCls', 'handler', {
-					name: 'searchText',
-					convert: function (v, data) {
-						let searchString = data.moduleName.replace('-', '').toLowerCase();
-						return searchString + ' ' + (data.text ? data.text.toLowerCase() : '?');
-					}
-				}],
-				sortInfo: {
-					field: 'text',
-					direction: 'ASC',
-				},
-			}),
-			listeners: {
-				afterrender: function(menu, e) {
-					menu.getEl().dom.addEventListener("click", (e) => {
-						menu.hide();
-					});
-
-					me.startMenuSearchField.on("render", (sf) => {
-						sf.getEl().dom.addEventListener("click", (e) => {
-							e.stopPropagation();
-						});
-					});
-				},
-				show: function() {
-					if(!GO.util.isMobileOrTablet()) {
-						this.startMenuSearchField.focus(false, 500);
-					}
-				},
-				itemclick : function(item, e) {
-					if (!item.textField) {
-						me.startMenu.openModule(item.moduleName);
-					}
-				},
-				scope: me
-			},
-			openModule: function(moduleName) {
-				me.openModule(moduleName);
-				me.startMenuSearchField.textField.reset();
-				me.startMenuSearchField.fireEvent('clear');
-
-				me.startMenu.hide();
-
 		const wrapper = groupofficeCore.main.getPanelById(moduleName);
 		if(!wrapper) {
 			return false;
 		}
 		return wrapper.extJSComp;
+	},
 
+
+
+	/**
+	 * Add module panel after rendering of main layout
 	 *
 	 * @example
+	 * ```
+	 * initModule: function () {
 	 *
+	 * 		setTimeout(function() {
 	 *
+	 * 			var test = Ext.extend(Ext.Panel, {
+	 * 				title: "Test",
+	 * 				html: "Dit is een test"
+	 * 			})
 	 *
+	 * 			GO.mainLayout.addModulePanel("test", test);
 	 *
+	 * 		}, 2000);
 	 *
+	 * 	}
+	 * 	```
 	 *
+	 * @param {type} moduleName
+	 * @param {type} panelClass
+	 * @param {type} panelConfig
 	 * @returns
+	 */
+	addModulePanel : function(moduleName, panelClass, panelConfig) {
+
+		panelConfig =panelConfig || {}
 		panelConfig.package = panelClass.prototype.package ?? "legacy";
+
 		// GO.moduleManager._addModule(moduleName, panelClass, panelConfig);
+
 		window.groupofficeCore.main.addLegacyPanel(panelConfig.package, moduleName, panelClass.prototype.title, panelClass,panelConfig);
+
 		// go.Router.add(new RegExp('^(' + moduleName + ")$"), function (name) {
 		// 	var pnl = GO.mainLayout.openModule(name);
 		// 	if(pnl.routeDefault) {
