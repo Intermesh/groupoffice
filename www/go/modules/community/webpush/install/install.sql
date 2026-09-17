c1e0f3e69ff46e715f3a3ce2d362901ad36c5f16
Merijn Schering <mschering@intermesh.nl>
2026-09-17 13:15:25 +0200

    remove old column types

--- www/go/modules/community/webpush/install/install.sql
+++ www/go/modules/community/webpush/install/install.sql
@@ -6,7 +6,6 @@ CREATE TABLE core_push_subscription (
 	 p256dh VARCHAR(255) NOT NULL,
 	 auth VARCHAR(255) NOT NULL,
 	 expires DATETIME NOT NULL,
-	 types VARCHAR(512) NOT NULL,
 	 verificationCode VARCHAR(20) NULL,
 	 CONSTRAINT `core_push_subscriptions_core_user_fk1` FOREIGN KEY (`createdBy`) REFERENCES `core_user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 );
